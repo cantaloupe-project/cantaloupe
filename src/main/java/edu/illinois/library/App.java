5ab51661c47ac6ba3ad1180b29ed03b2665aa43a
Alex Dolski <alexd@n-ary.net>
2015-09-01 14:23:47 -0500

    Initial commit of new Maven project

---
+++ src/main/java/edu/illinois/library/App.java
@@ -0,0 +1,13 @@
+package edu.illinois.library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
