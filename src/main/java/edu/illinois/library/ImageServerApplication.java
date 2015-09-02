87b2fb54e2b4d97e014b973412c3528bff541569
Alex Dolski <alexd@n-ary.net>
2015-09-02 13:21:50 -0500

    Add a custom status service

--- src/main/java/edu/illinois/library/ImageServerApplication.java
+++ src/main/java/edu/illinois/library/ImageServerApplication.java
@@ -4,10 +4,19 @@ import edu.illinois.library.resource.ImageResource;
 import edu.illinois.library.resource.InformationResource;
 import edu.illinois.library.resource.LandingResource;
 import org.restlet.Application;
+import org.restlet.Request;
+import org.restlet.Response;
 import org.restlet.Restlet;
+import org.restlet.data.CharacterSet;
+import org.restlet.data.Language;
+import org.restlet.data.MediaType;
+import org.restlet.data.Status;
 import org.restlet.engine.application.CorsFilter;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 import org.restlet.routing.Router;
 import org.restlet.routing.Template;
+import org.restlet.service.StatusService;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -16,6 +25,44 @@ import java.util.HashSet;
  * Created by alexd on 9/1/15.
  */
 public class ImageServerApplication extends Application {
+
+    /**
+     * Overrides the built-in Restlet status pages.
+     */
+    private class CustomStatusService extends StatusService {
+
+        @Override
+        public Representation toRepresentation(Status status, Request request,
+                                               Response response) {
+            Throwable cause = status.getThrowable().getCause();
+            String msg = String.format(
+                    "<html><head></head><body><h1>%s %s</h1><p>%s</p></body></html>",
+                    Integer.toString(status.getCode()),
+                    status.getReasonPhrase(),
+                    cause.getMessage());
+            return new StringRepresentation(msg, MediaType.TEXT_HTML,
+                    Language.ENGLISH, CharacterSet.UTF_8);
+        }
+
+        @Override
+        public Status getStatus(Throwable t, Request request,
+                                Response response) {
+            Status status;
+            Throwable cause = t.getCause();
+            if (cause instanceof IllegalArgumentException) {
+                status = new Status(Status.CLIENT_ERROR_BAD_REQUEST, t);
+            } else {
+                status = new Status(Status.SERVER_ERROR_INTERNAL, t);
+            }
+            return status;
+        }
+
+    }
+
+    public ImageServerApplication() {
+        super();
+        this.setStatusService(new CustomStatusService());
+    }
 
     /**
      * Creates a root Restlet that will receive all incoming calls.
