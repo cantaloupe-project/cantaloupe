4062ede4609a43771c162611a7bbb89a9aa14dca
Alex Dolski <alexd@n-ary.net>
2015-09-01 19:38:19 -0500

    Extract HTTP port into a config file

--- src/main/java/edu/illinois/library/App.java
+++ src/main/java/edu/illinois/library/App.java
@@ -1,13 +1,18 @@
 package edu.illinois.library;
 
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        PropertiesConfiguration config = new PropertiesConfiguration();
+        config.load("cantaloupe.properties");
+
         Component component = new Component();
-        component.getServers().add(Protocol.HTTP, 8182);
+        component.getServers().add(Protocol.HTTP,
+                Integer.valueOf((String) config.getProperty("http.port")));
         component.getDefaultHost().attach("/iiif",
                 new ImageServerApplication());
         component.start();
