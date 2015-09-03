194ba380af200636dc88fcd24492821c3e9b5cf6
Alex Dolski <alexd@n-ary.net>
2015-09-03 09:00:37 -0500

    process() mostly works

--- src/main/java/edu/illinois/library/cantaloupe/processor/ImageMagickProcessor.java
+++ src/main/java/edu/illinois/library/cantaloupe/processor/ImageMagickProcessor.java
@@ -1,6 +1,10 @@
 package edu.illinois.library.cantaloupe.processor;
 
 import edu.illinois.library.cantaloupe.image.Parameters;
+import edu.illinois.library.cantaloupe.image.Quality;
+import edu.illinois.library.cantaloupe.image.Region;
+import edu.illinois.library.cantaloupe.image.Rotation;
+import edu.illinois.library.cantaloupe.image.Size;
 import edu.illinois.library.cantaloupe.resolver.Resolver;
 import edu.illinois.library.cantaloupe.resolver.ResolverFactory;
 import org.im4java.core.ConvertCmd;
@@ -17,7 +21,62 @@ public class ImageMagickProcessor implements Processor {
         String filePath = this.getResolvedPathname(params.getIdentifier());
 
         IMOperation op = new IMOperation();
-        op.addImage("-");
+        op.addImage("-"); // read from stdin
+
+        // region transformation
+        Region region = params.getRegion();
+        if (!region.isFull()) {
+            if (region.isPercent()) {
+                op.crop(region.getWidth(), region.getHeight(),
+                        Math.round(region.getX()), Math.round(region.getY()),
+                        "%".charAt(0)); // TODO: this doesn't work
+            } else {
+                op.crop(region.getWidth(), region.getHeight(),
+                        Math.round(region.getX()), Math.round(region.getY()));
+            }
+        }
+
+        // size transformation
+        Size size = params.getSize();
+        if (!size.isFull()) {
+            if (size.isFitWidth()) {
+                op.resize(size.getWidth());
+            } else if (size.isFitHeight()) {
+                op.resize(null, size.getHeight());
+            } else if (size.getWidth() != null && size.getHeight() != null) {
+                op.resize(size.getWidth(), size.getHeight(), "!".charAt(0));
+            } else if (size.getScaleToPercent() != null) {
+                op.resize(Math.round(size.getScaleToPercent()),
+                        Math.round(size.getScaleToPercent()),
+                        "%".charAt(0));
+            } else if (size.isScaleToFit()) {
+                op.resize(size.getWidth(), size.getHeight());
+            }
+        }
+
+        // rotation transformation
+        Rotation rotation = params.getRotation();
+        if (rotation.getDegrees() != 0) {
+            if (rotation.shouldMirror()) {
+                op.flop();
+            }
+            op.rotate(rotation.getDegrees().doubleValue());
+        }
+
+        // quality transformation
+        Quality quality = params.getQuality();
+        if (quality != Quality.COLOR && quality != Quality.DEFAULT) {
+            switch (quality) {
+                case GRAY:
+                    op.colorspace("Gray");
+                    break;
+                case BITONAL:
+                    op.monochrome();
+                    break;
+            }
+        }
+
+        // format transformation
         op.addImage(params.getFormat().getExtension() + ":-"); // write to stdout
 
         FileInputStream fis = new FileInputStream(filePath);
