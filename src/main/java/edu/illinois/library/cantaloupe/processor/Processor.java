f722d827b8914c6291f1bf2ba62d9f4bb60a54a1
Alex Dolski <alexd@n-ary.net>
2015-09-03 10:32:26 -0500

    Add documentation

--- src/main/java/edu/illinois/library/cantaloupe/processor/Processor.java
+++ src/main/java/edu/illinois/library/cantaloupe/processor/Processor.java
@@ -4,10 +4,29 @@ import edu.illinois.library.cantaloupe.image.Parameters;
 
 import java.io.OutputStream;
 
+/**
+ * Interface to be implemented by all image processors.
+ */
 public interface Processor {
 
+    /**
+     * Processes an image based on the supplied parameters and writes the
+     * result to the given OutputStream. Implementations should use
+     * <code>ResolverFactory.getResolver()</code> to obtain an instance of the
+     * correct Resolver, and use that to find the source image based on its
+     * identifier (<code>Parameters.getIdentifier()</code>).
+     *
+     * @param p
+     * @param os
+     * @throws Exception
+     */
     public void process(Parameters p, OutputStream os) throws Exception;
 
+    /**
+     * @param identifier IIIF identifier
+     * @return Whether or not the resource corresponding to the given
+     * identifier exists.
+     */
     public boolean resourceExists(String identifier);
 
 }
--- src/main/java/edu/illinois/library/cantaloupe/resolver/Resolver.java
+++ src/main/java/edu/illinois/library/cantaloupe/resolver/Resolver.java
@@ -1,7 +1,14 @@
 package edu.illinois.library.cantaloupe.resolver;
 
+/**
+ * Locates a source image.
+ */
 public interface Resolver {
 
+    /**
+     * @param identifier IIIF identifier.
+     * @return Path, URL, or some other locator to the source image.
+     */
     String resolve(String identifier);
 
 }
