cantaloupe-project/cantaloupe

commit ceac5c2916842cc2ba2b8e94cca668736e963f57

Rename ...cantaloupe.image to ...cantaloupe.request

src/main/java/edu/illinois/library/cantaloupe/request/Format.java
@@ -1,14 +1,14 @@
-package edu.illinois.library.cantaloupe.image;
+package edu.illinois.library.cantaloupe.request;
 
 public enum Format {
 
-    GIF("gif", "image/gif"),
-    JP2("jp2", "image/jp2"),
-    JPG("jpg", "image/jpeg"),
+    GIF("gif", "request/gif"),
+    JP2("jp2", "request/jp2"),
+    JPG("jpg", "request/jpeg"),
     PDF("pdf", "application/pdf"),
-    PNG("png", "image/png"),
-    TIF("tif", "image/tiff"),
-    WEBP("webp", "image/webp");
+    PNG("png", "request/png"),
+    TIF("tif", "request/tiff"),
+    WEBP("webp", "request/webp");
 
     private String extension;
     private String mediaType;

src/main/java/edu/illinois/library/cantaloupe/processor/ImageMagickProcessor.java
@@ -1,10 +1,10 @@
 package edu.illinois.library.cantaloupe.processor;
 
-import edu.illinois.library.cantaloupe.image.Parameters;
-import edu.illinois.library.cantaloupe.image.Quality;
-import edu.illinois.library.cantaloupe.image.Region;
-import edu.illinois.library.cantaloupe.image.Rotation;
-import edu.illinois.library.cantaloupe.image.Size;
+import edu.illinois.library.cantaloupe.request.Parameters;
+import edu.illinois.library.cantaloupe.request.Quality;
+import edu.illinois.library.cantaloupe.request.Region;
+import edu.illinois.library.cantaloupe.request.Rotation;
+import edu.illinois.library.cantaloupe.request.Size;
 import edu.illinois.library.cantaloupe.resolver.Resolver;
 import edu.illinois.library.cantaloupe.resolver.ResolverFactory;
 import org.im4java.core.ConvertCmd;

src/main/java/edu/illinois/library/cantaloupe/processor/Processor.java
@@ -1,6 +1,6 @@
 package edu.illinois.library.cantaloupe.processor;
 
-import edu.illinois.library.cantaloupe.image.Parameters;
+import edu.illinois.library.cantaloupe.request.Parameters;
 
 import java.io.OutputStream;
 

src/main/java/edu/illinois/library/cantaloupe/request/Parameters.java
@@ -1,9 +1,9 @@
-package edu.illinois.library.cantaloupe.image;
+package edu.illinois.library.cantaloupe.request;
 
 /**
  * Encapsulates the parameters of an IIIF request.
  *
- * @see <a href="http://iiif.io/api/image/2.0/#image-request-parameters">IIIF
+ * @see <a href="http://iiif.io/api/request/2.0/#request-request-parameters">IIIF
  *      Image API 2.0</a>
  */
 public class Parameters {

src/main/java/edu/illinois/library/cantaloupe/request/Quality.java
@@ -1,4 +1,4 @@
-package edu.illinois.library.cantaloupe.image;
+package edu.illinois.library.cantaloupe.request;
 
 /**
  * Created by alexd on 9/2/15.

src/main/java/edu/illinois/library/cantaloupe/request/Region.java
@@ -1,4 +1,4 @@
-package edu.illinois.library.cantaloupe.image;
+package edu.illinois.library.cantaloupe.request;
 
 /**
  * Created by alexd on 9/2/15.

src/main/java/edu/illinois/library/cantaloupe/request/Rotation.java
@@ -1,4 +1,4 @@
-package edu.illinois.library.cantaloupe.image;
+package edu.illinois.library.cantaloupe.request;
 
 /**
  * Created by alexd on 9/2/15.
@@ -41,7 +41,7 @@ public class Rotation {
     }
 
     /**
-     * @param mirror Whether the image should be mirrored before being
+     * @param mirror Whether the request should be mirrored before being
      *               rotated.
      */
     public void setMirror(boolean mirror) {
@@ -49,7 +49,7 @@ public class Rotation {
     }
 
     /**
-     * @return Whether the image should be mirrored before being rotated.
+     * @return Whether the request should be mirrored before being rotated.
      */
     public boolean shouldMirror() {
         return mirror;

src/main/java/edu/illinois/library/cantaloupe/request/Size.java
@@ -1,4 +1,4 @@
-package edu.illinois.library.cantaloupe.image;
+package edu.illinois.library.cantaloupe.request;
 
 import org.apache.commons.lang3.StringUtils;
 

commit a6ba7ed97bd5f1509294d32fa775e8052d8475fb

Fix a bug introduced in ceac5c2

src/main/java/edu/illinois/library/cantaloupe/request/Format.java
@@ -2,13 +2,13 @@ package edu.illinois.library.cantaloupe.request;
 
 public enum Format {
 
-    GIF("gif", "request/gif"),
-    JP2("jp2", "request/jp2"),
-    JPG("jpg", "request/jpeg"),
+    GIF("gif", "image/gif"),
+    JP2("jp2", "image/jp2"),
+    JPG("jpg", "image/jpeg"),
     PDF("pdf", "application/pdf"),
-    PNG("png", "request/png"),
-    TIF("tif", "request/tiff"),
-    WEBP("webp", "request/webp");
+    PNG("png", "image/png"),
+    TIF("tif", "image/tiff"),
+    WEBP("webp", "image/webp");
 
     private String extension;
     private String mediaType;