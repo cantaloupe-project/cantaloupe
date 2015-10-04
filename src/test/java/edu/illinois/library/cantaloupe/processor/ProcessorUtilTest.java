72c411a66ca93f030c5826d6b9ebc8db6208b021
Alex Dolski <alexd@n-ary.net>
2015-10-03 21:44:43 -0500

    Add some empty tests

--- src/test/java/edu/illinois/library/cantaloupe/processor/ProcessorUtilTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/processor/ProcessorUtilTest.java
@@ -4,6 +4,26 @@ import junit.framework.TestCase;
 
 public class ProcessorUtilTest extends TestCase {
 
+    public void testConvertToRgb() {
+        // TODO: write this
+    }
+
+    public void testCropImage() {
+        // TODO: write this
+    }
+
+    public void testCropImageWithReductionFactor() {
+        // TODO: write this
+    }
+
+    public void testFilterImageWithBufferedImage() {
+        // TODO: write this
+    }
+
+    public void testFilteredImageWithRenderedOp() {
+        // TODO: write this
+    }
+
     public void testGetReductionFactor() {
         assertEquals(0, ProcessorUtil.getReductionFactor(0.75, 5));
         assertEquals(1, ProcessorUtil.getReductionFactor(0.5, 5));
@@ -20,6 +40,46 @@ public class ProcessorUtilTest extends TestCase {
         assertTrue(Math.abs(ProcessorUtil.getScale(1)) - Math.abs(0.5f) < fudge);
         assertTrue(Math.abs(ProcessorUtil.getScale(2)) - Math.abs(0.25f) < fudge);
         assertTrue(Math.abs(ProcessorUtil.getScale(3)) - Math.abs(0.125f) < fudge);
+    }
+
+    public void testGetSize() {
+        // TODO: write this
+    }
+
+    public void testImageIoOutputFormats() {
+        // TODO: write this
+    }
+
+    public void testRotateImageWithBufferedImage() {
+        // TODO: write this
+    }
+
+    public void testRotateImageWithRenderedOp() {
+        // TODO: write this
+    }
+
+    public void testScaleImageWithRenderedOp() {
+        // TODO: write this
+    }
+
+    public void testScaleImageWithRenderedOpWithReductionFactor() {
+        // TODO: write this
+    }
+
+    public void testScaleImageWithAffineTransform() {
+        // TODO: write this
+    }
+
+    public void testScaleImageWithG2d() {
+        // TODO: write this
+    }
+
+    public void testScaleImageWithG2dWithReductionFactor() {
+        // TODO: write this
+    }
+
+    public void testWriteImage() {
+        // TODO: write this
     }
 
 }
