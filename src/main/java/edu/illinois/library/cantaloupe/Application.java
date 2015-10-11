cantaloupe-project/cantaloupe

commit b576b230563085c775dcd836f485a292f4c33647

Improve feedback when -Dcantaloupe.config is missing

src/main/java/edu/illinois/library/cantaloupe/Application.java
@@ -36,6 +36,10 @@ public class Application {
     private static Configuration config;
 
     static {
+        if (getConfiguration() == null) {
+            System.exit(0);
+        }
+
         initializeLogging();
 
         Velocity.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
@@ -135,7 +139,9 @@ public class Application {
                 propConfig.load(configFilePath);
                 config = propConfig;
             } catch (ConfigurationException e) {
-                logger.error(e.getMessage());
+                // The logger has probably not been initialized yet, as it
+                // depends on a working configuration
+                System.out.println(e.getMessage());
             }
         }
         return config;

commit 8e9894aa9c63559e9c44fa6c9bc10878cd0cc7f6

Fix b576b23 which broke tests

src/main/java/edu/illinois/library/cantaloupe/Application.java
@@ -36,10 +36,6 @@ public class Application {
     private static Configuration config;
 
     static {
-        if (getConfiguration() == null) {
-            System.exit(0);
-        }
-
         initializeLogging();
 
         Velocity.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
@@ -109,6 +105,11 @@ public class Application {
     }
 
     public static void main(String[] args) throws Exception {
+        if (getConfiguration() == null) {
+            System.out.println("No configuration file specified. Try again " +
+                    "with the -Dcantaloupe.config=/path/to/cantaloupe.properties option.");
+            System.exit(0);
+        }
         logger.info(System.getProperty("java.vm.name") + " / " +
                 System.getProperty("java.vm.info"));
         logger.info("Starting Cantaloupe {}", getVersion());
@@ -129,20 +130,16 @@ public class Application {
         if (config == null) {
             try {
                 String configFilePath = System.getProperty("cantaloupe.config");
-                if (configFilePath == null) {
-                    throw new ConfigurationException("No configuration file " +
-                            "specified. Try again with the " +
-                            "-Dcantaloupe.config=/path/to/cantaloupe.properties " +
-                            "option.");
+                if (configFilePath != null) {
+                    configFilePath = configFilePath.replaceFirst("^~",
+                            System.getProperty("user.home"));
+                    PropertiesConfiguration propConfig = new PropertiesConfiguration();
+                    propConfig.load(configFilePath);
+                    config = propConfig;
                 }
-                configFilePath = configFilePath.replaceFirst("^~",
-                        System.getProperty("user.home"));
-                PropertiesConfiguration propConfig = new PropertiesConfiguration();
-                propConfig.load(configFilePath);
-                config = propConfig;
             } catch (ConfigurationException e) {
                 // The logger has probably not been initialized yet, as it
-                // depends on a working configuration
+                // depends on a working configuration.
                 System.out.println(e.getMessage());
             }
         }