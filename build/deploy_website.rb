9c8553e629e767409a498e462d0e2bbf34ca7dcb
Alex Dolski <alexd@n-ary.net>
2015-10-12 19:16:19 -0500

    Update

--- build/deploy_website.rb
+++ build/deploy_website.rb
@@ -6,42 +6,43 @@
 require 'tmpdir'
 
 # make sure there are no outstanding changes before beginning
-raise 'Outstanding changes' unless
-    `git status`.include?('nothing to commit, working directory clean')
+#raise 'Outstanding changes' unless
+#    `git status`.include?('nothing to commit, working directory clean')
 
-begin
-  # get the current git branch
-  starting_branch = nil
-  orphan_exists = false
-  `git branch --no-color`.each_line do |line|
-    branch = line.gsub('*', '').strip
-    starting_branch = branch if line[0] == '*'
-    orphan_exists = true if branch == 'gh-pages'
-  end
+# make sure there are no outstanding changes before beginning
+#raise 'Outstanding changes' unless
+#    `git status`.include?('nothing to commit, working directory clean')
 
-  # generate site in a temp dir
-  puts Dir.methods.sort
-  Dir.mktmpdir do |tmp_dir|
-    puts "Building site in #{tmp_dir}"
-    `jekyll build -d #{tmp_dir}`
-
-    # switch to gh-pages branch
-    if orphan_exists
-      result = system('git checkout gh-pages')
-    else
-      result = system('git checkout --orphan gh-pages')
-    end
-    raise 'Failed to checkout gh-pages' unless result
-
-    # wipe it clean and copy the docs back into it
-    `git rm -rf .`
-    `cp -r #{File.join(tmp_dir, '_site', '*')} .`
-
-    # commit and push
-    `git add *`
-    `git commit -m 'Update website'`
-    `git push origin gh-pages`
-  end
-ensure
-  `git checkout #{starting_branch}`
+# get the current git branch
+starting_branch = nil
+orphan_exists = false
+`git branch --no-color`.each_line do |line|
+  branch = line.gsub('*', '').strip
+  starting_branch = branch if line[0] == '*'
+  orphan_exists = true if branch == 'gh-pages'
 end
+
+# generate site in a temp dir
+Dir.mktmpdir('website') do |tmp_dir|
+  puts "Building site in #{tmp_dir}"
+  `jekyll build -d #{tmp_dir}`
+
+  # switch to gh-pages branch
+  if orphan_exists
+    result = system('git checkout gh-pages')
+  else
+    result = system('git checkout --orphan gh-pages')
+  end
+  raise 'Failed to checkout gh-pages' unless result
+
+  # wipe it clean and copy the docs back into it
+  `git rm -rf .`
+  `cp -r #{File.join(tmp_dir, '*')} .`
+
+  # commit and push
+  `git add *`
+  `git commit -m 'Update website'`
+  `git push origin gh-pages`
+end
+
+`git checkout #{starting_branch}`
