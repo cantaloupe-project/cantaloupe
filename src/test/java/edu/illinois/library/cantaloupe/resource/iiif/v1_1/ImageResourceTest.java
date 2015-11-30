7e6e3b498888a3c2ad59a5e5d0b12b54d1621ddf
Alex Dolski <alexd@n-ary.net>
2015-11-30 17:24:52 -0600

    Add ImageResourceTest and InformationResourceTest

---
+++ src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v1_1/ImageResourceTest.java
@@ -0,0 +1,172 @@
+package edu.illinois.library.cantaloupe.resource.iiif.v1_1;
+
+import edu.illinois.library.cantaloupe.Application;
+import edu.illinois.library.cantaloupe.ImageServerApplication;
+import edu.illinois.library.cantaloupe.resource.ImageRepresentation;
+import edu.illinois.library.cantaloupe.resource.ResourceTest;
+import org.apache.commons.configuration.Configuration;
+import org.restlet.data.CacheDirective;
+import org.restlet.data.ChallengeResponse;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Disposition;
+import org.restlet.data.Status;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ImageResourceTest extends ResourceTest {
+
+    @Override
+    protected ClientResource getClientForUriPath(String path) {
+        return super.getClientForUriPath(ImageServerApplication.IIIF_1_1_PATH + path);
+    }
+
+    public void testBasicAuth() throws Exception {
+        final String username = "user";
+        final String secret = "secret";
+        Application.stopServer();
+        Configuration config = Application.getConfiguration();
+        config.setProperty("http.auth.basic", "true");
+        config.setProperty("http.auth.basic.username", username);
+        config.setProperty("http.auth.basic.secret", secret);
+        Application.startServer();
+
+        // no credentials
+        ClientResource client = getClientForUriPath("/jpg/full/full/0/native.jpg");
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_UNAUTHORIZED, client.getStatus());
+        }
+
+        // invalid credentials
+        client.setChallengeResponse(
+                new ChallengeResponse(ChallengeScheme.HTTP_BASIC, "invalid", "invalid"));
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_UNAUTHORIZED, client.getStatus());
+        }
+
+        // valid credentials
+        client.setChallengeResponse(
+                new ChallengeResponse(ChallengeScheme.HTTP_BASIC, username, secret));
+        client.get();
+        assertEquals(Status.SUCCESS_OK, client.getStatus());
+    }
+
+    public void testCacheHeadersWhenCachingEnabled() {
+        Configuration config = Application.getConfiguration();
+        config.setProperty("cache.client.enabled", "true");
+        config.setProperty("cache.client.max_age", "1234");
+        config.setProperty("cache.client.shared_max_age", "4567");
+        config.setProperty("cache.client.public", "true");
+        config.setProperty("cache.client.private", "false");
+        config.setProperty("cache.client.no_cache", "false");
+        config.setProperty("cache.client.no_store", "false");
+        config.setProperty("cache.client.must_revalidate", "false");
+        config.setProperty("cache.client.proxy_revalidate", "false");
+        config.setProperty("cache.client.no_transform", "true");
+
+        Map<String, String> expectedDirectives = new HashMap<>();
+        expectedDirectives.put("max-age", "1234");
+        expectedDirectives.put("s-maxage", "4567");
+        expectedDirectives.put("public", null);
+        expectedDirectives.put("no-transform", null);
+
+        ClientResource client = getClientForUriPath("/jpg/full/full/0/native.jpg");
+        client.get();
+        List<CacheDirective> actualDirectives = client.getResponse().getCacheDirectives();
+        for (CacheDirective d : actualDirectives) {
+            if (d.getName() != null) {
+                assertTrue(expectedDirectives.keySet().contains(d.getName()));
+                if (d.getValue() != null) {
+                    assertTrue(expectedDirectives.get(d.getName()).equals(d.getValue()));
+                } else {
+                    assertNull(expectedDirectives.get(d.getName()));
+                }
+            }
+        }
+    }
+
+    public void testCacheHeadersWhenCachingDisabled() {
+        Configuration config = Application.getConfiguration();
+        config.setProperty("cache.client.enabled", "false");
+        config.setProperty("cache.client.max_age", "1234");
+        config.setProperty("cache.client.shared_max_age", "4567");
+        config.setProperty("cache.client.public", "true");
+        config.setProperty("cache.client.private", "false");
+        config.setProperty("cache.client.no_cache", "false");
+        config.setProperty("cache.client.no_store", "false");
+        config.setProperty("cache.client.must_revalidate", "false");
+        config.setProperty("cache.client.proxy_revalidate", "false");
+        config.setProperty("cache.client.no_transform", "true");
+        Application.setConfiguration(config);
+
+        ClientResource client = getClientForUriPath("/jpg/full/full/0/native.jpg");
+        client.get();
+        assertEquals(0, client.getResponse().getCacheDirectives().size());
+    }
+
+    public void testContentDispositionHeader() {
+        // no header
+        ClientResource client = getClientForUriPath("/jpg/full/full/0/native.jpg");
+        client.get();
+        assertNull(client.getResponseEntity().getDisposition());
+
+        // inline
+        Configuration config = Application.getConfiguration();
+        config.setProperty(ImageRepresentation.CONTENT_DISPOSITION_CONFIG_KEY,
+                "inline");
+        client.get();
+        assertEquals(Disposition.TYPE_INLINE,
+                client.getResponseEntity().getDisposition().getType());
+
+        // attachment
+        config.setProperty(ImageRepresentation.CONTENT_DISPOSITION_CONFIG_KEY,
+                "attachment");
+        client.get();
+        assertEquals(Disposition.TYPE_ATTACHMENT,
+                client.getResponseEntity().getDisposition().getType());
+        assertEquals("jpg.jpg",
+                client.getResponseEntity().getDisposition().getFilename());
+    }
+
+    public void testNotFound() throws IOException {
+        ClientResource client = getClientForUriPath("/invalid/info.json");
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_NOT_FOUND, client.getStatus());
+        }
+    }
+
+    public void testUnavailableSourceFormat() throws IOException {
+        ClientResource client = getClientForUriPath("/text.txt/full/full/0/native.jpg");
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE,
+                    client.getStatus());
+        }
+    }
+
+    public void testUnavailableOutputFormat() throws IOException {
+        ClientResource client = getClientForUriPath("/escher_logo.jpg/full/full/0/native.bogus");
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_BAD_REQUEST, client.getStatus());
+        }
+    }
+
+}
---
+++ src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v1_1/InformationResourceTest.java
@@ -0,0 +1,109 @@
+package edu.illinois.library.cantaloupe.resource.iiif.v1_1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.illinois.library.cantaloupe.Application;
+import edu.illinois.library.cantaloupe.ImageServerApplication;
+import edu.illinois.library.cantaloupe.resource.ResourceTest;
+import org.apache.commons.configuration.Configuration;
+import org.restlet.data.CacheDirective;
+import org.restlet.data.Status;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Functional test of the non-IIIF features of InformationResource.
+ */
+public class InformationResourceTest extends ResourceTest {
+
+    @Override
+    protected ClientResource getClientForUriPath(String path) {
+        return super.getClientForUriPath(ImageServerApplication.IIIF_1_1_PATH + path);
+    }
+
+    public void testCacheHeaders() {
+        Configuration config = Application.getConfiguration();
+        config.setProperty("cache.client.enabled", "true");
+        config.setProperty("cache.client.max_age", "1234");
+        config.setProperty("cache.client.shared_max_age", "4567");
+        config.setProperty("cache.client.public", "false");
+        config.setProperty("cache.client.private", "true");
+        config.setProperty("cache.client.no_cache", "true");
+        config.setProperty("cache.client.no_store", "true");
+        config.setProperty("cache.client.must_revalidate", "false");
+        config.setProperty("cache.client.proxy_revalidate", "true");
+        config.setProperty("cache.client.no_transform", "false");
+
+        Map<String, String> expectedDirectives = new HashMap<>();
+        expectedDirectives.put("max-age", "1234");
+        expectedDirectives.put("s-maxage", "4567");
+        expectedDirectives.put("private", null);
+        expectedDirectives.put("no-cache", null);
+        expectedDirectives.put("no-store", null);
+        expectedDirectives.put("proxy-revalidate", null);
+        expectedDirectives.put("no-transform", null);
+
+        ClientResource client = getClientForUriPath("/jpg/info.json");
+        client.get();
+        List<CacheDirective> actualDirectives = client.getResponse().getCacheDirectives();
+        for (CacheDirective d : actualDirectives) {
+            if (d.getName() != null) {
+                assertTrue(expectedDirectives.keySet().contains(d.getName()));
+                if (d.getValue() != null) {
+                    assertTrue(expectedDirectives.get(d.getName()).equals(d.getValue()));
+                } else {
+                    assertNull(expectedDirectives.get(d.getName()));
+                }
+            }
+        }
+    }
+
+    public void testJson() throws IOException {
+        // TODO: this could be a lot more thorough; but the aspects of the JSON
+        // response defined in the Image API spec are tested in
+        // ConformanceTest
+
+        // test whether the @id property respects the base_uri configuration
+        // option
+        ClientResource client = getClientForUriPath("/escher_lego.jpg/info.json");
+        client.get();
+        String json = client.getResponse().getEntityAsText();
+        ObjectMapper mapper = new ObjectMapper();
+        ImageInfo info = mapper.readValue(json, ImageInfo.class);
+        assertTrue(info.id.startsWith("http://") &&
+                info.id.contains(ImageServerApplication.IIIF_1_1_PATH + "/escher_lego.jpg"));
+
+        Configuration config = Application.getConfiguration();
+        config.setProperty("base_uri", "http://example.org/");
+        client.get();
+        json = client.getResponse().getEntityAsText();
+        info = mapper.readValue(json, ImageInfo.class);
+        assertTrue(info.id.startsWith("http://example.org/"));
+    }
+
+    public void testNotFound() throws IOException {
+        ClientResource client = getClientForUriPath("/invalid/info.json");
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_NOT_FOUND, client.getStatus());
+        }
+    }
+
+    public void testUnavailableSourceFormat() throws IOException {
+        ClientResource client = getClientForUriPath("/text.txt/info.json");
+        try {
+            client.get();
+            fail("Expected exception");
+        } catch (ResourceException e) {
+            assertEquals(Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE,
+                    client.getStatus());
+        }
+    }
+
+}
