272f9e06388c13fe28b213b783e35a39f1503682
Alex Dolski <alexd@n-ary.net>
2015-12-07 17:26:58 -0600

    Move getRepresentation() into AbstractResource

--- src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -4,10 +4,12 @@ import edu.illinois.library.cantaloupe.Application;
 import edu.illinois.library.cantaloupe.cache.Cache;
 import edu.illinois.library.cantaloupe.cache.CacheFactory;
 import edu.illinois.library.cantaloupe.image.Identifier;
+import edu.illinois.library.cantaloupe.image.OperationList;
 import edu.illinois.library.cantaloupe.image.SourceFormat;
 import edu.illinois.library.cantaloupe.processor.FileProcessor;
 import edu.illinois.library.cantaloupe.processor.Processor;
 import edu.illinois.library.cantaloupe.processor.StreamProcessor;
+import edu.illinois.library.cantaloupe.processor.UnsupportedSourceFormatException;
 import edu.illinois.library.cantaloupe.resolver.FileResolver;
 import edu.illinois.library.cantaloupe.resolver.Resolver;
 import edu.illinois.library.cantaloupe.resolver.StreamResolver;
@@ -15,6 +17,7 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.lang3.StringUtils;
 import org.restlet.data.CacheDirective;
 import org.restlet.data.Header;
+import org.restlet.data.MediaType;
 import org.restlet.data.Reference;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
@@ -23,6 +26,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.Dimension;
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -32,10 +37,37 @@ public abstract class AbstractResource extends ServerResource {
     private static Logger logger = LoggerFactory.
             getLogger(AbstractResource.class);
 
+    private static final String MAX_PIXELS_CONFIG_KEY = "max_pixels";
     protected static final String PURGE_MISSING_CONFIG_KEY =
             "cache.server.purge_missing";
     protected static final String RESOLVE_FIRST_CONFIG_KEY =
             "cache.server.resolve_first";
+
+    @Override
+    protected void doInit() throws ResourceException {
+        super.doInit();
+        // override the Server header
+        // TODO: this doesn't affect redirecting responses
+        this.getServerInfo().setAgent("Cantaloupe/" + Application.getVersion());
+    }
+
+    /**
+     * Convenience method that adds a response header.
+     *
+     * @param key Header key
+     * @param value Header value
+     */
+    @SuppressWarnings({"unchecked"})
+    protected void addHeader(String key, String value) {
+        Series<Header> responseHeaders = (Series<Header>) getResponse().
+                getAttributes().get("org.restlet.http.headers");
+        if (responseHeaders == null) {
+            responseHeaders = new Series(Header.class);
+            getResponse().getAttributes().
+                    put("org.restlet.http.headers", responseHeaders);
+        }
+        responseHeaders.add(new Header(key, value));
+    }
 
     protected List<CacheDirective> getCacheDirectives() {
         List<CacheDirective> directives = new ArrayList<>();
@@ -52,24 +84,24 @@ public abstract class AbstractResource extends ServerResource {
                     directives.add(CacheDirective.
                             sharedMaxAge(Integer.parseInt(sMaxAge)));
                 }
-                if (config.getBoolean("cache.client.public")) {
+                if (config.getBoolean("cache.client.public", true)) {
                     directives.add(CacheDirective.publicInfo());
-                } else if (config.getBoolean("cache.client.private")) {
+                } else if (config.getBoolean("cache.client.private", false)) {
                     directives.add(CacheDirective.privateInfo());
                 }
-                if (config.getBoolean("cache.client.no_cache")) {
+                if (config.getBoolean("cache.client.no_cache", false)) {
                     directives.add(CacheDirective.noCache());
                 }
-                if (config.getBoolean("cache.client.no_store")) {
+                if (config.getBoolean("cache.client.no_store", false)) {
                     directives.add(CacheDirective.noStore());
                 }
-                if (config.getBoolean("cache.client.must_revalidate")) {
+                if (config.getBoolean("cache.client.must_revalidate", false)) {
                     directives.add(CacheDirective.mustRevalidate());
                 }
-                if (config.getBoolean("cache.client.proxy_revalidate")) {
+                if (config.getBoolean("cache.client.proxy_revalidate", false)) {
                     directives.add(CacheDirective.proxyMustRevalidate());
                 }
-                if (config.getBoolean("cache.client.no_transform")) {
+                if (config.getBoolean("cache.client.no_transform", false)) {
                     directives.add(CacheDirective.noTransform());
                 }
             } else {
@@ -107,30 +139,64 @@ public abstract class AbstractResource extends ServerResource {
         return rootRef;
     }
 
-    @Override
-    protected void doInit() throws ResourceException {
-        super.doInit();
-        // override the Server header
-        // TODO: this doesn't affect redirecting responses
-        this.getServerInfo().setAgent("Cantaloupe/" + Application.getVersion());
-    }
+    protected ImageRepresentation getRepresentation(OperationList ops,
+                                                    SourceFormat sourceFormat,
+                                                    Resolver resolver,
+                                                    Processor proc)
+            throws Exception {
+        final MediaType mediaType = new MediaType(
+                ops.getOutputFormat().getMediaType());
+        final long maxAllowedSize = Application.getConfiguration().
+                getLong(MAX_PIXELS_CONFIG_KEY, 0);
 
-    /**
-     * Convenience method that adds a response header.
-     *
-     * @param key Header key
-     * @param value Header value
-     */
-    @SuppressWarnings({"unchecked"})
-    protected void addHeader(String key, String value) {
-        Series<Header> responseHeaders = (Series<Header>) getResponse().
-                getAttributes().get("org.restlet.http.headers");
-        if (responseHeaders == null) {
-            responseHeaders = new Series(Header.class);
-            getResponse().getAttributes().
-                    put("org.restlet.http.headers", responseHeaders);
+        // FileResolver -> StreamProcessor: OK, using FileInputStream
+        // FileResolver -> FileProcessor: OK, using File
+        // StreamResolver -> StreamProcessor: OK, using InputStream
+        // StreamResolver -> FileProcessor: NOPE
+        if (!(resolver instanceof FileResolver) &&
+                !(proc instanceof StreamProcessor)) {
+            // FileProcessors can't work with StreamResolvers
+            throw new UnsupportedSourceFormatException(
+                    String.format("%s is not compatible with %s",
+                            proc.getClass().getSimpleName(),
+                            resolver.getClass().getSimpleName()));
+        } else if (resolver instanceof FileResolver &&
+                proc instanceof FileProcessor) {
+            logger.debug("Using {} as a FileProcessor",
+                    proc.getClass().getSimpleName());
+            final FileProcessor fproc = (FileProcessor) proc;
+            final File inputFile = ((FileResolver) resolver).
+                    getFile(ops.getIdentifier());
+            final Dimension fullSize = fproc.getSize(inputFile, sourceFormat);
+            final Dimension effectiveSize = ops.getResultingSize(fullSize);
+            if (maxAllowedSize > 0 &&
+                    effectiveSize.width * effectiveSize.height > maxAllowedSize) {
+                throw new PayloadTooLargeException();
+            }
+            return new ImageRepresentation(mediaType, sourceFormat, fullSize,
+                    ops, inputFile);
+        } else if (resolver instanceof StreamResolver) {
+            logger.debug("Using {} as a StreamProcessor",
+                    proc.getClass().getSimpleName());
+            final StreamResolver sres = (StreamResolver) resolver;
+            if (proc instanceof StreamProcessor) {
+                final StreamProcessor sproc = (StreamProcessor) proc;
+                InputStream inputStream = sres.
+                        getInputStream(ops.getIdentifier());
+                final Dimension fullSize = sproc.getSize(inputStream,
+                        sourceFormat);
+                final Dimension effectiveSize = ops.getResultingSize(fullSize);
+                if (maxAllowedSize > 0 &&
+                        effectiveSize.width * effectiveSize.height > maxAllowedSize) {
+                    throw new PayloadTooLargeException();
+                }
+                // avoid reusing the stream
+                inputStream = sres.getInputStream(ops.getIdentifier());
+                return new ImageRepresentation(mediaType, sourceFormat,
+                        fullSize, ops, inputStream);
+            }
         }
-        responseHeaders.add(new Header(key, value));
+        return null; // should never happen
     }
 
     /**
--- src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2_0/ImageResource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2_0/ImageResource.java
@@ -7,20 +7,14 @@ import edu.illinois.library.cantaloupe.cache.CacheFactory;
 import edu.illinois.library.cantaloupe.image.OperationList;
 import edu.illinois.library.cantaloupe.image.OutputFormat;
 import edu.illinois.library.cantaloupe.image.SourceFormat;
-import edu.illinois.library.cantaloupe.processor.FileProcessor;
 import edu.illinois.library.cantaloupe.processor.Processor;
 import edu.illinois.library.cantaloupe.processor.ProcessorFactory;
-import edu.illinois.library.cantaloupe.processor.StreamProcessor;
 import edu.illinois.library.cantaloupe.processor.UnsupportedSourceFormatException;
-import edu.illinois.library.cantaloupe.resolver.FileResolver;
 import edu.illinois.library.cantaloupe.resolver.Resolver;
 import edu.illinois.library.cantaloupe.resolver.ResolverFactory;
-import edu.illinois.library.cantaloupe.resolver.StreamResolver;
 import edu.illinois.library.cantaloupe.resource.AbstractResource;
 import edu.illinois.library.cantaloupe.resource.CachedImageRepresentation;
 import edu.illinois.library.cantaloupe.resource.EndpointDisabledException;
-import edu.illinois.library.cantaloupe.resource.ImageRepresentation;
-import edu.illinois.library.cantaloupe.resource.PayloadTooLargeException;
 import org.restlet.data.MediaType;
 import org.restlet.representation.OutputRepresentation;
 import org.restlet.resource.Get;
@@ -28,8 +22,6 @@ import org.restlet.resource.ResourceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.Dimension;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Map;
@@ -135,66 +127,6 @@ public class ImageResource extends AbstractResource {
         this.addHeader("Link", String.format("<%s%s/%s>;rel=\"canonical\"",
                 getPublicRootRef().toString(),
                 ImageServerApplication.IIIF_2_0_PATH, params.toString()));
-    }
-
-    private OutputRepresentation getRepresentation(OperationList ops,
-                                                   SourceFormat sourceFormat,
-                                                   Resolver resolver,
-                                                   Processor proc)
-            throws Exception {
-        final MediaType mediaType = new MediaType(
-                ops.getOutputFormat().getMediaType());
-        final long maxAllowedSize = Application.getConfiguration().
-                getLong("max_pixels", 0);
-
-        // FileResolver -> StreamProcessor: OK, using FileInputStream
-        // FileResolver -> FileProcessor: OK, using File
-        // StreamResolver -> StreamProcessor: OK, using InputStream
-        // StreamResolver -> FileProcessor: NOPE
-        if (!(resolver instanceof FileResolver) &&
-                !(proc instanceof StreamProcessor)) {
-            // FileProcessors can't work with StreamResolvers
-            throw new UnsupportedSourceFormatException(
-                    String.format("%s is not compatible with %s",
-                            proc.getClass().getSimpleName(),
-                            resolver.getClass().getSimpleName()));
-        } else if (resolver instanceof FileResolver &&
-                proc instanceof FileProcessor) {
-            logger.debug("Using {} as a FileProcessor",
-                    proc.getClass().getSimpleName());
-            final FileProcessor fproc = (FileProcessor) proc;
-            final File inputFile = ((FileResolver) resolver).
-                    getFile(ops.getIdentifier());
-            final Dimension fullSize = fproc.getSize(inputFile, sourceFormat);
-            final Dimension effectiveSize = ops.getResultingSize(fullSize);
-            if (maxAllowedSize > 0 &&
-                    effectiveSize.width * effectiveSize.height > maxAllowedSize) {
-                throw new PayloadTooLargeException();
-            }
-            return new ImageRepresentation(mediaType, sourceFormat, fullSize,
-                    ops, inputFile);
-        } else if (resolver instanceof StreamResolver) {
-            logger.debug("Using {} as a StreamProcessor",
-                    proc.getClass().getSimpleName());
-            final StreamResolver sres = (StreamResolver) resolver;
-            if (proc instanceof StreamProcessor) {
-                final StreamProcessor sproc = (StreamProcessor) proc;
-                InputStream inputStream = sres.
-                        getInputStream(ops.getIdentifier());
-                final Dimension fullSize = sproc.getSize(inputStream,
-                        sourceFormat);
-                final Dimension effectiveSize = ops.getResultingSize(fullSize);
-                if (maxAllowedSize > 0 &&
-                        effectiveSize.width * effectiveSize.height > maxAllowedSize) {
-                    throw new PayloadTooLargeException();
-                }
-                // avoid reusing the stream
-                inputStream = sres.getInputStream(ops.getIdentifier());
-                return new ImageRepresentation(mediaType, sourceFormat,
-                        fullSize, ops, inputStream);
-            }
-        }
-        return null; // should never happen
     }
 
 }
