35e4030f1dbf00611d97443ac060213c5f1cef57
Alex Dolski <alexd@n-ary.net>
2015-12-11 16:56:27 -0600

    Add a ScriptLookupStrategy to HttpResolver

--- src/main/java/edu/illinois/library/cantaloupe/resolver/HttpResolver.java
+++ src/main/java/edu/illinois/library/cantaloupe/resolver/HttpResolver.java
@@ -4,6 +4,8 @@ import edu.illinois.library.cantaloupe.Application;
 import edu.illinois.library.cantaloupe.image.SourceFormat;
 import edu.illinois.library.cantaloupe.image.Identifier;
 import org.apache.commons.configuration.Configuration;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.restlet.Client;
 import org.restlet.Context;
@@ -17,44 +19,99 @@ import org.restlet.resource.ResourceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.AccessDeniedException;
+import java.util.HashSet;
+import java.util.Set;
 
 class HttpResolver implements StreamResolver {
 
     private static Logger logger = LoggerFactory.getLogger(HttpResolver.class);
 
-    private static final String BASIC_AUTH_SECRET_CONFIG_KEY =
+    public static final String BASIC_AUTH_SECRET_CONFIG_KEY =
             "HttpResolver.auth.basic.secret";
-    private static final String BASIC_AUTH_USERNAME_CONFIG_KEY =
+    public static final String BASIC_AUTH_USERNAME_CONFIG_KEY =
             "HttpResolver.auth.basic.username";
-    private static final String PATH_SEPARATOR_CONFIG_KEY =
+    public static final String LOOKUP_SCRIPT_CONFIG_KEY =
+            "HttpResolver.ScriptLookupStrategy.script";
+    public static final String LOOKUP_STRATEGY_CONFIG_KEY =
+            "HttpResolver.lookup_strategy";
+    public static final String PATH_SEPARATOR_CONFIG_KEY =
             "HttpResolver.path_separator";
-    private static final String URL_PREFIX_CONFIG_KEY =
-            "HttpResolver.url_prefix";
-    private static final String URL_SUFFIX_CONFIG_KEY =
-            "HttpResolver.url_suffix";
+    public static final String URL_PREFIX_CONFIG_KEY =
+            "HttpResolver.BasicLookupStrategy.url_prefix";
+    public static final String URL_SUFFIX_CONFIG_KEY =
+            "HttpResolver.BasicLookupStrategy.url_suffix";
 
-    private static Client client = new Client(new Context(), Protocol.HTTP);
+    private static final Set<String> SUPPORTED_SCRIPT_EXTENSIONS =
+            new HashSet<>();
+
+    private static Client client = new Client(new Context(), Protocol.HTTP); // TODO: HTTPS
+
+    // Caches the lookup script for improved performance.
+    private static String lookupScriptContents;
+
+    // lock object for synchronization
+    private final Object lock = new Object();
+
+    static {
+        SUPPORTED_SCRIPT_EXTENSIONS.add("rb");
+    }
+
+    /**
+     * Passes the given identifier to a function in the given script.
+     *
+     * @param identifier
+     * @param script
+     * @return Pathname of the image file corresponding to the given identifier,
+     * as reported by the lookup script.
+     * @throws IOException If the lookup script configuration key is undefined
+     * @throws ScriptException If the script failed to execute
+     * @throws ScriptException If the script is of an unsupported type
+     */
+    public String executeLookupScript(Identifier identifier, File script)
+            throws IOException, ScriptException {
+        final String extension = FilenameUtils.getExtension(script.getName());
+
+        if (SUPPORTED_SCRIPT_EXTENSIONS.contains(extension)) {
+            logger.debug("Using lookup script: {}", script);
+            if (lookupScriptContents == null) {
+                synchronized (lock) {
+                    lookupScriptContents = FileUtils.readFileToString(script);
+                }
+            }
+            final ScriptEngineManager manager = new ScriptEngineManager();
+
+            switch (extension) {
+                case "rb":
+                    final String idParam = StringUtils.replace(
+                            identifier.toString(), "'", "\\'");
+                    final String statement = String.format("%s\nget_url('%s')",
+                            lookupScriptContents, idParam);
+                    final ScriptEngine engine = manager.getEngineByName("jruby");
+                    final long msec = System.currentTimeMillis();
+                    final String result = (String) engine.eval(statement);
+                    logger.debug("Lookup function execution time: {} msec",
+                            System.currentTimeMillis() - msec);
+                    return result;
+            }
+        }
+        throw new ScriptException("Unsupported script type: " + extension);
+    }
 
     @Override
-    public InputStream getInputStream(Identifier identifier)
+    public InputStream getInputStream(final Identifier identifier)
             throws IOException {
-        Configuration config = Application.getConfiguration();
         Reference url = getUrl(identifier);
         logger.debug("Resolved {} to {}", identifier, url);
-        ClientResource resource = new ClientResource(url);
+        ClientResource resource = newClientResource(url);
         resource.setNext(client);
-
-        // set up HTTP Basic authentication
-        String username = config.getString(BASIC_AUTH_USERNAME_CONFIG_KEY, "");
-        String secret = config.getString(BASIC_AUTH_SECRET_CONFIG_KEY, "");
-        if (username.length() > 0 && secret.length() > 0) {
-            resource.setChallengeResponse(ChallengeScheme.HTTP_BASIC,
-                    username, secret);
-        }
         try {
             return resource.get().getStream();
         } catch (ResourceException e) {
@@ -69,7 +126,8 @@ class HttpResolver implements StreamResolver {
         }
     }
 
-    public SourceFormat getSourceFormat(Identifier identifier)
+    @Override
+    public SourceFormat getSourceFormat(final Identifier identifier)
             throws IOException {
         SourceFormat format = getSourceFormatFromIdentifier(identifier);
         if (format == SourceFormat.UNKNOWN) {
@@ -79,27 +137,24 @@ class HttpResolver implements StreamResolver {
         return format;
     }
 
-    public Reference getUrl(Identifier identifier) {
-        Configuration config = Application.getConfiguration();
-        String prefix = config.getString(URL_PREFIX_CONFIG_KEY);
-        if (prefix == null) {
-            prefix = "";
-        }
-        String suffix = config.getString(URL_SUFFIX_CONFIG_KEY);
-        if (suffix == null) {
-            suffix = "";
-        }
+    public Reference getUrl(Identifier identifier) throws IOException {
+        final Configuration config = Application.getConfiguration();
+        identifier = replacePathSeparators(identifier);
 
-        String idStr = identifier.toString();
-
-        // Some web servers have issues dealing with encoded slashes (%2F) in
-        // URL identifiers. HttpResolver.path_separator enables the use of an
-        // alternate string as a path separator.
-        String separator = config.getString(PATH_SEPARATOR_CONFIG_KEY, "/");
-        if (!separator.equals("/")) {
-            idStr = StringUtils.replace(idStr, separator, "/");
+        switch (config.getString(LOOKUP_STRATEGY_CONFIG_KEY)) {
+            case "BasicLookupStrategy":
+                return getUrlWithBasicStrategy(identifier);
+            case "ScriptLookupStrategy":
+                try {
+                    return getUrlWithScriptStrategy(identifier);
+                } catch (ScriptException e) {
+                    logger.error(e.getMessage(), e);
+                    throw new IOException(e);
+                }
+            default:
+                throw new IOException(LOOKUP_STRATEGY_CONFIG_KEY +
+                        " is invalid or not set");
         }
-        return new Reference(prefix + idStr + suffix);
     }
 
     /**
@@ -133,8 +188,10 @@ class HttpResolver implements StreamResolver {
      *
      * @param identifier
      * @return A source format, or {@link SourceFormat#UNKNOWN} if unknown.
+     * @throws IOException
      */
-    private SourceFormat getSourceFormatFromServer(Identifier identifier) {
+    private SourceFormat getSourceFormatFromServer(Identifier identifier)
+            throws IOException {
         SourceFormat sourceFormat = SourceFormat.UNKNOWN;
         String contentType = "";
         Reference url = getUrl(identifier);
@@ -161,6 +218,84 @@ class HttpResolver implements StreamResolver {
             }
         }
         return sourceFormat;
+    }
+
+    private Reference getUrlWithBasicStrategy(final Identifier identifier) {
+        final Configuration config = Application.getConfiguration();
+        final String prefix = config.getString(URL_PREFIX_CONFIG_KEY, "");
+        final String suffix = config.getString(URL_SUFFIX_CONFIG_KEY, "");
+        return new Reference(prefix + identifier.toString() + suffix);
+    }
+
+    /**
+     * @param identifier
+     * @return
+     * @throws FileNotFoundException If a script does not exist
+     * @throws IOException
+     * @throws ScriptException If the script fails to execute
+     * @throws ScriptException If the script is of an unsupported type
+     */
+    private Reference getUrlWithScriptStrategy(Identifier identifier)
+            throws IOException, ScriptException {
+        final Configuration config = Application.getConfiguration();
+        // The script name may be an absolute path or a filename.
+        final String scriptValue = config.
+                getString(LOOKUP_SCRIPT_CONFIG_KEY);
+        File script = new File(scriptValue);
+        if (!script.isAbsolute()) {
+            // Search for it in the same folder as the application
+            // config (if available), or the current working
+            // directory if not.
+            final File configFile = Application.getConfigurationFile();
+            if (configFile != null) {
+                script = new File(configFile.getParent() + "/" +
+                        script.getName());
+            } else {
+                script = new File("./" + script.getName());
+            }
+        }
+        if (!script.exists()) {
+            throw new FileNotFoundException("Does not exist: " +
+                    script.getAbsolutePath());
+        }
+        return new Reference(executeLookupScript(identifier, script));
+    }
+
+    /**
+     * Factory method.
+     *
+     * @param url
+     * @return New ClientResource respecting HttpResolver configuration
+     * options.
+     */
+    private ClientResource newClientResource(final Reference url) {
+        final ClientResource resource = new ClientResource(url);
+        final Configuration config = Application.getConfiguration();
+        final String username = config.getString(BASIC_AUTH_USERNAME_CONFIG_KEY, "");
+        final String secret = config.getString(BASIC_AUTH_SECRET_CONFIG_KEY, "");
+        if (username.length() > 0 && secret.length() > 0) {
+            resource.setChallengeResponse(ChallengeScheme.HTTP_BASIC,
+                    username, secret);
+        }
+        return resource;
+    }
+
+    /**
+     * Some web servers have issues dealing with encoded slashes (%2F) in URL
+     * identifiers. This method enables the use of an alternate string as a
+     * path separator via {@link #PATH_SEPARATOR_CONFIG_KEY}.
+     * #
+     * @param identifier
+     * @return
+     */
+    private Identifier replacePathSeparators(final Identifier identifier) {
+        final Configuration config = Application.getConfiguration();
+        String idStr = identifier.toString();
+        final String separator = config.getString(PATH_SEPARATOR_CONFIG_KEY, "");
+        if (separator.length() > 0) {
+            idStr = StringUtils.replace(idStr, separator, "/");
+        }
+        return new Identifier(idStr);
     }
 
 }
--- src/test/java/edu/illinois/library/cantaloupe/resolver/HttpResolverTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/resolver/HttpResolverTest.java
@@ -4,29 +4,54 @@ import edu.illinois.library.cantaloupe.Application;
 import edu.illinois.library.cantaloupe.CantaloupeTestCase;
 import edu.illinois.library.cantaloupe.image.SourceFormat;
 import edu.illinois.library.cantaloupe.image.Identifier;
+import edu.illinois.library.cantaloupe.test.TestUtil;
 import org.apache.commons.configuration.BaseConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.io.FileUtils;
+import org.restlet.data.Reference;
 
+import javax.script.ScriptException;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HttpResolverTest extends CantaloupeTestCase {
 
-    private static final Identifier IMAGE = new Identifier("14405804_o1.jpg");
-    HttpResolver instance;
+    private static final Identifier IDENTIFIER = new Identifier("14405804_o1.jpg");
+
+    private HttpResolver instance;
 
     public void setUp() throws IOException {
         BaseConfiguration config = new BaseConfiguration();
-        config.setProperty("HttpResolver.url_prefix",
+        config.setProperty(HttpResolver.LOOKUP_STRATEGY_CONFIG_KEY,
+                "BasicLookupStrategy");
+        config.setProperty(HttpResolver.URL_PREFIX_CONFIG_KEY,
                 "https://ia601502.us.archive.org/4/items/14405804O1_201507/");
         Application.setConfiguration(config);
 
         instance = new HttpResolver();
     }
 
+    public void testExecuteLookupScript() throws Exception {
+        // valid script
+        File script = TestUtil.getFixture("lookup.rb");
+        String result = instance.executeLookupScript(IDENTIFIER, script);
+        assertEquals("http://example.org/bla/" + IDENTIFIER, result);
+
+        // unsupported script
+        try {
+            script = TestUtil.getFixture("lookup.js");
+            instance.executeLookupScript(IDENTIFIER, script);
+            fail("Expected exception");
+        } catch (ScriptException e) {
+            assertEquals("Unsupported script type: js", e.getMessage());
+        }
+    }
+
     public void testGetInputStream() {
         // present, readable image
         try {
-            assertNotNull(instance.getInputStream(IMAGE));
+            assertNotNull(instance.getInputStream(IDENTIFIER));
         } catch (IOException e) {
             fail();
         }
@@ -44,7 +69,7 @@ public class HttpResolverTest extends CantaloupeTestCase {
     }
 
     public void testGetSourceFormat() throws IOException {
-        assertEquals(SourceFormat.JPG, instance.getSourceFormat(IMAGE));
+        assertEquals(SourceFormat.JPG, instance.getSourceFormat(IDENTIFIER));
         try {
             instance.getSourceFormat(new Identifier("image.bogus"));
             fail("Expected exception");
@@ -59,29 +84,81 @@ public class HttpResolverTest extends CantaloupeTestCase {
         }
     }
 
-    public void testGetUrl() {
+    public void testGetUrlWithBasicLookupStrategy() throws Exception {
         BaseConfiguration config = (BaseConfiguration) Application.getConfiguration();
         // with prefix
-        config.setProperty("HttpResolver.url_prefix",
+        config.setProperty(HttpResolver.URL_PREFIX_CONFIG_KEY,
                 "http://example.org/prefix/");
         assertEquals("http://example.org/prefix/id",
                 instance.getUrl(new Identifier("id")).toString());
         // with suffix
-        config.setProperty("HttpResolver.url_suffix", "/suffix");
+        config.setProperty(HttpResolver.URL_SUFFIX_CONFIG_KEY, "/suffix");
         assertEquals("http://example.org/prefix/id/suffix",
                 instance.getUrl(new Identifier("id")).toString());
         // without prefix or suffix
-        config.setProperty("HttpResolver.url_prefix", "");
-        config.setProperty("HttpResolver.url_suffix", "");
+        config.setProperty(HttpResolver.URL_PREFIX_CONFIG_KEY, "");
+        config.setProperty(HttpResolver.URL_SUFFIX_CONFIG_KEY, "");
         assertEquals("http://example.org/images/image.jpg",
                 instance.getUrl(new Identifier("http://example.org/images/image.jpg")).toString());
-        // with path separator
-        config.setProperty("HttpResolver.url_prefix", "http://example.org/");
-        config.setProperty("HttpResolver.url_suffix", "");
+    }
+
+    public void testGetUrlWithScriptLookupStrategyAndAbsolutePath()
+            throws Exception {
+        Configuration config = Application.getConfiguration();
+        config.setProperty(HttpResolver.LOOKUP_STRATEGY_CONFIG_KEY,
+                "ScriptLookupStrategy");
+
+        // valid, present script
+        config.setProperty(HttpResolver.LOOKUP_SCRIPT_CONFIG_KEY,
+                TestUtil.getFixture("lookup.rb").getAbsolutePath());
+        assertEquals(new Reference("http://example.org/bla/" + IDENTIFIER),
+                instance.getUrl(IDENTIFIER));
+
+        // missing script
+        try {
+            config.setProperty(HttpResolver.LOOKUP_SCRIPT_CONFIG_KEY,
+                    TestUtil.getFixture("bogus.rb").getAbsolutePath());
+            instance.getUrl(IDENTIFIER);
+            fail("Expected exception");
+        } catch (FileNotFoundException e) {
+            // pass
+        }
+    }
+
+    public void testGetUrlWithScriptLookupStrategyAndRelativePath()
+            throws Exception {
+        Configuration config = Application.getConfiguration();
+        config.setProperty(HttpResolver.LOOKUP_STRATEGY_CONFIG_KEY,
+                "ScriptLookupStrategy");
+
+        // filename of script, located in cwd
+        config.setProperty(HttpResolver.LOOKUP_SCRIPT_CONFIG_KEY,
+                "lookup.rb");
+        final File tempFile = new File("./lookup.rb");
+        try {
+            FileUtils.copyFile(TestUtil.getFixture("lookup.rb"), tempFile);
+            assertEquals(new Reference("http://example.org/bla/" + IDENTIFIER),
+                    instance.getUrl(IDENTIFIER));
+        } finally {
+            //FileUtils.forceDelete(tempFile);
+        }
+    }
+
+    public void testGetUrlWithScriptLookupStrategyAndPathSeparator()
+            throws Exception {
+        Configuration config = Application.getConfiguration();
+        config.setProperty(HttpResolver.LOOKUP_STRATEGY_CONFIG_KEY,
+                "ScriptLookupStrategy");
+        config.setProperty(HttpResolver.LOOKUP_SCRIPT_CONFIG_KEY,
+                TestUtil.getFixture("lookup.rb").getAbsolutePath());
+
         String separator = "CATS";
-        config.setProperty("HttpResolver.path_separator", separator);
-        assertEquals("http://example.org/1/2/3",
-                instance.getUrl(new Identifier("1" + separator + "2" + separator + "3")).toString());
+        config.setProperty(HttpResolver.PATH_SEPARATOR_CONFIG_KEY, separator);
+        final Reference expected = new Reference("http://example.org/bla/1" +
+                File.separator + "2" + File.separator + "3");
+        final Reference actual = instance.getUrl(
+                new Identifier("1" + separator + "2" + separator + "3"));
+        assertEquals(expected, actual);
     }
 
 }
