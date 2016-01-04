2b3215b4e99479a64319a7215374afcf266e5dca
Alex Dolski <alexd@n-ary.net>
2016-01-03 21:48:00 -0600

    Fix testGetScriptEngine()

--- src/test/java/edu/illinois/library/cantaloupe/script/ScriptEngineFactoryTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/script/ScriptEngineFactoryTest.java
@@ -1,12 +1,9 @@
 package edu.illinois.library.cantaloupe.script;
 
 import edu.illinois.library.cantaloupe.Application;
-import edu.illinois.library.cantaloupe.test.TestUtil;
 import org.apache.commons.configuration.BaseConfiguration;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.File;
 
 import static org.junit.Assert.*;
 
@@ -21,15 +18,6 @@ public class ScriptEngineFactoryTest {
     public void testGetScriptEngine() throws Exception {
         ScriptEngine engine = ScriptEngineFactory.getScriptEngine();
         assertNotNull(engine);
-        assertFalse(engine.methodExists("get_iiif2_service"));
-
-        File script = TestUtil.getFixture("delegate.rb");
-        Application.getConfiguration().setProperty("delegate_script",
-                script.getAbsolutePath());
-
-        engine = ScriptEngineFactory.getScriptEngine();
-        assertNotNull(engine);
-        assertTrue(engine.methodExists("get_iiif2_service"));
     }
 
 
