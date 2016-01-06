afe0082af732b1916c0de68c8632cfded7e3e020
Alex Dolski <alexd@n-ary.net>
2016-01-06 16:07:09 -0600

    Update website

--- target/test-classes/lookup.rb
+++
@@ -1,9 +0,0 @@
-require 'uri'
-
-def get_pathname(identifier)
-  '/bla/' + identifier
-end
-
-def get_url(identifier)
-  'http://example.org/bla/' + URI.escape(identifier)
-end
---
+++ target/test-classes/lookup_nil.rb
@@ -0,0 +1,11 @@
+module Cantaloupe
+
+  def self.get_pathname(identifier)
+    nil
+  end
+
+  def self.get_url(identifier)
+    nil
+  end
+
+end
