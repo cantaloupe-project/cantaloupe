0a2536f3c42ee1cabf5fce7676361f7ec4eebd3c
Alex Dolski <alexd@n-ary.net>
2016-02-11 09:16:54 -0600

    Name and prioritize the config watcher thread

--- src/main/java/edu/illinois/library/cantaloupe/Application.java
+++ src/main/java/edu/illinois/library/cantaloupe/Application.java
@@ -212,6 +212,8 @@ public class Application {
                     }
                 }
             };
+            configWatcher.setPriority(Thread.MIN_PRIORITY);
+            configWatcher.setName("ConfigurationWatcher");
             configWatcher.start();
         }
 
