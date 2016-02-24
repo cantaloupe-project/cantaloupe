3bade6900d23ec739910c86a7c04fdcbbfcbbe02
Alex Dolski <alexd@n-ary.net>
2016-02-24 11:05:31 -0600

    Create user manual 2.3

--- build/prep_version.rb
+++ build/prep_version.rb
@@ -7,44 +7,47 @@ require 'FileUtils'
 
 def print_usage
   puts 'Usage:
-  prep_version.rb <path to manual> <new version number>'
+  prep_version.rb <current version> <new version>'
 end
 
-if ARGV.length < 2
+if ARGV.length != 2
   print_usage
   exit
 end
 
-def update_version_in_text_in_pathname(pathname)
+def update_version_in_text_in_pathname(pathname, source_version, dest_version)
   Dir.glob(pathname + '/**/*.html').each do |file|
     text = File.read(file)
     new_contents = text.gsub(">#{source_version}<", ">#{dest_version}<").
         gsub("/#{source_version}", "/#{dest_version}").
-        gsub("/_#{source_version}/", "/_#{dest_version}/")
+        gsub("/_#{source_version}/", "/_#{dest_version}/").
+        gsub("layout: manual_#{source_version}", "layout: manual_#{dest_version}")
     File.open(file, 'w') { |file| file.puts new_contents }
   end
 end
 
-# Copy the manual
-source_pathname = ARGV[0]
+source_version = ARGV[0]
 dest_version = ARGV[1]
-source_version = source_pathname.chomp(File::SEPARATOR).
-    split(File::SEPARATOR).last
-dest_pathname = source_pathname.gsub(
-    'manual' + File::SEPARATOR + source_version,
-    'manual' + File::SEPARATOR + dest_version)
+source_pathname = "#{Dir.getwd}/website/manual/#{source_version}"
+dest_pathname = "#{Dir.getwd}/website/manual/#{dest_version}"
 
-
+# Copy the manual
 FileUtils.rm_r(dest_pathname) if File.exist?(dest_pathname)
 FileUtils.cp_r(source_pathname, dest_pathname)
-update_version_in_text_in_pathname(dest_pathname)
+update_version_in_text_in_pathname(dest_pathname, source_version, dest_version)
 
 # Copy the table of contents include
-source_pathname = '_includes/_manual/_' + source_version
-dest_pathname = source_pathname.gsub(
-    '_manual/_' + source_version,
-    '_manual/_' + dest_version)
+source_pathname = Dir.getwd + '/website/_includes/_manual/_' + source_version
+dest_pathname = Dir.getwd + '/website/_includes/_manual/_' + dest_version
 
 FileUtils.rm_r(dest_pathname) if File.exist?(dest_pathname)
 FileUtils.cp_r(source_pathname, dest_pathname)
-update_version_in_text_in_pathname(dest_pathname)
+update_version_in_text_in_pathname(dest_pathname, source_version, dest_version)
+
+# Copy the layout
+source_pathname = "#{Dir.getwd}/website/_layouts/manual_#{source_version}.html"
+dest_pathname = "#{Dir.getwd}/website/_layouts/manual_#{dest_version}.html"
+
+FileUtils.rm(dest_pathname) if File.exist?(dest_pathname)
+FileUtils.cp(source_pathname, dest_pathname)
+update_version_in_text_in_pathname(dest_pathname, source_version, dest_version)
