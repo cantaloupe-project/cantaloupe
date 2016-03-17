cantaloupe-project/cantaloupe

commit 08f70b18c42f3610d01887bd1233373913e62be3

Eliminate the static initializer

src/main/java/edu/illinois/library/cantaloupe/resolver/JdbcResolver.java
@@ -68,18 +68,6 @@ class JdbcResolver extends AbstractResolver implements StreamResolver {
 
     private static HikariDataSource dataSource;
 
-    static {
-        try (Connection connection = getConnection()) {
-            logger.info("Using {} {}", connection.getMetaData().getDriverName(),
-                    connection.getMetaData().getDriverVersion());
-            Configuration config = Application.getConfiguration();
-            logger.info("Connection string: {}",
-                    config.getString(JDBC_URL_CONFIG_KEY));
-        } catch (SQLException e) {
-            logger.error("Failed to establish a database connection", e);
-        }
-    }
-
     /**
      * @return Connection from the connection pool. Clients must
      * <code>close()</code> it when they are done with it.
@@ -88,6 +76,16 @@ class JdbcResolver extends AbstractResolver implements StreamResolver {
     public static synchronized Connection getConnection() throws SQLException {
         if (dataSource == null) {
             final Configuration config = Application.getConfiguration();
+
+            try (Connection connection = getConnection()) {
+                logger.info("Using {} {}", connection.getMetaData().getDriverName(),
+                        connection.getMetaData().getDriverVersion());
+                logger.info("Connection string: {}",
+                        config.getString(JDBC_URL_CONFIG_KEY));
+            } catch (SQLException e) {
+                logger.error("Failed to establish a database connection", e);
+            }
+
             final String connectionString = config.
                     getString(JDBC_URL_CONFIG_KEY, "");
             final int connectionTimeout = 1000 *

commit 59821d02967d41d5b6a771afab1f71742e89f9dd

Fix a regression from 08f70b1

src/main/java/edu/illinois/library/cantaloupe/resolver/JdbcResolver.java
@@ -77,15 +77,6 @@ class JdbcResolver extends AbstractResolver implements StreamResolver {
         if (dataSource == null) {
             final Configuration config = Application.getConfiguration();
 
-            try (Connection connection = getConnection()) {
-                logger.info("Using {} {}", connection.getMetaData().getDriverName(),
-                        connection.getMetaData().getDriverVersion());
-                logger.info("Connection string: {}",
-                        config.getString(JDBC_URL_CONFIG_KEY));
-            } catch (SQLException e) {
-                logger.error("Failed to establish a database connection", e);
-            }
-
             final String connectionString = config.
                     getString(JDBC_URL_CONFIG_KEY, "");
             final int connectionTimeout = 1000 *
@@ -101,6 +92,13 @@ class JdbcResolver extends AbstractResolver implements StreamResolver {
             dataSource.setPoolName("JdbcResolverPool");
             dataSource.setMaximumPoolSize(maxPoolSize);
             dataSource.setConnectionTimeout(connectionTimeout);
+
+            try (Connection connection = dataSource.getConnection()) {
+                logger.info("Using {} {}", connection.getMetaData().getDriverName(),
+                        connection.getMetaData().getDriverVersion());
+                logger.info("Connection string: {}",
+                        config.getString(JDBC_URL_CONFIG_KEY));
+            }
         }
         return dataSource.getConnection();
     }