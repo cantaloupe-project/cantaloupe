cantaloupe-project/cantaloupe

commit 26485d084d409fe0d7c6f722fb32b94550eff134

Redesign CacheWorker to run once and be stoppable

src/main/java/edu/illinois/library/cantaloupe/Application.java
@@ -23,6 +23,10 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -45,6 +49,8 @@ public class Application {
     public static final String PURGE_EXPIRED_FROM_CACHE_VM_ARGUMENT =
             "cantaloupe.cache.purge_expired";
 
+    private static ScheduledExecutorService cacheWorkerExecutorService;
+    private static ScheduledFuture<?> cacheWorkerFuture;
     private static Configuration config;
     private static Thread configWatcher;
     private static FilesystemWatcher fsWatcher;
@@ -253,8 +259,7 @@ public class Application {
         // If the cache worker is enabled, run it in a low-priority
         // background thread.
         if (getConfiguration().getBoolean(CacheWorker.ENABLED_CONFIG_KEY, false)) {
-            // Wait 10 seconds to reduce startup load.
-            CacheWorker.runInBackground(10);
+            startCacheWorker();
         }
         webServer.start();
     }
@@ -298,7 +303,17 @@ public class Application {
      * Shuts down the application in a Servlet context.
      */
     public static synchronized void shutdown() {
+        stopCacheWorker();
         stopConfigWatcher();
+    }
+
+    private static synchronized void startCacheWorker() {
+        cacheWorkerExecutorService =
+                Executors.newSingleThreadScheduledExecutor();
+        cacheWorkerFuture = cacheWorkerExecutorService.scheduleAtFixedRate(
+                new CacheWorker(), 5,
+                getConfiguration().getInt(CacheWorker.INTERVAL_CONFIG_KEY, -1),
+                TimeUnit.SECONDS);
     }
 
     private static synchronized void startConfigWatcher() {
@@ -331,6 +346,11 @@ public class Application {
             configWatcher.setName("ConfigWatcher");
             configWatcher.start();
         }
+    }
+
+    private static synchronized void stopCacheWorker() {
+        cacheWorkerFuture.cancel(true);
+        cacheWorkerExecutorService.shutdown();
     }
 
     private static synchronized void stopConfigWatcher() {

cantaloupe.properties.sample
@@ -382,7 +382,7 @@ cache.server.resolve_first = false
 # items in the background.
 cache.server.worker.enabled = false
 
-# The cache worker will wait this many seconds between purgings.
+# !! The cache worker will wait this many seconds between purgings.
 cache.server.worker.interval = 86400
 
 #----------------------------------------

src/main/java/edu/illinois/library/cantaloupe/cache/CacheWorker.java
@@ -1,11 +1,7 @@
 package edu.illinois.library.cantaloupe.cache;
 
-import edu.illinois.library.cantaloupe.Application;
-import org.apache.commons.configuration.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Purges expired items from the cache.
@@ -20,104 +16,48 @@ public class CacheWorker implements Runnable {
     public static final String INTERVAL_CONFIG_KEY =
             "cache.server.worker.interval";
 
-    private static AtomicBoolean running = new AtomicBoolean(false);
-
-    private int startupDelay = 0;
-
     /**
-     * Runs a new CacheWorker instance in a low-priority background thread.
-     *
-     * @param delay Number of seconds to wait before running.
+     * Runs one sweep of the worker.
      */
-    public static synchronized void runInBackground(int delay) {
-        if (!running.get()) {
-            CacheWorker worker = new CacheWorker();
-            worker.setStartupDelay(delay);
-            Thread workerThread = new Thread(worker);
-            workerThread.setName(worker.getClass().getSimpleName());
-            workerThread.setPriority(Thread.MIN_PRIORITY);
-            workerThread.start();
-            running.set(true);
-        }
-    }
-
-    public int getStartupDelay() {
-        return startupDelay;
-    }
-
     @Override
     public void run() {
-        final Configuration config = Application.getConfiguration();
+        // Disabled caches will be null.
+        final Cache sourceCache = CacheFactory.getSourceCache();
+        final Cache derivativeCache = CacheFactory.getDerivativeCache();
+        logger.info("Working...");
 
-        if (!config.getBoolean(ENABLED_CONFIG_KEY, false)) {
-            logger.info("Cache worker is disabled. To enable it, set {} to " +
-                    "true and restart.", ENABLED_CONFIG_KEY);
-            return;
-        }
-
-        try {
-            logger.info("Will start in {} seconds", getStartupDelay());
-            Thread.sleep(getStartupDelay() * 1000);
-        } catch (InterruptedException e) {
-            logger.error(e.getMessage(), e);
-        }
-
-        while (true) {
-            // Disabled caches will be null.
-            final Cache sourceCache = CacheFactory.getSourceCache();
-            final Cache derivativeCache = CacheFactory.getDerivativeCache();
+        if (sourceCache != null) {
+            // Purge expired items from the source cache.
             try {
-                final int interval = config.getInt(INTERVAL_CONFIG_KEY, -1);
-                if (derivativeCache != null) {
-                    logger.info("Working...");
-                    try {
-                        // Ensure the validity of the interval.
-                        if (interval < 0) {
-                            throw new IllegalArgumentException("Invalid " +
-                                    "interval (" + INTERVAL_CONFIG_KEY +
-                                    "). Aborting.");
-                        }
-                        // Purge expired items from the source cache.
-                        try {
-                            sourceCache.purgeExpired();
-                        } catch (CacheException e) {
-                            logger.error(e.getMessage());
-                        }
-                        // Clean up the source cache.
-                        try {
-                            sourceCache.cleanUp();
-                        } catch (CacheException e) {
-                            logger.error(e.getMessage());
-                        }
-                        // Purge expired items from the derivative cache.
-                        try {
-                            derivativeCache.purgeExpired();
-                        } catch (CacheException e) {
-                            logger.error(e.getMessage());
-                        }
-                        // Clean up the derivative cache.
-                        try {
-                            derivativeCache.cleanUp();
-                        } catch (CacheException e) {
-                            logger.error(e.getMessage());
-                        }
-                        logger.info("Done working.");
-                    } catch (IllegalArgumentException e) {
-                        logger.error(e.getMessage());
-                    }
-                } else {
-                    logger.info("Caching is disabled. Nothing to do.");
-                }
-                logger.info("Sleeping for {} seconds...", interval);
-                Thread.sleep(interval * 1000);
-            } catch (InterruptedException e) {
+                sourceCache.purgeExpired();
+            } catch (CacheException e) {
+                logger.error(e.getMessage());
+            }
+            // Clean up the source cache.
+            try {
+                sourceCache.cleanUp();
+            } catch (CacheException e) {
                 logger.error(e.getMessage());
             }
         }
-    }
+        if (derivativeCache != null) {
+            // Purge expired items from the derivative cache.
+            try {
+                derivativeCache.purgeExpired();
+            } catch (CacheException e) {
+                logger.error(e.getMessage());
+            }
+            // Clean up the derivative cache.
+            try {
+                derivativeCache.cleanUp();
+            } catch (CacheException e) {
+                logger.error(e.getMessage());
+            }
+        } else {
+            logger.info("Caching is disabled. Nothing to do.");
+        }
 
-    public void setStartupDelay(int delay) {
-        startupDelay = delay;
+        logger.info("Done working.");
     }
 
 }

src/main/resources/admin.vm
@@ -2132,7 +2132,7 @@
                                         <input class="form-control" type="number"
                                                name="cache.server.worker.interval"
                                                value="true" min="0"
-                                               data-requires-restart="false">
+                                               data-requires-restart="true">
                                     </td>
                                 </tr>
                             </table>

commit 7cdbd5abfe02e1d261159cf47812329c51c26664

Fix a regression in 26485d0

src/main/java/edu/illinois/library/cantaloupe/Application.java
@@ -264,7 +264,7 @@ public class Application {
         if (getConfiguration().getBoolean(CacheWorker.ENABLED_CONFIG_KEY, false)) {
             startCacheWorker();
         }
-        webServer.start();
+        getWebServer().start();
     }
 
     public static void main(String[] args) throws Exception {