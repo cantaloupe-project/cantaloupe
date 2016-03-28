cantaloupe-project/cantaloupe

commit 6029f1559395ed113ad3a3ae19717ad9c55eb599

Visibility reduction and use more try-with-resources

src/main/java/edu/illinois/library/cantaloupe/cache/AzureStorageCache.java
@@ -35,20 +35,20 @@ class AzureStorageCache implements DerivativeCache {
     private static Logger logger = LoggerFactory.
             getLogger(AzureStorageCache.class);
 
-    public static final String ACCOUNT_KEY_CONFIG_KEY =
+    static final String ACCOUNT_KEY_CONFIG_KEY =
             "AzureStorageCache.account_key";
-    public static final String ACCOUNT_NAME_CONFIG_KEY =
+    static final String ACCOUNT_NAME_CONFIG_KEY =
             "AzureStorageCache.account_name";
-    public static final String CONTAINER_NAME_CONFIG_KEY =
+    static final String CONTAINER_NAME_CONFIG_KEY =
             "AzureStorageCache.container_name";
-    public static final String OBJECT_KEY_PREFIX_CONFIG_KEY =
+    static final String OBJECT_KEY_PREFIX_CONFIG_KEY =
             "AzureStorageCache.object_key_prefix";
-    public static final String TTL_SECONDS_CONFIG_KEY =
+    static final String TTL_SECONDS_CONFIG_KEY =
             "AzureStorageCache.ttl_seconds";
 
     private static CloudBlobClient client;
 
-    public static synchronized CloudBlobClient getClientInstance() {
+    static synchronized CloudBlobClient getClientInstance() {
         if (client == null) {
             try {
                 final Configuration config = Application.getConfiguration();
@@ -75,7 +75,7 @@ class AzureStorageCache implements DerivativeCache {
         return client;
     }
 
-    public static String getContainerName() {
+    static String getContainerName() {
         final Configuration config = Application.getConfiguration();
         // All letters in a container name must be lowercase.
         return config.getString(CONTAINER_NAME_CONFIG_KEY).toLowerCase();
@@ -160,7 +160,7 @@ class AzureStorageCache implements DerivativeCache {
      * @return Object key of the serialized ImageInfo associated with the given
      *         identifier.
      */
-    public String getObjectKey(Identifier identifier) {
+    String getObjectKey(Identifier identifier) {
         try {
             return getObjectKeyPrefix() + "info/" +
                     URLEncoder.encode(identifier.toString(), "UTF-8") + ".json";
@@ -174,7 +174,7 @@ class AzureStorageCache implements DerivativeCache {
      * @param opList
      * @return Object key of the image associated with the given operation list.
      */
-    public String getObjectKey(OperationList opList) {
+    String getObjectKey(OperationList opList) {
         try {
             return getObjectKeyPrefix() + "image/" +
                     URLEncoder.encode(opList.toString(), "UTF-8");
@@ -188,7 +188,7 @@ class AzureStorageCache implements DerivativeCache {
      * @return Value of {@link #OBJECT_KEY_PREFIX_CONFIG_KEY} with trailing
      * slash.
      */
-    public String getObjectKeyPrefix() {
+    String getObjectKeyPrefix() {
         String prefix = Application.getConfiguration().
                 getString(OBJECT_KEY_PREFIX_CONFIG_KEY);
         if (prefix.length() < 1 || prefix.equals("/")) {
@@ -296,7 +296,6 @@ class AzureStorageCache implements DerivativeCache {
         final CloudBlobClient client = getClientInstance();
         final String objectKey = getObjectKey(identifier);
 
-        OutputStream os = null;
         try {
             final CloudBlobContainer container =
                     client.getContainerReference(containerName);
@@ -304,18 +303,11 @@ class AzureStorageCache implements DerivativeCache {
             blob.getProperties().setContentType("application/json");
             blob.getProperties().setContentEncoding("UTF-8");
 
-            os = blob.openOutputStream();
-            imageInfo.writeAsJson(os);
+            try (OutputStream os = blob.openOutputStream()) {
+                imageInfo.writeAsJson(os);
+            }
         } catch (IOException | URISyntaxException | StorageException e) {
             throw new CacheException(e.getMessage(), e);
-        } finally {
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (IOException e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
         }
     }
 }

src/main/java/edu/illinois/library/cantaloupe/cache/AmazonS3Cache.java
@@ -67,10 +67,10 @@ class AmazonS3Cache implements DerivativeCache {
         private final String objectKey;
         private final AmazonS3 s3;
 
-        public AmazonS3OutputStream(final AmazonS3 s3,
-                                    final String bucketName,
-                                    final String objectKey,
-                                    final ObjectMetadata metadata) {
+        AmazonS3OutputStream(final AmazonS3 s3,
+                             final String bucketName,
+                             final String objectKey,
+                             final ObjectMetadata metadata) {
             this.bucketName = bucketName;
             this.s3 = s3;
             this.objectKey = objectKey;
@@ -121,23 +121,20 @@ class AmazonS3Cache implements DerivativeCache {
 
     private static Logger logger = LoggerFactory.getLogger(AmazonS3Cache.class);
 
-    public static final String ACCESS_KEY_ID_CONFIG_KEY =
+    static final String ACCESS_KEY_ID_CONFIG_KEY =
             "AmazonS3Cache.access_key_id";
-    public static final String BUCKET_NAME_CONFIG_KEY =
-            "AmazonS3Cache.bucket.name";
-    public static final String BUCKET_REGION_CONFIG_KEY =
+    static final String BUCKET_NAME_CONFIG_KEY = "AmazonS3Cache.bucket.name";
+    static final String BUCKET_REGION_CONFIG_KEY =
             "AmazonS3Cache.bucket.region";
-    public static final String OBJECT_KEY_PREFIX_CONFIG_KEY =
+    static final String OBJECT_KEY_PREFIX_CONFIG_KEY =
             "AmazonS3Cache.object_key_prefix";
-    public static final String SECRET_KEY_CONFIG_KEY =
-            "AmazonS3Cache.secret_key";
-    public static final String TTL_SECONDS_CONFIG_KEY =
-            "AmazonS3Cache.ttl_seconds";
+    static final String SECRET_KEY_CONFIG_KEY = "AmazonS3Cache.secret_key";
+    static final String TTL_SECONDS_CONFIG_KEY = "AmazonS3Cache.ttl_seconds";
 
     /** Lazy-initialized by {@link #getClientInstance} */
     private static AmazonS3 client;
 
-    public static synchronized AmazonS3 getClientInstance() {
+    static synchronized AmazonS3 getClientInstance() {
         if (client == null) {
             class ConfigFileCredentials implements AWSCredentials {
                 @Override
@@ -174,7 +171,7 @@ class AmazonS3Cache implements DerivativeCache {
     @Override
     public void cleanUp() {}
 
-    public String getBucketName() {
+    String getBucketName() {
         return Application.getConfiguration().getString(BUCKET_NAME_CONFIG_KEY);
     }
 
@@ -241,7 +238,7 @@ class AmazonS3Cache implements DerivativeCache {
      * @return Object key of the serialized ImageInfo associated with the given
      *         identifier.
      */
-    public String getObjectKey(Identifier identifier) {
+    String getObjectKey(Identifier identifier) {
         return getObjectKeyPrefix() + "info/" + identifier.toString() + ".json";
     }
 
@@ -249,7 +246,7 @@ class AmazonS3Cache implements DerivativeCache {
      * @param opList
      * @return Object key of the image associated with the given operation list.
      */
-    public String getObjectKey(OperationList opList) {
+    String getObjectKey(OperationList opList) {
         return getObjectKeyPrefix() + "image/" + opList.toString();
     }
 
@@ -257,7 +254,7 @@ class AmazonS3Cache implements DerivativeCache {
      * @return Value of {@link #OBJECT_KEY_PREFIX_CONFIG_KEY} with trailing
      * slash.
      */
-    public String getObjectKeyPrefix() {
+    String getObjectKeyPrefix() {
         String prefix = Application.getConfiguration().
                 getString(OBJECT_KEY_PREFIX_CONFIG_KEY);
         if (prefix.length() < 1 || prefix.equals("/")) {

src/main/java/edu/illinois/library/cantaloupe/cache/FilesystemCache.java
@@ -64,7 +64,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
         private final PathMatcher matcher;
         private int numDeleted = 0;
 
-        public CacheCleaner() {
+        CacheCleaner() {
             matcher = FileSystems.getDefault()
                     .getPathMatcher("glob:*" + TEMP_EXTENSION);
         }
@@ -132,10 +132,10 @@ class FilesystemCache implements SourceCache, DerivativeCache {
          * @throws FileNotFoundException
          */
         @SuppressWarnings("unchecked")
-        public ConcurrentFileOutputStream(File tempFile,
-                                          File destinationFile,
-                                          Set imagesBeingWritten,
-                                          Object toRemove)
+        ConcurrentFileOutputStream(File tempFile,
+                                   File destinationFile,
+                                   Set imagesBeingWritten,
+                                   Object toRemove)
                 throws FileNotFoundException {
             super(tempFile);
             imagesBeingWritten.add(toRemove);
@@ -158,7 +158,11 @@ class FilesystemCache implements SourceCache, DerivativeCache {
             } finally {
                 logger.debug("Closing stream for {}", toRemove);
                 imagesBeingWritten.remove(toRemove);
-                super.close();
+                try {
+                    super.close();
+                } catch (IOException e) {
+                    logger.info(e.getMessage(), e);
+                }
             }
         }
 
@@ -187,12 +191,12 @@ class FilesystemCache implements SourceCache, DerivativeCache {
     private static final Logger logger = LoggerFactory.
             getLogger(FilesystemCache.class);
 
-    public static final String DIRECTORY_DEPTH_CONFIG_KEY =
+    static final String DIRECTORY_DEPTH_CONFIG_KEY =
             "FilesystemCache.dir.depth";
-    public static final String DIRECTORY_NAME_LENGTH_CONFIG_KEY =
+    static final String DIRECTORY_NAME_LENGTH_CONFIG_KEY =
             "FilesystemCache.dir.name_length";
-    public static final String PATHNAME_CONFIG_KEY = "FilesystemCache.pathname";
-    public static final String TTL_CONFIG_KEY = "FilesystemCache.ttl_seconds";
+    static final String PATHNAME_CONFIG_KEY = "FilesystemCache.pathname";
+    static final String TTL_CONFIG_KEY = "FilesystemCache.ttl_seconds";
 
     private static final short FILENAME_MAX_LENGTH = 255;
     // https://en.wikipedia.org/wiki/Filename#Comparison_of_filename_limitations
@@ -258,7 +262,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @param inputString String to make filename-safe
      * @return Filename-safe string
      */
-    public static String filenameSafe(String inputString) {
+    static String filenameSafe(String inputString) {
         final StringBuffer sb = new StringBuffer();
         final Matcher matcher = FILENAME_SAFE_PATTERN.matcher(inputString);
 
@@ -279,7 +283,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @return Directory path composed of fragments of a hash of the given
      * string.
      */
-    public static String getHashedStringBasedSubdirectory(String uniqueString) {
+    static String getHashedStringBasedSubdirectory(String uniqueString) {
         final StringBuilder path = new StringBuilder();
         try {
             // Use a fast algo. Collisions don't matter.
@@ -404,7 +408,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @param ops Operation list identifying the file.
      * @return File corresponding to the given operation list.
      */
-    public File getDerivativeImageFile(OperationList ops) throws CacheException {
+    File getDerivativeImageFile(OperationList ops) throws CacheException {
         final List<String> parts = new ArrayList<>();
         final String cacheRoot =
                 StringUtils.stripEnd(getRootDerivativeImagePathname(), File.separator);
@@ -432,12 +436,12 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      *         given identifier.
      * @throws CacheException
      */
-    public Collection<File> getDerivativeImageFiles(Identifier identifier)
+    Collection<File> getDerivativeImageFiles(Identifier identifier)
             throws CacheException {
         class IdentifierFilter implements FilenameFilter {
             private Identifier identifier;
 
-            public IdentifierFilter(Identifier identifier) {
+            private IdentifierFilter(Identifier identifier) {
                 this.identifier = identifier;
             }
 
@@ -459,8 +463,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @return Temp file corresponding to the given operation list. Clients
      * should delete it when they are done with it.
      */
-    public File getDerivativeImageTempFile(OperationList ops)
-            throws CacheException {
+    File getDerivativeImageTempFile(OperationList ops) throws CacheException {
         return new File(getDerivativeImageFile(ops).getAbsolutePath() +
                 TEMP_EXTENSION);
     }
@@ -640,7 +643,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @param identifier
      * @return File corresponding to the given parameters.
      */
-    public File getInfoFile(final Identifier identifier) throws CacheException {
+    File getInfoFile(final Identifier identifier) throws CacheException {
         final String cacheRoot =
                 StringUtils.stripEnd(getRootInfoPathname(), File.separator);
         final String subfolderPath = StringUtils.stripEnd(
@@ -652,8 +655,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
         return new File(pathname);
     }
 
-    public File getInfoTempFile(final Identifier identifier)
-            throws CacheException {
+    File getInfoTempFile(final Identifier identifier) throws CacheException {
         return new File(getInfoFile(identifier).getAbsolutePath() +
                 TEMP_EXTENSION);
     }
@@ -664,7 +666,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @param identifier Identifier identifying the file.
      * @return File corresponding to the given identifier.
      */
-    public File getSourceImageFile(Identifier identifier) throws CacheException {
+    File getSourceImageFile(Identifier identifier) throws CacheException {
         final String cacheRoot = StringUtils.stripEnd(
                 getRootSourceImagePathname(), File.separator);
         final String subfolderPath = StringUtils.stripEnd(
@@ -681,8 +683,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
      * @return Temp file corresponding to a source image with the given
      * identifier. Clients should delete it when they are done with it.
      */
-    public File getSourceImageTempFile(Identifier identifier)
-            throws CacheException {
+    File getSourceImageTempFile(Identifier identifier) throws CacheException {
         return new File(getSourceImageFile(identifier).getAbsolutePath() +
                 TEMP_EXTENSION);
     }

src/main/java/edu/illinois/library/cantaloupe/cache/JdbcCache.java
@@ -63,7 +63,7 @@ class JdbcCache implements DerivativeCache {
          * @param ops Derivative image operation list
          * @throws SQLException
          */
-        public ImageBlobOutputStream(Connection conn, OperationList ops)
+        ImageBlobOutputStream(Connection conn, OperationList ops)
                 throws SQLException {
             this.connection = conn;
             this.ops = ops;
@@ -124,28 +124,25 @@ class JdbcCache implements DerivativeCache {
     private static final Logger logger = LoggerFactory.
             getLogger(JdbcCache.class);
 
-    public static final String DERIVATIVE_IMAGE_TABLE_IMAGE_COLUMN =
-            "image";
-    public static final String DERIVATIVE_IMAGE_TABLE_LAST_ACCESSED_COLUMN =
+    static final String DERIVATIVE_IMAGE_TABLE_IMAGE_COLUMN = "image";
+    static final String DERIVATIVE_IMAGE_TABLE_LAST_ACCESSED_COLUMN =
             "last_accessed";
-    public static final String DERIVATIVE_IMAGE_TABLE_OPERATIONS_COLUMN =
-            "operations";
+    static final String DERIVATIVE_IMAGE_TABLE_OPERATIONS_COLUMN = "operations";
 
-    public static final String INFO_TABLE_IDENTIFIER_COLUMN = "identifier";
-    public static final String INFO_TABLE_INFO_COLUMN = "info";
-    public static final String INFO_TABLE_LAST_ACCESSED_COLUMN = "last_accessed";
+    static final String INFO_TABLE_IDENTIFIER_COLUMN = "identifier";
+    static final String INFO_TABLE_INFO_COLUMN = "info";
+    static final String INFO_TABLE_LAST_ACCESSED_COLUMN = "last_accessed";
 
-    public static final String CONNECTION_TIMEOUT_CONFIG_KEY =
+    static final String CONNECTION_TIMEOUT_CONFIG_KEY =
             "JdbcCache.connection_timeout";
-    public static final String DERIVATIVE_IMAGE_TABLE_CONFIG_KEY =
+    static final String DERIVATIVE_IMAGE_TABLE_CONFIG_KEY =
             "JdbcCache.derivative_image_table";
-    public static final String JDBC_URL_CONFIG_KEY = "JdbcCache.url";
-    public static final String PASSWORD_CONFIG_KEY = "JdbcCache.password";
-    public static final String INFO_TABLE_CONFIG_KEY = "JdbcCache.info_table";
-    public static final String MAX_POOL_SIZE_CONFIG_KEY =
-            "JdbcCache.max_pool_size";
-    public static final String TTL_CONFIG_KEY = "JdbcCache.ttl_seconds";
-    public static final String USER_CONFIG_KEY = "JdbcCache.user";
+    static final String JDBC_URL_CONFIG_KEY = "JdbcCache.url";
+    static final String PASSWORD_CONFIG_KEY = "JdbcCache.password";
+    static final String INFO_TABLE_CONFIG_KEY = "JdbcCache.info_table";
+    static final String MAX_POOL_SIZE_CONFIG_KEY = "JdbcCache.max_pool_size";
+    static final String TTL_CONFIG_KEY = "JdbcCache.ttl_seconds";
+    static final String USER_CONFIG_KEY = "JdbcCache.user";
 
     private static HikariDataSource dataSource;
 
@@ -198,7 +195,7 @@ class JdbcCache implements DerivativeCache {
      * @return Name of the derivative image table.
      * @throws CacheException If the image table name is not set.
      */
-    public static String getDerivativeImageTableName() throws CacheException {
+    static String getDerivativeImageTableName() throws CacheException {
         final String name = Application.getConfiguration().
                 getString(DERIVATIVE_IMAGE_TABLE_CONFIG_KEY);
         if (name == null) {
@@ -212,7 +209,7 @@ class JdbcCache implements DerivativeCache {
      * @return Name of the image info table.
      * @throws CacheException If the info table name is not set.
      */
-    public static String getInfoTableName() throws CacheException {
+    static String getInfoTableName() throws CacheException {
         final String name = Application.getConfiguration().
                 getString(INFO_TABLE_CONFIG_KEY);
         if (name == null) {
@@ -368,7 +365,7 @@ class JdbcCache implements DerivativeCache {
         return new Timestamp(now.getTime());
     }
 
-    public Timestamp oldestValidDate() {
+    Timestamp oldestValidDate() {
         final Configuration config = Application.getConfiguration();
         final long ttl = config.getLong(TTL_CONFIG_KEY, 0);
         if (ttl > 0) {

src/main/java/edu/illinois/library/cantaloupe/processor/FfmpegProcessor.java
@@ -12,7 +12,6 @@ import edu.illinois.library.cantaloupe.image.Scale;
 import edu.illinois.library.cantaloupe.image.Transpose;
 import edu.illinois.library.cantaloupe.image.watermark.Watermark;
 import edu.illinois.library.cantaloupe.resource.iiif.ProcessorFeature;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,7 +49,7 @@ class FfmpegProcessor extends AbstractProcessor implements FileProcessor {
     private static Logger logger = LoggerFactory.
             getLogger(FfmpegProcessor.class);
 
-    public static final String PATH_TO_BINARIES_CONFIG_KEY =
+    static final String PATH_TO_BINARIES_CONFIG_KEY =
             "FfmpegProcessor.path_to_binaries";
 
     private static final Set<ProcessorFeature> SUPPORTED_FEATURES =
@@ -131,7 +130,7 @@ class FfmpegProcessor extends AbstractProcessor implements FileProcessor {
                 case RIGHT_CENTER:
                     return String.format(
                             "main_w-overlay_w-%d:(main_h-overlay_h)/2",
-                            edgeMargin, edgeMargin);
+                            edgeMargin);
                 case CENTER:
                     return "(main_w-overlay_w)/2:(main_h-overlay_h)/2";
             }
@@ -274,28 +273,24 @@ class FfmpegProcessor extends AbstractProcessor implements FileProcessor {
             logger.info("Executing {}", StringUtils.join(pb.command(), " "));
             final Process process = pb.start();
 
-            InputStream processInputStream = process.getInputStream();
-            InputStream processErrorStream = process.getErrorStream();
-            OutputStream processOutputStream = process.getOutputStream();
-
-            executorService.submit(
-                    new StreamCopier(processInputStream, outputStream));
-            executorService.submit(
-                    new StreamCopier(processErrorStream, errorBucket));
-            try {
-                int code = process.waitFor();
-                if (code != 0) {
-                    logger.error("ffmpeg returned with code {}", code);
-                    final String errorStr = errorBucket.toString();
-                    if (errorStr != null && errorStr.length() > 0) {
-                        throw new ProcessorException(errorStr);
+            try (final InputStream processInputStream = process.getInputStream();
+                 final InputStream processErrorStream = process.getErrorStream()) {
+                executorService.submit(
+                        new StreamCopier(processInputStream, outputStream));
+                executorService.submit(
+                        new StreamCopier(processErrorStream, errorBucket));
+                try {
+                    int code = process.waitFor();
+                    if (code != 0) {
+                        logger.error("ffmpeg returned with code {}", code);
+                        final String errorStr = errorBucket.toString();
+                        if (errorStr != null && errorStr.length() > 0) {
+                            throw new ProcessorException(errorStr);
+                        }
                     }
+                } finally {
+                    process.destroy();
                 }
-            } finally {
-                processInputStream.close();
-                processOutputStream.close();
-                processErrorStream.close();
-                process.destroy();
             }
         } catch (IOException | InterruptedException e) {
             String msg = e.getMessage();

src/main/java/edu/illinois/library/cantaloupe/processor/GraphicsMagickProcessor.java
@@ -47,9 +47,9 @@ class GraphicsMagickProcessor extends AbstractProcessor
     private static Logger logger = LoggerFactory.
             getLogger(GraphicsMagickProcessor.class);
 
-    public static final String BACKGROUND_COLOR_CONFIG_KEY =
+    static final String BACKGROUND_COLOR_CONFIG_KEY =
             "GraphicsMagickProcessor.background_color";
-    public static final String PATH_TO_BINARIES_CONFIG_KEY =
+    static final String PATH_TO_BINARIES_CONFIG_KEY =
             "GraphicsMagickProcessor.path_to_binaries";
 
     private static final Set<ProcessorFeature> SUPPORTED_FEATURES =
@@ -125,9 +125,8 @@ class GraphicsMagickProcessor extends AbstractProcessor
             try {
                 logger.info("Executing {}", commandString);
                 final Process process = pb.start();
-                final InputStream processInputStream = process.getInputStream();
 
-                try {
+                try (final InputStream processInputStream = process.getInputStream()) {
                     BufferedReader stdInput = new BufferedReader(
                             new InputStreamReader(processInputStream));
                     String s;
@@ -171,8 +170,6 @@ class GraphicsMagickProcessor extends AbstractProcessor
                     //outputFormats.add(OutputFormat.GIF);
                 } catch (InterruptedException e) {
                     logger.error(e.getMessage());
-                } finally {
-                    processInputStream.close();
                 }
             } catch (IOException e) {
                 logger.error(e.getMessage());
@@ -200,9 +197,7 @@ class GraphicsMagickProcessor extends AbstractProcessor
         if (getAvailableOutputFormats().size() < 1) {
             throw new UnsupportedSourceFormatException(format);
         }
-        InputStream inputStream = null;
-        try {
-            inputStream = streamSource.newInputStream();
+        try (InputStream inputStream = streamSource.newInputStream()) {
             Info sourceInfo = new Info(
                     format.getPreferredExtension() + ":-",
                     inputStream, true);
@@ -211,14 +206,6 @@ class GraphicsMagickProcessor extends AbstractProcessor
                     sourceInfo.getImageHeight(), getSourceFormat());
         } catch (IM4JavaException | IOException e) {
             throw new ProcessorException(e.getMessage(), e);
-        } finally {
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-            } catch (IOException e) {
-                logger.error(e.getMessage(), e);
-            }
         }
     }
 
@@ -283,13 +270,10 @@ class GraphicsMagickProcessor extends AbstractProcessor
                 convert.setSearchPath(binaryPath);
             }
 
-            InputStream inputStream = streamSource.newInputStream();
-            try {
+            try (InputStream inputStream = streamSource.newInputStream()) {
                 convert.setInputProvider(new Pipe(inputStream, null));
                 convert.setOutputConsumer(new Pipe(null, outputStream));
                 convert.run(op);
-            } finally {
-                inputStream.close();
             }
         } catch (InterruptedException | IM4JavaException | IOException e) {
             throw new ProcessorException(e.getMessage(), e);

commit 8f7b6e426696fc9db198d9feb55f0f709e3152d7

Fix a regression from 6029f15

src/main/java/edu/illinois/library/cantaloupe/cache/AzureStorageCache.java
@@ -295,6 +295,7 @@ class AzureStorageCache implements DerivativeCache {
         final CloudBlobClient client = getClientInstance();
         final String objectKey = getObjectKey(identifier);
 
+        OutputStream os = null;
         try {
             final CloudBlobContainer container =
                     client.getContainerReference(containerName);
@@ -302,11 +303,18 @@ class AzureStorageCache implements DerivativeCache {
             blob.getProperties().setContentType("application/json");
             blob.getProperties().setContentEncoding("UTF-8");
 
-            try (OutputStream os = blob.openOutputStream()) {
-                imageInfo.writeAsJson(os);
-            }
+            os = blob.openOutputStream();
+            imageInfo.writeAsJson(os);
         } catch (IOException | URISyntaxException | StorageException e) {
             throw new CacheException(e.getMessage(), e);
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
         }
     }
 }