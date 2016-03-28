cantaloupe-project/cantaloupe

commit 1b92565aa073dc8d5ae3e1958998bdc9cca16759

Move WebServer.getWarLocation() to StandaloneEntry.getWarFile()

src/main/java/edu/illinois/library/cantaloupe/StandaloneEntry.java
@@ -1,5 +1,10 @@
 package edu.illinois.library.cantaloupe;
 
+
+import java.io.File;
+import java.net.URL;
+import java.security.ProtectionDomain;
+
 /**
  * <p>Serves as the main application class in a standalone context.</p>
  */
@@ -16,6 +21,13 @@ public class StandaloneEntry {
      */
     public static void main(String[] args) throws Exception {
         getWebServer().start();
+    }
+
+    static File getWarFile() {
+        ProtectionDomain protectionDomain =
+                WebServer.class.getProtectionDomain();
+        URL location = protectionDomain.getCodeSource().getLocation();
+        return new File(location.toExternalForm());
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/WebServer.java
@@ -11,8 +11,6 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-import java.net.URL;
-import java.security.ProtectionDomain;
 import java.util.Arrays;
 
 public class WebServer {
@@ -86,13 +84,6 @@ public class WebServer {
         return httpsPort;
     }
 
-    private String getWarLocation() {
-        ProtectionDomain protectionDomain =
-                WebServer.class.getProtectionDomain();
-        URL location = protectionDomain.getCodeSource().getLocation();
-        return location.toExternalForm();
-    }
-
     public boolean isHttpEnabled() {
         return httpEnabled;
     }
@@ -139,7 +130,7 @@ public class WebServer {
 
         // If we are running from a WAR file, tell Jetty to load the war file
         // via a WebAppContext. Otherwise, use a ServletContextHandler.
-        final String warLocation = getWarLocation();
+        final String warLocation = StandaloneEntry.getWarFile().getName();
         if (warLocation.endsWith(".war")) {
             WebAppContext context = new WebAppContext();
             context.setServer(server);

commit ee1994a803a0227606970a13b99494dece67fcf1

Fix a regression from 1b92565

src/main/java/edu/illinois/library/cantaloupe/StandaloneEntry.java
@@ -42,7 +42,7 @@ public class StandaloneEntry {
         ProtectionDomain protectionDomain =
                 WebServer.class.getProtectionDomain();
         URL location = protectionDomain.getCodeSource().getLocation();
-        return new File(location.toExternalForm());
+        return new File(location.getFile());
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/WebServer.java
@@ -137,8 +137,8 @@ public class WebServer {
 
         // If we are running from a WAR file, tell Jetty to load the war file
         // via a WebAppContext. Otherwise, use a ServletContextHandler.
-        final String warLocation = StandaloneEntry.getWarFile().getName();
-        if (warLocation.endsWith(".war")) {
+        final String warPath = StandaloneEntry.getWarFile().getAbsolutePath();
+        if (warPath.endsWith(".war")) {
             WebAppContext context = new WebAppContext();
             context.setServer(server);
             context.setConfigurationClasses(Arrays.asList(
@@ -148,7 +148,7 @@ public class WebServer {
                     "org.eclipse.jetty.webapp.FragmentConfiguration",
                     "org.eclipse.jetty.webapp.JettyWebXmlConfiguration"));
             context.setContextPath("/");
-            context.setWar(warLocation);
+            context.setWar(warPath);
             server.setHandler(context);
         } else {
             ServletContextHandler context = new ServletContextHandler(