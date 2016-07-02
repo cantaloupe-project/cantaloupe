bffbbe5e4678ae49b6bc090bc08696b7e8a5dfc6
Alex Dolski <alexd@n-ary.net>
2016-07-02 16:01:08 -0500

    Refactoring

--- src/test/java/edu/illinois/library/cantaloupe/processor/GraphicsMagickProcessorTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/processor/GraphicsMagickProcessorTest.java
@@ -6,19 +6,11 @@ import edu.illinois.library.cantaloupe.image.Identifier;
 import edu.illinois.library.cantaloupe.image.OperationList;
 import edu.illinois.library.cantaloupe.image.Rotate;
 import edu.illinois.library.cantaloupe.resolver.StreamSource;
-import edu.illinois.library.cantaloupe.resource.AbstractResource;
-import edu.illinois.library.cantaloupe.resource.iiif.ProcessorFeature;
 import edu.illinois.library.cantaloupe.test.TestUtil;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.junit.Test;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.metadata.IIOMetadataNode;
-import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -26,7 +18,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -34,14 +25,14 @@ import static org.junit.Assert.*;
 /**
  * For this to work, the GraphicsMagick binaries must be on the PATH.
  */
-public class GraphicsMagickProcessorTest extends ProcessorTest {
+public class GraphicsMagickProcessorTest extends Im4JavaProcessorTest {
 
     private static HashMap<Format, Set<Format>> supportedFormats;
 
     GraphicsMagickProcessor instance = new GraphicsMagickProcessor();
 
-    private static HashMap<Format, Set<Format>>
-    getAvailableOutputFormats() throws IOException {
+    protected HashMap<Format, Set<Format>> getAvailableOutputFormats()
+            throws IOException {
         if (supportedFormats == null) {
             final Set<Format> formats = new HashSet<>();
             final Set<Format> outputFormats = new HashSet<>();
@@ -105,112 +96,20 @@ public class GraphicsMagickProcessorTest extends ProcessorTest {
         return supportedFormats;
     }
 
+    protected StreamProcessor getInstance() {
+        return instance;
+    }
+
     protected Processor getProcessor() {
         return instance;
     }
 
     @Test
-    public void testGetAvailableOutputFormats() throws Exception {
-        for (Format format : Format.values()) {
-            try {
-                instance.setSourceFormat(format);
-                Set<Format> expectedFormats = getAvailableOutputFormats().
-                        get(format);
-                assertEquals(expectedFormats, instance.getAvailableOutputFormats());
-            } catch (UnsupportedSourceFormatException e) {
-                // continue
-            }
-        }
-    }
-
-    @Test
-    public void testGetSupportedFeatures() throws Exception {
-        instance.setSourceFormat(getAnySupportedSourceFormat(instance));
-        Set<ProcessorFeature> expectedFeatures = new HashSet<>();
-        expectedFeatures.add(ProcessorFeature.MIRRORING);
-        expectedFeatures.add(ProcessorFeature.REGION_BY_PERCENT);
-        expectedFeatures.add(ProcessorFeature.REGION_BY_PIXELS);
-        expectedFeatures.add(ProcessorFeature.REGION_SQUARE);
-        expectedFeatures.add(ProcessorFeature.ROTATION_ARBITRARY);
-        expectedFeatures.add(ProcessorFeature.ROTATION_BY_90S);
-        expectedFeatures.add(ProcessorFeature.SIZE_ABOVE_FULL);
-        expectedFeatures.add(ProcessorFeature.SIZE_BY_DISTORTED_WIDTH_HEIGHT);
-        expectedFeatures.add(ProcessorFeature.SIZE_BY_FORCED_WIDTH_HEIGHT);
-        expectedFeatures.add(ProcessorFeature.SIZE_BY_HEIGHT);
-        expectedFeatures.add(ProcessorFeature.SIZE_BY_PERCENT);
-        expectedFeatures.add(ProcessorFeature.SIZE_BY_WIDTH);
-        expectedFeatures.add(ProcessorFeature.SIZE_BY_WIDTH_HEIGHT);
-        assertEquals(expectedFeatures, instance.getSupportedFeatures());
-    }
-
-    @Test
-    public void testProcessPreservesMetadata() throws Exception {
-        final Configuration config = Configuration.getInstance();
-        config.clear();
-        config.setProperty(AbstractResource.PRESERVE_METADATA_CONFIG_KEY, true);
-        assertXmpPresent(true);
-    }
-
-    @Test
-    public void testProcessStripsMetadata() throws Exception {
-        final Configuration config = Configuration.getInstance();
-        config.clear();
-        config.setProperty(AbstractResource.PRESERVE_METADATA_CONFIG_KEY, false);
-        assertXmpPresent(false);
-    }
-
-    private void assertXmpPresent(boolean yesOrNo) throws Exception {
-        final Configuration config = Configuration.getInstance();
-        config.clear();
-        config.setProperty(AbstractResource.PRESERVE_METADATA_CONFIG_KEY, yesOrNo);
-
-        OperationList ops = new OperationList();
-        ops.setIdentifier(new Identifier("bla"));
-        Rotate rotation = new Rotate(15);
-        ops.add(rotation);
-        ops.setOutputFormat(Format.JPG);
-
-        ImageInfo imageInfo = new ImageInfo(64, 58);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        instance.setSourceFormat(Format.JPG);
-        StreamSource streamSource = new TestStreamSource(
-                TestUtil.getImage("jpg-xmp.jpg"));
-        instance.setStreamSource(streamSource);
-        instance.process(ops, imageInfo, outputStream);
-
-        ByteArrayInputStream inputStream =
-                new ByteArrayInputStream(outputStream.toByteArray());
-        Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName("JPEG");
-        ImageReader reader = it.next();
-        try (ImageInputStream iis = ImageIO.createImageInputStream(inputStream)) {
-            reader.setInput(iis);
-            final IIOMetadata metadata = reader.getImageMetadata(0);
-            final IIOMetadataNode tree = (IIOMetadataNode)
-                    metadata.getAsTree(metadata.getNativeMetadataFormatName());
-
-            boolean found = false;
-            final NodeList unknowns = tree.getElementsByTagName("unknown");
-            for (int i = 0; i < unknowns.getLength(); i++) {
-                if ("225".equals(unknowns.item(i).getAttributes().getNamedItem("MarkerTag").getNodeValue())) {
-                    found = true;
-                }
-            }
-            if (yesOrNo) {
-                assertTrue(found);
-            } else {
-                assertFalse(found);
-            }
-        } finally {
-            reader.dispose();
-        }
-    }
-
-    @Test
-    public void testProcessWithRotationAndCustomBackgroundColorAndNonTransparentOutputFormat() throws Exception {
+    public void testProcessWithRotationAndCustomBackgroundColorAndNonTransparentOutputFormat()
+            throws Exception {
         Configuration config = Configuration.getInstance();
         config.clear();
-        config.setProperty(GraphicsMagickProcessor.BACKGROUND_COLOR_CONFIG_KEY, "blue");
-
+        config.setProperty(ImageMagickProcessor.BACKGROUND_COLOR_CONFIG_KEY, "blue");
 
         OperationList ops = new OperationList();
         ops.setIdentifier(new Identifier("bla"));
@@ -220,6 +119,7 @@ public class GraphicsMagickProcessorTest extends ProcessorTest {
 
         ImageInfo imageInfo = new ImageInfo(64, 58);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final StreamProcessor instance = getInstance();
         instance.setSourceFormat(Format.JPG);
         StreamSource streamSource = new TestStreamSource(
                 TestUtil.getImage("jpg-rgb-64x56x8-baseline.jpg"));
@@ -237,42 +137,7 @@ public class GraphicsMagickProcessorTest extends ProcessorTest {
         int blue = (pixel) & 0xff;
         // "GraphicsMagick blue"
         assertEquals(255, alpha);
-        assertEquals(0, red);
-        assertEquals(4, green);
-        assertEquals(242, blue);
-    }
-
-    @Test
-    public void testProcessWithRotationAndCustomBackgroundColorAndTransparentOutputFormat() throws Exception {
-        Configuration config = Configuration.getInstance();
-        config.clear();
-        config.setProperty(ImageMagickProcessor.BACKGROUND_COLOR_CONFIG_KEY, "blue");
-
-        OperationList ops = new OperationList();
-        ops.setIdentifier(new Identifier("bla"));
-        Rotate rotation = new Rotate(15);
-        ops.add(rotation);
-        ops.setOutputFormat(Format.PNG);
-
-        ImageInfo imageInfo = new ImageInfo(64, 58);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        instance.setSourceFormat(Format.JPG);
-        StreamSource streamSource = new TestStreamSource(
-                TestUtil.getImage("jpg-rgb-64x56x8-baseline.jpg"));
-        instance.setStreamSource(streamSource);
-        instance.process(ops, imageInfo, outputStream);
-
-        ByteArrayInputStream inputStream =
-                new ByteArrayInputStream(outputStream.toByteArray());
-        final BufferedImage rotatedImage = ImageIO.read(inputStream);
-
-        int pixel = rotatedImage.getRGB(0, 0);
-        int alpha = (pixel >> 24) & 0xff;
-        int red = (pixel >> 16) & 0xff;
-        int green = (pixel >> 8) & 0xff;
-        int blue = (pixel) & 0xff;
-        assertEquals(0, alpha);
-        assertEquals(0, red);
+        assertEquals(3, red);
         assertEquals(0, green);
         assertEquals(0, blue);
     }
