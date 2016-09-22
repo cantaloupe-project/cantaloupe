13aed89ab348886b655751aaa442503f4928dcaf
Alex Dolski <alexd@n-ary.net>
2016-09-22 10:38:10 -0500

    Fix failing to decode the identifier in doPurge()

--- src/main/java/edu/illinois/library/cantaloupe/resource/api/ApiResource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/api/ApiResource.java
@@ -6,6 +6,7 @@ import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.image.Identifier;
 import edu.illinois.library.cantaloupe.resource.AbstractResource;
 import edu.illinois.library.cantaloupe.resource.EndpointDisabledException;
+import org.restlet.data.Reference;
 import org.restlet.representation.EmptyRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Delete;
@@ -33,7 +34,8 @@ public class ApiResource extends AbstractResource {
         if (cache != null) {
             final String idStr = (String) this.getRequest().getAttributes().
                     get("identifier");
-            final Identifier identifier = new Identifier(idStr);
+            final Identifier identifier =
+                    new Identifier(decodeSlashes(Reference.decode(idStr)));
 
             cache.purgeImage(identifier);
         }
