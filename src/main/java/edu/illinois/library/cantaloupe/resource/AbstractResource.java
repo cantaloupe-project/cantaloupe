f63a07684cdef9ba9f33ee523d9af1f6e7c541ee
Alex Dolski <alexd@n-ary.net>
2016-10-20 13:28:00 -0500

    Log the request URI in doInit()

--- src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -179,6 +179,7 @@ public abstract class AbstractResource extends ServerResource {
         super.doInit();
         getResponse().getHeaders().add("X-Powered-By",
                 "Cantaloupe/" + Application.getVersion());
+        logger.info("doInit(): handling {}", getReference());
     }
 
     /**
