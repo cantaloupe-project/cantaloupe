b47156497762f586f4b0aea04c2745e21fe981ae
Alex Dolski <alexd@n-ary.net>
2016-10-26 18:47:08 -0500

    Add a helpful message

--- src/main/java/edu/illinois/library/cantaloupe/script/DelegateScriptDisabledException.java
+++ src/main/java/edu/illinois/library/cantaloupe/script/DelegateScriptDisabledException.java
@@ -1,4 +1,12 @@
 package edu.illinois.library.cantaloupe.script;
 
 public class DelegateScriptDisabledException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "The delegate script is disabled (" +
+                ScriptEngineFactory.DELEGATE_SCRIPT_ENABLED_CONFIG_KEY +
+                " = false)";
+    }
+
 }
