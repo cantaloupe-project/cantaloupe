6a35998301c878254903645a9481d2a4c29f1e2b
Alex Dolski <alexd@n-ary.net>
2016-11-04 20:14:16 -0500

    Remove isCompatible()

--- src/main/java/edu/illinois/library/cantaloupe/resolver/Resolver.java
+++ src/main/java/edu/illinois/library/cantaloupe/resolver/Resolver.java
@@ -2,9 +2,6 @@ package edu.illinois.library.cantaloupe.resolver;
 
 import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.image.Identifier;
-import edu.illinois.library.cantaloupe.processor.FileProcessor;
-import edu.illinois.library.cantaloupe.processor.Processor;
-import edu.illinois.library.cantaloupe.processor.StreamProcessor;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -27,23 +24,6 @@ public interface Resolver {
      * @throws IOException if there is some other issue accessing the image
      */
     Format getSourceFormat() throws IOException;
-
-    /**
-     * <dl>
-     *     <dt>{@link FileResolver} &rarr; {@link StreamProcessor}</dt>
-     *     <dd>OK, using {@link java.io.FileInputStream}</dd>
-     *     <dt>{@link FileResolver} &rarr; {@link FileProcessor}</dt>
-     *     <dd>OK, using {@link java.io.File}</dd>
-     *     <dt>{@link StreamResolver} &rarr; {@link StreamProcessor}</dt>
-     *     <dd>OK, using {@link java.io.InputStream}</dd>
-     *     <dt>{@link StreamResolver} &rarr; {@link FileProcessor}</dt>
-     *     <dd>Incompatible</dd>
-     * </dl>
-     *
-     * @param processor
-     * @return Whether the instance is compatible with the given processor.
-     */
-    boolean isCompatible(Processor processor);
 
     /**
      * @param identifier Identifier of a source image to resolve.
