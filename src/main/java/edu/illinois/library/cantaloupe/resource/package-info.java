32c2eca94523b32ffefcf9f3db1b1c1364ded48c
Alex Dolski <alexd@n-ary.net>
2016-11-26 12:53:59 -0600

    Add e.i.l.c.resource.package-info.java

---
+++ src/main/java/edu/illinois/library/cantaloupe/resource/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>Package for RESTful Restlet resources.</p>
+ *
+ * <p>Additional resources for the various major application components are
+ * present in subpackages.</p>
+ */
+package edu.illinois.library.cantaloupe.resource;
