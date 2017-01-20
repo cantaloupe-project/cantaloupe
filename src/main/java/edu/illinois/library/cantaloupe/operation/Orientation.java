911569c45aeb2f12b1c30a5cc0558b43658b2b13
Alex Dolski <alexd@n-ary.net>
2017-01-20 14:44:18 -0600

    Improve documentation

--- src/main/java/edu/illinois/library/cantaloupe/operation/Orientation.java
+++ src/main/java/edu/illinois/library/cantaloupe/operation/Orientation.java
@@ -1,10 +1,35 @@
 package edu.illinois.library.cantaloupe.operation;
 
+/**
+ * <p>Signifies an image orientation.</p>
+ *
+ * <p>Mainly this is used in support of images whose orientation may be
+ * different from that of their pixel data. It aligns with the EXIF Orientation
+ * tag, although it currently supports only rotation and not flipping.
+ */
 public enum Orientation {
 
+    /**
+     * No rotation. (EXIF Orientation value 1)
+     */
     ROTATE_0(0),
+
+    /**
+     * The image is rotated counter-clockwise (or, the view is rotated
+     * clockwise) 90 degrees. (EXIF Orientation value 6; "right top")
+     */
     ROTATE_90(90),
+
+    /**
+     * The image is rotated 180 degrees. (EXIF Orientation value 3; "bottom
+     * right")
+     */
     ROTATE_180(180),
+
+    /**
+     * The image is rotated counter-clockwise (or, the view is rotated
+     * clockwise) 270 degrees. (EXIF Orientation value 8; "left bottom")
+     */
     ROTATE_270(270);
 
     private int degrees;
