219f52a22edc8c0653df23875cbfceb221d88a82
Alex Dolski <alexd@n-ary.net>
2017-01-21 12:17:33 -0600

    Use TikaInputStream rather than InputStream, as it is likely more efficient

--- src/main/java/edu/illinois/library/cantaloupe/util/MediaTypeUtil.java
+++ src/main/java/edu/illinois/library/cantaloupe/util/MediaTypeUtil.java
@@ -1,35 +1,33 @@
 package edu.illinois.library.cantaloupe.util;
 
 import org.apache.tika.detect.Detector;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.restlet.data.MediaType;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MediaTypeUtil {
 
     public List<MediaType> detectMediaTypes(File file) throws IOException {
-        return detectMediaTypes(file.getAbsolutePath());
-    }
-
-    public List<MediaType> detectMediaTypes(String pathname) throws IOException {
         final List<MediaType> types = new ArrayList<>();
-        try (InputStream is = new BufferedInputStream(new FileInputStream(pathname))) {
+        try (TikaInputStream is = TikaInputStream.get(file)) {
             AutoDetectParser parser = new AutoDetectParser();
             Detector detector = parser.getDetector();
             Metadata md = new Metadata();
-            md.add(Metadata.RESOURCE_NAME_KEY, pathname);
+            md.add(Metadata.RESOURCE_NAME_KEY, file.getAbsolutePath());
             org.apache.tika.mime.MediaType mediaType = detector.detect(is, md);
             types.add(new MediaType(mediaType.toString()));
         }
         return types;
+    }
+
+    public List<MediaType> detectMediaTypes(String pathname) throws IOException {
+        return detectMediaTypes(new File(pathname));
     }
 
 }
