5a82f9fea9b4c19ba310cad803584e90b183f889
Alex Dolski <alexd@n-ary.net>
2017-02-28 12:02:52 -0600

    Add package-info.java

---
+++ src/main/java/edu/illinois/library/cantaloupe/operation/overlay/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>Package for classes related to string and image overlays.</p>
+ *
+ * <p>The main public class of interest is
+ * {@link edu.illinois.library.cantaloupe.operation.overlay.OverlayService},
+ * which is used to acquire an
+ * {@link edu.illinois.library.cantaloupe.operation.overlay.Overlay}
+ * appropriate for a given request based on the configuration and (possibly)
+ * the return value of a delegate method.</p>
+ */
+package edu.illinois.library.cantaloupe.operation.overlay;
