426a558014821559baaea1ed5d2606866c7868ae
Alex Dolski <alexd@n-ary.net>
2017-03-31 10:24:04 -0500

    Minor refactoring and removing the last traces of EnvironmentConfiguration

--- src/main/java/edu/illinois/library/cantaloupe/logging/LoggerUtil.java
+++ src/main/java/edu/illinois/library/cantaloupe/logging/LoggerUtil.java
@@ -3,9 +3,7 @@ package edu.illinois.library.cantaloupe.logging;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.util.StatusPrinter;
-import edu.illinois.library.cantaloupe.Application;
 import edu.illinois.library.cantaloupe.config.Configuration;
-import edu.illinois.library.cantaloupe.config.ConfigurationFactory;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
@@ -17,7 +15,7 @@ public abstract class LoggerUtil {
      * Reloads the Logback configuration from logback.xml.
      */
     public static synchronized void reloadConfiguration() {
-        Configuration appConfig = ConfigurationFactory.getInstance();
+        Configuration appConfig = Configuration.getInstance();
         if (appConfig != null) {
             // Reset the logger context.
             LoggerContext loggerContext = (LoggerContext)
@@ -27,17 +25,15 @@ public abstract class LoggerUtil {
             loggerContext.reset();
             // Copy logging-related configuration key/values into logger
             // context properties.
-            Iterator it = appConfig.getKeys();
+            final Iterator it = appConfig.getKeys();
             while (it.hasNext()) {
-                String key = (String) it.next();
-                // The second condition works with EnvironmentConfiguration;
-                // the first works with all other configurations.
-                if (key.startsWith("log.") || key.contains("_LOG_")) {
+                final String key = (String) it.next();
+                if (key.startsWith("log.")) {
                     loggerContext.putProperty(key, appConfig.getString(key));
                 }
             }
             // Finally, reload the Logback configuration.
-            try (InputStream stream = Application.class.getClassLoader().
+            try (InputStream stream = LoggerUtil.class.getClassLoader().
                     getResourceAsStream("logback.xml")) {
                 jc.doConfigure(stream);
             } catch (Exception e) {
