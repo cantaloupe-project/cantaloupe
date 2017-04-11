aa26c2de6a4ec1f59f704ee01ff30a71bfab47e9
Alex Dolski <alexd@n-ary.net>
2017-04-11 11:37:51 -0500

    Redesign anonymous callback as inner class (backported from e62d57cf6968c197c147401f867495be47ca8a17)

--- src/main/java/edu/illinois/library/cantaloupe/config/ConfigurationWatcher.java
+++ src/main/java/edu/illinois/library/cantaloupe/config/ConfigurationWatcher.java
@@ -3,6 +3,7 @@ package edu.illinois.library.cantaloupe.config;
 import edu.illinois.library.cantaloupe.logging.LoggerUtil;
 import edu.illinois.library.cantaloupe.util.FilesystemWatcher;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 
@@ -12,35 +13,47 @@ import java.nio.file.Path;
  */
 class ConfigurationWatcher implements Runnable {
 
+    private static class CallbackImpl implements FilesystemWatcher.Callback {
+
+        @Override
+        public void created(Path path) {
+            handle(path);
+        }
+
+        @Override
+        public void deleted(Path path) {}
+
+        @Override
+        public void modified(Path path) {
+            handle(path);
+        }
+
+        private void handle(Path path) {
+            final Configuration config = Configuration.getInstance();
+            if (path.toFile().equals(config.getFile())) {
+                try {
+                    config.reload();
+                    LoggerUtil.reloadConfiguration();
+                } catch (FileNotFoundException e) {
+                    System.err.println("ConfigurationWatcher$CallbackImpl: " +
+                            "file not found: " + e.getMessage());
+                } catch (IOException e) {
+                    System.err.println("ConfigurationWatcher$CallbackImpl: " +
+                            e.getMessage());
+                }
+            }
+        }
+
+    }
+
     private FilesystemWatcher filesystemWatcher;
 
     @Override
     public void run() {
-        final Configuration config = ConfigurationFactory.getInstance();
-        FilesystemWatcher.Callback callback = new FilesystemWatcher.Callback() {
-            @Override
-            public void created(Path path) { handle(path); }
-
-            @Override
-            public void deleted(Path path) {}
-
-            @Override
-            public void modified(Path path) { handle(path); }
-
-            private void handle(Path path) {
-                if (path.toFile().equals(config.getFile())) {
-                    try {
-                        config.reload();
-                        LoggerUtil.reloadConfiguration();
-                    } catch (IOException e) {
-                        System.err.println("ConfigurationWatcher.run(): " + e.getMessage());
-                    }
-                }
-            }
-        };
+        final Configuration config = Configuration.getInstance();
         try {
             Path path = config.getFile().toPath().getParent();
-            filesystemWatcher = new FilesystemWatcher(path, callback);
+            filesystemWatcher = new FilesystemWatcher(path, new CallbackImpl());
             filesystemWatcher.processEvents();
         } catch (IOException e) {
             System.err.println("ConfigurationWatcher.run(): " + e.getMessage());
--- src/main/java/edu/illinois/library/cantaloupe/script/ScriptWatcher.java
+++ src/main/java/edu/illinois/library/cantaloupe/script/ScriptWatcher.java
@@ -14,47 +14,51 @@ import java.nio.file.Path;
  */
 class ScriptWatcher implements Runnable {
 
+    private static class CallbackImpl implements FilesystemWatcher.Callback {
+
+        private Logger logger = LoggerFactory.getLogger(CallbackImpl.class);
+
+        @Override
+        public void created(Path path) {
+            handle(path);
+        }
+
+        @Override
+        public void deleted(Path path) {}
+
+        @Override
+        public void modified(Path path) {
+            handle(path);
+        }
+
+        private void handle(Path path) {
+            try {
+                if (path.toFile().equals(ScriptEngineFactory.getScriptFile())) {
+                    try {
+                        ScriptEngineFactory.getScriptEngine().
+                                load(FileUtils.readFileToString(path.toFile()));
+                    } catch (DelegateScriptDisabledException e) {
+                        logger.debug(e.getMessage());
+                    } catch (Exception e) {
+                        logger.error(e.getMessage());
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                logger.error("File not found: {}", e.getMessage());
+            }
+        }
+
+    }
+
     private static Logger logger = LoggerFactory.getLogger(ScriptWatcher.class);
 
     private FilesystemWatcher filesystemWatcher;
 
     @Override
     public void run() {
-
-        FilesystemWatcher.Callback callback = new FilesystemWatcher.Callback() {
-
-            private Logger logger = LoggerFactory.
-                    getLogger(FilesystemWatcher.Callback.class);
-
-            @Override
-            public void created(Path path) { handle(path); }
-
-            @Override
-            public void deleted(Path path) {}
-
-            @Override
-            public void modified(Path path) { handle(path); }
-
-            private void handle(Path path) {
-                try {
-                    if (path.toFile().equals(ScriptEngineFactory.getScriptFile())) {
-                        try {
-                            ScriptEngineFactory.getScriptEngine().
-                                    load(FileUtils.readFileToString(path.toFile()));
-                        } catch(DelegateScriptDisabledException e) {
-                            logger.debug("handle(): {}", e.getMessage());
-                        } catch (Exception e) {
-                            logger.error("run(): {}", e.getMessage());
-                        }
-                    }
-                } catch (FileNotFoundException e) {
-                    logger.error("run(): {}", e.getMessage());
-                }
-            }
-        };
         try {
             Path path = ScriptEngineFactory.getScriptFile().toPath().getParent();
-            filesystemWatcher = new FilesystemWatcher(path, callback);
+            filesystemWatcher = new FilesystemWatcher(path, new CallbackImpl());
             filesystemWatcher.processEvents();
         } catch (IOException e) {
             logger.error("run(): {}", e.getMessage());
