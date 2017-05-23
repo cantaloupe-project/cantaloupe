cantaloupe-project/cantaloupe

commit 5a6e4ed14de5ef36d54e50ece158b91fff0ab206

Centralize configuration keys in e.i.l.c.config.Key

src/main/java/edu/illinois/library/cantaloupe/operation/overlay/BasicStringOverlayService.java
@@ -3,6 +3,7 @@ package edu.illinois.library.cantaloupe.operation.overlay;
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.ConfigurationException;
 import edu.illinois.library.cantaloupe.config.ConfigurationFactory;
+import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.operation.Color;
 
 import java.awt.Font;
@@ -11,27 +12,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 class BasicStringOverlayService extends BasicOverlayService {
-
-    static final String BACKGROUND_COLOR_CONFIG_KEY =
-            "overlays.BasicStrategy.string.background.color";
-    static final String COLOR_CONFIG_KEY =
-            "overlays.BasicStrategy.string.color";
-    static final String FONT_CONFIG_KEY =
-            "overlays.BasicStrategy.string.font";
-    static final String FONT_MIN_SIZE_CONFIG_KEY =
-            "overlays.BasicStrategy.string.font.min_size";
-    static final String FONT_SIZE_CONFIG_KEY =
-            "overlays.BasicStrategy.string.font.size";
-    static final String FONT_WEIGHT_CONFIG_KEY =
-            "overlays.BasicStrategy.string.font.weight";
-    static final String GLYPH_SPACING_CONFIG_KEY =
-            "overlays.BasicStrategy.string.glyph_spacing";
-    static final String STRING_CONFIG_KEY =
-            "overlays.BasicStrategy.string";
-    static final String STROKE_COLOR_CONFIG_KEY =
-            "overlays.BasicStrategy.string.stroke.color";
-    static final String STROKE_WIDTH_CONFIG_KEY =
-            "overlays.BasicStrategy.string.stroke.width";
 
     private Color backgroundColor;
     private Color color;
@@ -56,35 +36,35 @@ class BasicStringOverlayService extends BasicOverlayService {
 
         // Background color
         backgroundColor = Color.fromString(
-                config.getString(BACKGROUND_COLOR_CONFIG_KEY));
+                config.getString(Key.OVERLAY_STRING_BACKGROUND_COLOR));
 
         // Fill color
-        color = Color.fromString(config.getString(COLOR_CONFIG_KEY));
+        color = Color.fromString(config.getString(Key.OVERLAY_STRING_COLOR));
 
         // Font
         final Map<TextAttribute, Object> attributes = new HashMap<>();
         attributes.put(TextAttribute.FAMILY,
-                config.getString(FONT_CONFIG_KEY, "Helvetica"));
+                config.getString(Key.OVERLAY_STRING_FONT, "Helvetica"));
         attributes.put(TextAttribute.SIZE,
-                config.getInt(FONT_SIZE_CONFIG_KEY, 18));
+                config.getInt(Key.OVERLAY_STRING_FONT_SIZE, 18));
         attributes.put(TextAttribute.WEIGHT,
-                config.getFloat(FONT_WEIGHT_CONFIG_KEY, 1f));
+                config.getFloat(Key.OVERLAY_STRING_FONT_WEIGHT, 1f));
         attributes.put(TextAttribute.TRACKING,
-                config.getFloat(GLYPH_SPACING_CONFIG_KEY, 0f));
+                config.getFloat(Key.OVERLAY_STRING_GLYPH_SPACING, 0f));
         font = Font.getFont(attributes);
 
         // Min size
-        minSize = config.getInt(FONT_MIN_SIZE_CONFIG_KEY, 14);
+        minSize = config.getInt(Key.OVERLAY_STRING_FONT_MIN_SIZE, 14);
 
         // String
-        string = config.getString(STRING_CONFIG_KEY, "");
+        string = config.getString(Key.OVERLAY_STRING_STRING, "");
 
         // Stroke color
         strokeColor = Color.fromString(
-                config.getString(STROKE_COLOR_CONFIG_KEY, "black"));
+                config.getString(Key.OVERLAY_STRING_COLOR, "black"));
 
         // Stroke width
-        strokeWidth = config.getFloat(STROKE_WIDTH_CONFIG_KEY, 2f);
+        strokeWidth = config.getFloat(Key.OVERLAY_STRING_STROKE_WIDTH, 2f);
     }
 
 }

src/test/java/edu/illinois/library/cantaloupe/operation/overlay/BasicStringOverlayServiceTest.java
@@ -2,6 +2,7 @@ package edu.illinois.library.cantaloupe.operation.overlay;
 
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.ConfigurationFactory;
+import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.test.BaseTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,21 +19,21 @@ public class BasicStringOverlayServiceTest extends BaseTest {
 
     public static void setUpConfiguration() {
         Configuration config = ConfigurationFactory.getInstance();
-        config.setProperty(OverlayService.ENABLED_CONFIG_KEY, true);
-        config.setProperty(OverlayService.STRATEGY_CONFIG_KEY, "BasicStrategy");
-        config.setProperty(BasicOverlayService.TYPE_CONFIG_KEY, "string");
-        config.setProperty(BasicOverlayService.INSET_CONFIG_KEY, 10);
-        config.setProperty(BasicOverlayService.POSITION_CONFIG_KEY, "top left");
-        config.setProperty(BasicStringOverlayService.BACKGROUND_COLOR_CONFIG_KEY, "rgba(12, 23, 34, 45)");
-        config.setProperty(BasicStringOverlayService.COLOR_CONFIG_KEY, "red");
-        config.setProperty(BasicStringOverlayService.FONT_CONFIG_KEY, "Helvetica");
-        config.setProperty(BasicStringOverlayService.FONT_MIN_SIZE_CONFIG_KEY, 11);
-        config.setProperty(BasicStringOverlayService.FONT_SIZE_CONFIG_KEY, 14);
-        config.setProperty(BasicStringOverlayService.FONT_WEIGHT_CONFIG_KEY, 2f);
-        config.setProperty(BasicStringOverlayService.GLYPH_SPACING_CONFIG_KEY, 0.2f);
-        config.setProperty(BasicStringOverlayService.STRING_CONFIG_KEY, "cats");
-        config.setProperty(BasicStringOverlayService.STROKE_COLOR_CONFIG_KEY, "blue");
-        config.setProperty(BasicStringOverlayService.STROKE_WIDTH_CONFIG_KEY, 3);
+        config.setProperty(Key.OVERLAY_ENABLED, true);
+        config.setProperty(Key.OVERLAY_STRATEGY, "BasicStrategy");
+        config.setProperty(Key.OVERLAY_TYPE, "string");
+        config.setProperty(Key.OVERLAY_INSET, 10);
+        config.setProperty(Key.OVERLAY_POSITION, "top left");
+        config.setProperty(Key.OVERLAY_STRING_BACKGROUND_COLOR, "rgba(12, 23, 34, 45)");
+        config.setProperty(Key.OVERLAY_STRING_COLOR, "red");
+        config.setProperty(Key.OVERLAY_STRING_FONT, "Helvetica");
+        config.setProperty(Key.OVERLAY_STRING_FONT_MIN_SIZE, 11);
+        config.setProperty(Key.OVERLAY_STRING_FONT_SIZE, 14);
+        config.setProperty(Key.OVERLAY_STRING_FONT_WEIGHT, 2f);
+        config.setProperty(Key.OVERLAY_STRING_GLYPH_SPACING, 0.2f);
+        config.setProperty(Key.OVERLAY_STRING_STRING, "cats");
+        config.setProperty(Key.OVERLAY_STRING_STROKE_COLOR, "blue");
+        config.setProperty(Key.OVERLAY_STRING_STROKE_WIDTH, 3);
     }
 
     @Before
@@ -69,23 +70,23 @@ public class BasicStringOverlayServiceTest extends BaseTest {
         final Dimension imageSize = new Dimension(100, 100);
 
         // image width > width threshold, image height > height threshold
-        config.setProperty(BasicOverlayService.OUTPUT_WIDTH_THRESHOLD_CONFIG_KEY, 50);
-        config.setProperty(BasicOverlayService.OUTPUT_HEIGHT_THRESHOLD_CONFIG_KEY, 50);
+        config.setProperty(Key.OVERLAY_OUTPUT_WIDTH_THRESHOLD, 50);
+        config.setProperty(Key.OVERLAY_OUTPUT_HEIGHT_THRESHOLD, 50);
         assertTrue(BasicStringOverlayService.shouldApplyToImage(imageSize));
 
         // image width < width threshold, image height < height threshold
-        config.setProperty(BasicOverlayService.OUTPUT_WIDTH_THRESHOLD_CONFIG_KEY, 200);
-        config.setProperty(BasicOverlayService.OUTPUT_HEIGHT_THRESHOLD_CONFIG_KEY, 200);
+        config.setProperty(Key.OVERLAY_OUTPUT_WIDTH_THRESHOLD, 200);
+        config.setProperty(Key.OVERLAY_OUTPUT_HEIGHT_THRESHOLD, 200);
         assertFalse(BasicStringOverlayService.shouldApplyToImage(imageSize));
 
         // image width < width threshold, image height > height threshold
-        config.setProperty(BasicOverlayService.OUTPUT_WIDTH_THRESHOLD_CONFIG_KEY, 200);
-        config.setProperty(BasicOverlayService.OUTPUT_HEIGHT_THRESHOLD_CONFIG_KEY, 50);
+        config.setProperty(Key.OVERLAY_OUTPUT_WIDTH_THRESHOLD, 200);
+        config.setProperty(Key.OVERLAY_OUTPUT_HEIGHT_THRESHOLD, 50);
         assertFalse(BasicStringOverlayService.shouldApplyToImage(imageSize));
 
         // image width > width threshold, image height < height threshold
-        config.setProperty(BasicOverlayService.OUTPUT_WIDTH_THRESHOLD_CONFIG_KEY, 50);
-        config.setProperty(BasicOverlayService.OUTPUT_HEIGHT_THRESHOLD_CONFIG_KEY, 200);
+        config.setProperty(Key.OVERLAY_OUTPUT_WIDTH_THRESHOLD, 50);
+        config.setProperty(Key.OVERLAY_OUTPUT_HEIGHT_THRESHOLD, 200);
         assertFalse(BasicStringOverlayService.shouldApplyToImage(imageSize));
     }
 

commit ff5812f486477bf2c02064febf28b28a0fb67452

Fix stroke color returned by getOverlay() (regression introduced in 5a6e4ed14de5ef36d54e50ece158b91fff0ab206)

src/main/java/edu/illinois/library/cantaloupe/operation/overlay/BasicStringOverlayService.java
@@ -61,7 +61,7 @@ class BasicStringOverlayService extends BasicOverlayService {
 
         // Stroke color
         strokeColor = Color.fromString(
-                config.getString(Key.OVERLAY_STRING_COLOR, "black"));
+                config.getString(Key.OVERLAY_STRING_STROKE_COLOR, "black"));
 
         // Stroke width
         strokeWidth = config.getFloat(Key.OVERLAY_STRING_STROKE_WIDTH, 2f);

src/test/java/edu/illinois/library/cantaloupe/operation/overlay/BasicStringOverlayServiceTest.java
@@ -3,11 +3,11 @@ package edu.illinois.library.cantaloupe.operation.overlay;
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.ConfigurationFactory;
 import edu.illinois.library.cantaloupe.config.Key;
+import edu.illinois.library.cantaloupe.operation.Color;
 import edu.illinois.library.cantaloupe.test.BaseTest;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.font.TextAttribute;
 
@@ -32,7 +32,7 @@ public class BasicStringOverlayServiceTest extends BaseTest {
         config.setProperty(Key.OVERLAY_STRING_FONT_WEIGHT, 2f);
         config.setProperty(Key.OVERLAY_STRING_GLYPH_SPACING, 0.2f);
         config.setProperty(Key.OVERLAY_STRING_STRING, "cats");
-        config.setProperty(Key.OVERLAY_STRING_STROKE_COLOR, "blue");
+        config.setProperty(Key.OVERLAY_STRING_STROKE_COLOR, "orange");
         config.setProperty(Key.OVERLAY_STRING_STROKE_WIDTH, 3);
     }
 
@@ -58,7 +58,7 @@ public class BasicStringOverlayServiceTest extends BaseTest {
         assertEquals(11, overlay.getMinSize());
         assertEquals(2f, overlay.getFont().getAttributes().get(TextAttribute.WEIGHT));
         assertEquals(0.2f, overlay.getFont().getAttributes().get(TextAttribute.TRACKING));
-        assertEquals(Color.blue, overlay.getStrokeColor());
+        assertEquals(Color.ORANGE, overlay.getStrokeColor());
         assertEquals(3, overlay.getStrokeWidth(), 0.00001f);
     }
 