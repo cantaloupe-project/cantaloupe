2dbd7f39c66ce6b5227c70fdb5da0cec3d9d8f0e
Alex Dolski <alexd@n-ary.net>
2017-06-21 14:00:16 -0500

    Remove e.i.l.c.resource.iiif.IIIFResource

--- src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/IIIF2Resource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/IIIF2Resource.java
@@ -2,11 +2,11 @@ package edu.illinois.library.cantaloupe.resource.iiif.v2;
 
 import edu.illinois.library.cantaloupe.config.ConfigurationFactory;
 import edu.illinois.library.cantaloupe.config.Key;
+import edu.illinois.library.cantaloupe.resource.AbstractResource;
 import edu.illinois.library.cantaloupe.resource.EndpointDisabledException;
-import edu.illinois.library.cantaloupe.resource.iiif.IIIFResource;
 import org.restlet.resource.ResourceException;
 
-abstract class IIIF2Resource extends IIIFResource {
+abstract class IIIF2Resource extends AbstractResource {
 
     @Override
     protected void doInit() throws ResourceException {
