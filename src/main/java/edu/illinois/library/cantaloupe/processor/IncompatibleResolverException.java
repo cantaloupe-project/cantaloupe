db38d80cb584d79e8e2499e2b10b5bfb58374263
Alex Dolski <alexd@n-ary.net>
2017-08-22 14:16:04 -0500

    Make package-protected

--- src/main/java/edu/illinois/library/cantaloupe/processor/IncompatibleResolverException.java
+++ src/main/java/edu/illinois/library/cantaloupe/processor/IncompatibleResolverException.java
@@ -3,10 +3,10 @@ package edu.illinois.library.cantaloupe.processor;
 import edu.illinois.library.cantaloupe.config.ConfigurationException;
 import edu.illinois.library.cantaloupe.resolver.Resolver;
 
-public class IncompatibleResolverException extends ConfigurationException {
+class IncompatibleResolverException extends ConfigurationException {
 
-    public IncompatibleResolverException(final Resolver resolver,
-                                         final Processor processor) {
+    IncompatibleResolverException(final Resolver resolver,
+                                  final Processor processor) {
         super(String.format("%s is not compatible with %s",
                 processor.getClass().getSimpleName(),
                 resolver.getClass().getSimpleName()));
