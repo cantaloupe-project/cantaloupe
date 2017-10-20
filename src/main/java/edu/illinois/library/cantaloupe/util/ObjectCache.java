84e86c1f662323a1dc0e10cb8467a58f83cf9515
Alex Dolski <alexd@n-ary.net>
2017-10-20 14:51:59 -0500

    Remove logging

--- src/main/java/edu/illinois/library/cantaloupe/util/ObjectCache.java
+++ src/main/java/edu/illinois/library/cantaloupe/util/ObjectCache.java
@@ -2,16 +2,11 @@ package edu.illinois.library.cantaloupe.util;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Size-bounded heap cache.
  */
 public final class ObjectCache<K, V> {
-
-    private static final Logger LOGGER = LoggerFactory.
-            getLogger(ObjectCache.class);
 
     // This is thread-safe.
     private Cache<K, V> store;
@@ -20,7 +15,6 @@ public final class ObjectCache<K, V> {
      * Creates an instance with the given max size.
      */
     public ObjectCache(long maxSize) {
-        LOGGER.info("{} limit: {}", getClass().getSimpleName(), maxSize);
         store = Caffeine.newBuilder().maximumSize(maxSize).build();
     }
 
