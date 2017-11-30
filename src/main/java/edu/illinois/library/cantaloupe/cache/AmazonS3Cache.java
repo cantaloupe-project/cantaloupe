948b3f6c192aa85536d370a6028d2eb8f09f542a
Alex Dolski <alexd@n-ary.net>
2017-11-30 11:46:44 -0600

    Fix AmazonS3Cache failing to traverse truncated ObjectListings

--- src/main/java/edu/illinois/library/cantaloupe/cache/AmazonS3Cache.java
+++ src/main/java/edu/illinois/library/cantaloupe/cache/AmazonS3Cache.java
@@ -5,7 +5,6 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.iterable.S3Objects;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
@@ -289,13 +288,25 @@ class AmazonS3Cache implements DerivativeCache {
     @Override
     public void purge() throws CacheException {
         final AmazonS3 s3 = getClientInstance();
-        final ObjectListing listing = s3.listObjects(getBucketName(),
+
+        ObjectListing listing = s3.listObjects(getBucketName(),
                 getObjectKeyPrefix());
         int count = 0;
-        for (S3ObjectSummary summary : listing.getObjectSummaries()) {
-            s3.deleteObject(getBucketName(), summary.getKey());
-            count++;
+
+        while (true) {
+            for (S3ObjectSummary summary : listing.getObjectSummaries()) {
+                s3.deleteObject(getBucketName(), summary.getKey());
+                count++;
+            }
+
+            if (listing.isTruncated()) {
+                logger.debug("purge(): retrieving next batch");
+                listing = s3.listNextBatchOfObjects(listing);
+            } else {
+                break;
+            }
         }
+
         logger.info("purge(): deleted {} items", count);
     }
 
@@ -319,16 +330,27 @@ class AmazonS3Cache implements DerivativeCache {
         c.add(Calendar.SECOND, 0 - config.getInt(Cache.TTL_CONFIG_KEY));
         Date cutoffDate = c.getTime();
 
-        final S3Objects objects = S3Objects.withPrefix(s3, bucketName,
+        ObjectListing listing = s3.listObjects(getBucketName(),
                 getObjectKeyPrefix());
         int count = 0, deletedCount = 0;
-        for (S3ObjectSummary summary : objects) {
-            count++;
-            if (summary.getLastModified().before(cutoffDate)) {
-                deletedCount++;
-                s3.deleteObject(bucketName, summary.getKey());
+
+        while (true) {
+            for (S3ObjectSummary summary : listing.getObjectSummaries()) {
+                count++;
+                if (summary.getLastModified().before(cutoffDate)) {
+                    s3.deleteObject(bucketName, summary.getKey());
+                    deletedCount++;
+                }
+            }
+
+            if (listing.isTruncated()) {
+                logger.debug("purgeExpired(): retrieving next batch");
+                listing = s3.listNextBatchOfObjects(listing);
+            } else {
+                break;
             }
         }
+
         logger.info("purgeExpired(): deleted {} of {} items",
                 deletedCount, count);
     }
@@ -341,13 +363,25 @@ class AmazonS3Cache implements DerivativeCache {
         // purge images
         final AmazonS3 s3 = getClientInstance();
         final String bucketName = getBucketName();
-        final S3Objects objects = S3Objects.withPrefix(s3, bucketName,
+
+        ObjectListing listing = s3.listObjects(getBucketName(),
                 getObjectKeyPrefix() + "image/" + identifier.toString());
         int count = 0;
-        for (S3ObjectSummary summary : objects) {
-            count++;
-            s3.deleteObject(bucketName, summary.getKey());
+
+        while (true) {
+            for (S3ObjectSummary summary : listing.getObjectSummaries()) {
+                s3.deleteObject(bucketName, summary.getKey());
+                count++;
+            }
+
+            if (listing.isTruncated()) {
+                logger.debug("purge(Identifier): retrieving next batch");
+                listing = s3.listNextBatchOfObjects(listing);
+            } else {
+                break;
+            }
         }
+
         logger.info("purge(Identifier): deleted {} items", count);
     }
 
