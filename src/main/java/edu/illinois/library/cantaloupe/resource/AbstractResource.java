cantaloupe-project/cantaloupe

commit d9c75436393bc5c5b126c1d648b7307b5991788b

Minor refactoring (no functionality changes)

src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -39,6 +39,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -230,7 +231,7 @@ public abstract class AbstractResource extends ServerResource {
             final java.awt.Dimension fullSize)
             throws IOException, ScriptException, AccessDeniedException {
         final Authorizer authorizer = new Authorizer(getReference().toString(),
-                getCanonicalClientIpAddress(),
+                getCanonicalClientIPAddress(),
                 getRequest().getHeaders().getValuesMap(),
                 getRequest().getCookies().getValuesMap());
         final AuthInfo info = authorizer.authorize(opList, fullSize);
@@ -269,7 +270,7 @@ public abstract class AbstractResource extends ServerResource {
     private String decodeSlashes(final String uriPathComponent) {
         final String substitute = Configuration.getInstance().
                 getString(Key.SLASH_SUBSTITUTE, "");
-        if (substitute.length() > 0) {
+        if (!substitute.isEmpty()) {
             return StringUtils.replace(uriPathComponent, substitute, "/");
         }
         return uriPathComponent;
@@ -335,7 +336,7 @@ public abstract class AbstractResource extends ServerResource {
      * @return Best guess at the user agent's IP address, respecting the
      *         <code>X-Forwarded-For</code> request header, if present.
      */
-    protected String getCanonicalClientIpAddress() {
+    protected String getCanonicalClientIPAddress() {
         String addr;
         // The value is supposed to be in the format: "client, proxy1, proxy2"
         final String forwardedFor =
@@ -364,7 +365,7 @@ public abstract class AbstractResource extends ServerResource {
         // Decode slash substitutes.
         final String identifier = decodeSlashes(decodedIdentifier);
 
-        LOGGER.debug("getIdentifier(): requested: {} / decoded: {} / " +
+        LOGGER.debug("Identifier requested: {} -> decoded: {} -> " +
                         "slashes substituted: {}",
                 urlIdentifier, decodedIdentifier, identifier);
 
@@ -403,10 +404,10 @@ public abstract class AbstractResource extends ServerResource {
         final String urlID = (String) attrs.get("identifier");
         final String decodedID = Reference.decode(urlID);
         final String reSlashedID = decodeSlashes(decodedID);
-        final String headerID = getRequest().getHeaders().getFirstValue(
-                PUBLIC_IDENTIFIER_HEADER, true);
+        final String headerID = getRequest().getHeaders().
+                getFirstValue(PUBLIC_IDENTIFIER_HEADER, true);
 
-        LOGGER.debug("Identifier requested: {} -> decoded: {} -> " +
+        LOGGER.debug("Public identifier requested: {} -> decoded: {} -> " +
                         "slashes substituted: {} | {} header: {}",
                 urlID, decodedID, reSlashedID, PUBLIC_IDENTIFIER_HEADER,
                 headerID);
@@ -501,7 +502,7 @@ public abstract class AbstractResource extends ServerResource {
         final RequestContext context = new RequestContext();
         context.setRequestURI(getReference().toString());
         context.setRequestHeaders(getRequest().getHeaders().getValuesMap());
-        context.setClientIP(getCanonicalClientIpAddress());
+        context.setClientIP(getCanonicalClientIPAddress());
         context.setCookies(getRequest().getCookies().getValuesMap());
         return context;
     }
@@ -531,7 +532,7 @@ public abstract class AbstractResource extends ServerResource {
      *             template variables.
      */
     public Representation template(String name) {
-        return template(name, new HashMap<>());
+        return template(name, Collections.emptyMap());
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/config/HeritablePropertiesConfiguration.java
@@ -35,7 +35,7 @@ class HeritablePropertiesConfiguration extends HeritableFileConfiguration
     /**
      * Map of PropertiesConfigurations in order from leaf to trunk.
      */
-    private LinkedHashMap<File, PropertiesConfiguration> commonsConfigs =
+    private final LinkedHashMap<File, PropertiesConfiguration> commonsConfigs =
             new LinkedHashMap<>();
 
     /**

src/main/java/edu/illinois/library/cantaloupe/config/MemoryConfiguration.java
@@ -16,7 +16,7 @@ import java.util.concurrent.ConcurrentMap;
 class MemoryConfiguration extends AbstractConfiguration
         implements Configuration {
 
-    private ConcurrentMap<String,Object> configuration =
+    private final ConcurrentMap<String,Object> configuration =
             new ConcurrentHashMap<>();
 
     @Override
@@ -34,13 +34,17 @@ class MemoryConfiguration extends AbstractConfiguration
         Object value = configuration.get(key);
         boolean bool;
         if (value != null) {
-            String stringValue = value.toString();
-            if (stringValue.equals("1") || stringValue.equals("true")) {
-                bool = true;
-            } else if (stringValue.equals("0") || stringValue.equals("false")) {
-                bool = false;
-            } else {
-                throw new ConversionException(key);
+            switch (value.toString()) {
+                case "1":
+                case "true":
+                    bool = true;
+                    break;
+                case "0":
+                case "false":
+                    bool = false;
+                    break;
+                default:
+                    throw new ConversionException(key);
             }
             return bool;
         } else {
@@ -185,7 +189,7 @@ class MemoryConfiguration extends AbstractConfiguration
      * No-op.
      */
     @Override
-    public synchronized void reload() {}
+    public void reload() {}
 
     /**
      * No-op.

src/main/java/edu/illinois/library/cantaloupe/resource/ImageRepresentation.java
@@ -145,11 +145,10 @@ public class ImageRepresentation extends OutputRepresentation {
         if (!opList.hasEffect(processor.getSourceFormat())) {
             if (processor instanceof FileProcessor &&
                     ((FileProcessor) processor).getSourceFile() != null) {
-                final File sourceFile =
-                        ((FileProcessor) processor).getSourceFile();
+                File sourceFile = ((FileProcessor) processor).getSourceFile();
                 Files.copy(sourceFile.toPath(), outputStream);
             } else {
-                final StreamSource streamSource =
+                StreamSource streamSource =
                         ((StreamProcessor) processor).getStreamSource();
                 try (InputStream inputStream = streamSource.newInputStream()) {
                     IOUtils.copy(inputStream, outputStream);

src/main/java/edu/illinois/library/cantaloupe/resource/PayloadTooLargeException.java
@@ -7,8 +7,8 @@ class PayloadTooLargeException extends ResourceException {
 
     PayloadTooLargeException() {
         super(Status.CLIENT_ERROR_FORBIDDEN,
-                "The requested image pixel area exceeds the threshold set in " +
-                        "the configuration.");
+                "The requested image pixel area exceeds the maximum " +
+                        "threshold set in the configuration.");
     }
 
 }

src/main/java/edu/illinois/library/cantaloupe/resource/admin/StatusResource.java
@@ -2,6 +2,8 @@ package edu.illinois.library.cantaloupe.resource.admin;
 
 import edu.illinois.library.cantaloupe.cache.InfoService;
 import edu.illinois.library.cantaloupe.resource.JSONRepresentation;
+import edu.illinois.library.cantaloupe.script.InvocationCache;
+import edu.illinois.library.cantaloupe.script.ScriptEngine;
 import edu.illinois.library.cantaloupe.script.ScriptEngineFactory;
 import edu.illinois.library.cantaloupe.util.TimeUtils;
 import org.restlet.representation.Representation;
@@ -27,13 +29,13 @@ public class StatusResource extends AbstractAdminResource {
 
         public Status() {
             try {
-                delegateMethodInvocationCache.put("size",
-                        ScriptEngineFactory.getScriptEngine().getInvocationCache().size());
-                delegateMethodInvocationCache.put("maxSize",
-                        ScriptEngineFactory.getScriptEngine().getInvocationCache().maxSize());
+                InvocationCache cache =
+                        ScriptEngineFactory.getScriptEngine().getInvocationCache();
+                delegateMethodInvocationCache.put("size", cache.size());
+                delegateMethodInvocationCache.put("maxSize", cache.maxSize());
             } catch (Exception e) {
-                // Not a big deal; if there's a significant problem it will be
-                // handled elsewhere.
+                // If this is significant it will be noticed & handled
+                // elsewhere.
             }
 
             this.infoCache.put("size",
@@ -55,7 +57,7 @@ public class StatusResource extends AbstractAdminResource {
     }
 
     @Get("json")
-    public Representation getConfiguration() throws Exception {
+    public Representation getConfiguration() {
         return new JSONRepresentation(new Status());
     }
 

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageResource.java
@@ -16,7 +16,6 @@ import edu.illinois.library.cantaloupe.resolver.Resolver;
 import edu.illinois.library.cantaloupe.resolver.ResolverFactory;
 import edu.illinois.library.cantaloupe.processor.ProcessorConnector;
 import edu.illinois.library.cantaloupe.resource.ImageRepresentation;
-import edu.illinois.library.cantaloupe.resource.RequestContext;
 import org.apache.commons.lang3.StringUtils;
 import org.restlet.data.Disposition;
 import org.restlet.representation.Representation;
@@ -123,7 +122,7 @@ public class ImageResource extends IIIF1Resource {
 
         ops.applyNonEndpointMutations(fullSize,
                 info.getOrientation(),
-                getCanonicalClientIpAddress(),
+                getCanonicalClientIPAddress(),
                 getReference().toUri(),
                 getRequest().getHeaders().getValuesMap(),
                 getCookies().getValuesMap());

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageResource.java
@@ -17,7 +17,6 @@ import edu.illinois.library.cantaloupe.resolver.ResolverFactory;
 import edu.illinois.library.cantaloupe.processor.ProcessorConnector;
 import edu.illinois.library.cantaloupe.resource.CachedImageRepresentation;
 import edu.illinois.library.cantaloupe.resource.ImageRepresentation;
-import edu.illinois.library.cantaloupe.resource.RequestContext;
 import edu.illinois.library.cantaloupe.resource.iiif.SizeRestrictedException;
 import org.restlet.data.Disposition;
 import org.restlet.representation.Representation;
@@ -78,7 +77,7 @@ public class ImageResource extends IIIF2Resource {
             if (info != null) {
                 ops.applyNonEndpointMutations(info.getSize(),
                         info.getOrientation(),
-                        getCanonicalClientIpAddress(),
+                        getCanonicalClientIPAddress(),
                         getReference().toUri(),
                         getRequest().getHeaders().getValuesMap(),
                         getCookies().getValuesMap());
@@ -160,7 +159,7 @@ public class ImageResource extends IIIF2Resource {
         try {
             ops.applyNonEndpointMutations(fullSize,
                     info.getOrientation(),
-                    getCanonicalClientIpAddress(),
+                    getCanonicalClientIPAddress(),
                     getReference().toUri(),
                     getRequest().getHeaders().getValuesMap(),
                     getCookies().getValuesMap());

commit 311180362ac3e76a6b4d6b50655e63d94f0138e4

Fix a regression from d9c75436393bc5c5b126c1d648b7307b5991788b

src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -39,7 +39,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -532,7 +531,7 @@ public abstract class AbstractResource extends ServerResource {
      *             template variables.
      */
     public Representation template(String name) {
-        return template(name, Collections.emptyMap());
+        return template(name, new HashMap<>());
     }
 
     /**