26f2fb6cc85fafb002ef15d36584d57451749835
Alex Dolski <alexd@n-ary.net>
2017-12-15 12:31:59 -0600

    Refactor getJavaMajorVersion() to improve testability

--- src/main/java/edu/illinois/library/cantaloupe/util/SystemUtils.java
+++ src/main/java/edu/illinois/library/cantaloupe/util/SystemUtils.java
@@ -10,17 +10,27 @@ public final class SystemUtils {
      * @return Java major version such as <code>8</code>, <code>9</code>, etc.
      */
     public static int getJavaMajorVersion() {
+        final String versionStr = System.getProperty("java.version");
+        return parseJavaMajorVersion(versionStr);
+    }
+
+    /**
+     * @param version Value of the <code>java.version</code> system property.
+     * @return Major version.
+     */
+    static int parseJavaMajorVersion(String version) {
         // Up to Java 8, this will be a string like: "1.8.0_60"
         // Beginning in Java 9, it will be a string like "9", "9.0.1", etc.
-        final String versionStr = System.getProperty("java.version");
-
-        if (versionStr.startsWith("1")) { // < 9
-            String[] parts = versionStr.split(Pattern.quote("."));
+        if (version.startsWith("1.")) { // < 9
+            String[] parts = version.split(Pattern.quote("."));
             if (parts.length > 1) {
                 return Integer.parseInt(parts[1]);
             }
         }
-        return Integer.parseInt(versionStr.substring(0, versionStr.indexOf(".")));
+        if (version.contains(".")) { // 9.x, 9.x.x, 10.x, 10.x.x, etc.
+            return Integer.parseInt(version.substring(0, version.indexOf(".")));
+        }
+        return Integer.parseInt(version); // 9, 10, etc.
     }
 
     /**
