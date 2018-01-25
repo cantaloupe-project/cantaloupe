a9829bdfd58444bbb7e7d615735132a15ef4223d
Alex Dolski <alexd@n-ary.net>
2018-01-25 15:15:00 -0600

    write() closes the instance's InputStream

--- src/main/java/edu/illinois/library/cantaloupe/resource/CachedImageRepresentation.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/CachedImageRepresentation.java
@@ -13,42 +13,46 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Restlet representation for cached images.
+ * Representation for cached images.
  */
 public class CachedImageRepresentation extends OutputRepresentation {
 
-    private static Logger logger = LoggerFactory.
-            getLogger(CachedImageRepresentation.class);
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CachedImageRepresentation.class);
 
     private InputStream inputStream;
 
     /**
      * Constructor for images from the cache.
      *
-     * @param mediaType
-     * @param disposition,
-     * @param inputStream
+     * @param inputStream Cache input stream.
+     * @param mediaType   Media type of the cached image.
+     * @param disposition HTTP {@literal Content-Disposition}.
      */
-    public CachedImageRepresentation(MediaType mediaType,
-                                     Disposition disposition,
-                                     InputStream inputStream) {
+    public CachedImageRepresentation(InputStream inputStream,
+                                     MediaType mediaType,
+                                     Disposition disposition) {
         super(new org.restlet.data.MediaType(mediaType.toString()));
         this.inputStream = inputStream;
         setDisposition(disposition);
     }
 
     /**
-     * Writes the source image to the given output stream.
+     * Writes the cached image to the given output stream.
      *
-     * @param outputStream Response body stream supplied by Restlet
-     * @throws IOException
+     * @param outputStream Response body stream supplied by Restlet.
      */
     @Override
     public void write(OutputStream outputStream) throws IOException {
-        final Stopwatch watch = new Stopwatch();
-        IOUtils.copy(inputStream, outputStream);
-        logger.debug("Streamed from the cache without resolving in {} msec",
-                watch.timeElapsed());
+        try {
+            final Stopwatch watch = new Stopwatch();
+            IOUtils.copy(inputStream, outputStream);
+            LOGGER.debug("Streamed from the cache without resolving in {} msec",
+                    watch.timeElapsed());
+        } finally {
+            inputStream.close();
+            // N.B.: Restlet will close the output stream.
+        }
     }
 
 }
