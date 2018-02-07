cf4d46026a42b09acab9a3a4b71a650772d72c5d
Alex Dolski <alexd@n-ary.net>
2018-02-07 09:15:39 -0600

    No need to use an AtomicLong

--- src/main/java/edu/illinois/library/cantaloupe/util/Stopwatch.java
+++ src/main/java/edu/illinois/library/cantaloupe/util/Stopwatch.java
@@ -1,21 +1,19 @@
 package edu.illinois.library.cantaloupe.util;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
- * <p>Simple class used for measuring time elapsed.</p>
+ * <p>Measures time elapsed. The timer starts at instantiation.</p>
  *
  * <p>Instances are thread-safe.</p>
  */
 public class Stopwatch {
 
-    private AtomicLong startTime = new AtomicLong(System.currentTimeMillis());
+    private final long startTime = System.currentTimeMillis();
 
     /**
      * @return Number of milliseconds since the stopwatch was started.
      */
     public long timeElapsed() {
-        return System.currentTimeMillis() - startTime.get();
+        return System.currentTimeMillis() - startTime;
     }
 
 }
