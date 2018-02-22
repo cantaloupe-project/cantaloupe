cantaloupe-project/cantaloupe

commit 7e42505215b5fa65fa5fffd00fbf5e42e2af03e2

HttpResolver falls back to inferring the source format from a resource;s magic bytes, if necessary. The HEAD request is replaced with a 32-byte-ranged GET request.

src/main/java/edu/illinois/library/cantaloupe/resolver/HttpResolver.java
@@ -27,12 +27,14 @@ import org.slf4j.LoggerFactory;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 import javax.script.ScriptException;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.NoSuchFileException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -47,32 +49,43 @@ import java.util.concurrent.TimeoutException;
  *
  * <p>HTTP/2 is not supported.</p>
  *
- * <h1>Format Determination</h1>
+ * <h1>Format Inference</h1>
  *
- * <p>For images with recognized name extensions, the format will be inferred
- * by {@link Format#inferFormat(Identifier)}. For images with unrecognized or
- * missing extensions, the <code>Content-Type</code> header will be checked to
- * determine their format, which will incur an extra <code>HEAD</code> request.
- * It is therefore more efficient to serve images with extensions.</p>
+ * <ol>
+ *     <li>If the source image's identifier has a recognized filename
+ *     extension, the format will be inferred from that.</li>
+ *     <li>Otherwise, a {@literal GET} request will be sent with a {@literal
+ *     Range} header specifying a small range of data from the beginning of the
+ *     resource.
+ *         <ol>
+ *             <li>If a {@literal Content-Type} header is present in the
+ *             response, and is specific enough (i.e. not {@literal
+ *             application/octet-stream}), a format will be inferred from
+ *             it.</li>
+ *             <li>Otherwise, a format will be inferred from the magic bytes in
+ *             the respone body.</li>
+ *         </ol>
+ *     </li>
+ * </ol>
  *
  * <h1>Lookup Strategies</h1>
  *
  * <p>Two distinct lookup strategies are supported, defined by
- * {@link Key#HTTPRESOLVER_LOOKUP_STRATEGY}. BasicLookupStrategy locates
- * images by concatenating a pre-defined URL prefix and/or suffix.
- * ScriptLookupStrategy invokes a delegate method to retrieve a URL (and
- * optional auth info) dynamically.</p>
+ * {@link Key#HTTPRESOLVER_LOOKUP_STRATEGY}. {@link LookupStrategy#BASIC}
+ * locates images by concatenating a pre-defined URL prefix and/or suffix.
+ * {@link LookupStrategy#DELEGATE_SCRIPT} invokes a delegate method to
+ * retrieve a URL (and optional auth info) dynamically.</p>
  *
  * <h1>Authentication Support</h1>
  *
  * <p>HTTP Basic authentication is supported.</p>
  *
  * <ul>
- *     <li>When using BasicLookupStrategy, auth info is set globally in the
- *     {@link Key#HTTPRESOLVER_BASIC_AUTH_USERNAME} and
+ *     <li>When using {@link LookupStrategy#BASIC}, auth info is set globally
+ *     in the {@link Key#HTTPRESOLVER_BASIC_AUTH_USERNAME} and
  *     {@link Key#HTTPRESOLVER_BASIC_AUTH_SECRET} configuration keys.</li>
- *     <li>When using ScriptLookupStrategy, auth info can be returned from a
- *     delegate method.</li>
+ *     <li>When using {@link LookupStrategy#DELEGATE_SCRIPT}, auth info can be
+ *     returned from the delegate method.</li>
  * </ul>
  *
  * @see <a href="http://www.eclipse.org/jetty/documentation/current/http-client.html">
@@ -158,15 +171,23 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(HttpResolver.class);
 
+    /**
+     * Byte length of the range used to infer the source image format.
+     */
+    private static final int FORMAT_INFERENCE_RANGE_LENGTH = 32;
+
     private static final String GET_URL_DELEGATE_METHOD =
             "HttpResolver::get_url";
 
     private static HttpClient jettyClient;
 
+    private InputStreamResponseListener rangedGETResponseListener;
+
     /**
-     * Cached HTTP HEAD response.
+     * Cached HTTP {@literal GET} response with a maximum body length of {@link
+     * #FORMAT_INFERENCE_RANGE_LENGTH}.
      */
-    private Response headResponse;
+    private Response rangedGETResponse;
 
     /**
      * Lazy-loaded by {@link #getResourceInfo()}.
@@ -226,10 +247,11 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
 
     @Override
     public void checkAccess() throws IOException {
-        Response response = retrieveHEADResponse();
+        Response response = retrieveRangedGETResponse();
+
         if (response.getStatus() >= HttpStatus.BAD_REQUEST_400) {
-            final String statusLine = "HTTP " + headResponse.getStatus() +
-                    ": " + headResponse.getReason();
+            final String statusLine = "HTTP " + rangedGETResponse.getStatus() +
+                    ": " + rangedGETResponse.getReason();
 
             if (response.getStatus() == HttpStatus.NOT_FOUND_404
                     || response.getStatus() == HttpStatus.GONE_410) {
@@ -248,39 +270,62 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
         if (sourceFormat == null) {
             sourceFormat = Format.inferFormat(identifier);
             if (Format.UNKNOWN.equals(sourceFormat)) {
-                sourceFormat = inferSourceFormatFromContentTypeHeader();
+                sourceFormat = inferSourceFormatFromResponse();
             }
         }
         return sourceFormat;
     }
 
     /**
-     * Issues an HTTP <code>HEAD</code> request and checks the response
-     * <code>Content-Type</code> header to determine the source format.
+     * Issues an HTTP {@literal GET} request for a small {@literal Range} of
+     * the beginning of the resource and checks for the response {@literal
+     * Content-Type} header. If it is not present or specific enough (i.e.
+     * {@literal application/octet-stream}), the magic bytes in the response
+     * body are checked.
      *
      * @return Inferred source format, or {@link Format#UNKNOWN} if unknown.
      */
-    private Format inferSourceFormatFromContentTypeHeader() {
+    private Format inferSourceFormatFromResponse() {
         Format format = Format.UNKNOWN;
         try {
             final ResourceInfo info = getResourceInfo();
-            final Response response = retrieveHEADResponse();
+            final Response response = retrieveRangedGETResponse();
 
             if (response.getStatus() >= 200 && response.getStatus() < 300) {
                 HttpField field = response.getHeaders().getField("Content-Type");
+
                 if (field != null && field.getValue() != null) {
                     format = new MediaType(field.getValue()).toFormat();
+
                     if (Format.UNKNOWN.equals(format)) {
-                        LOGGER.warn("Unrecognized Content-Type header value for HEAD {}",
-                                info.getURI());
+                        LOGGER.debug("Unrecognized Content-Type header value for GET {}: {}",
+                                info.getURI(), field.getValue().substring(0, 128));
                     }
                 } else {
-                    LOGGER.warn("No Content-Type header for HEAD {}",
+                    LOGGER.debug("No Content-Type header for GET {}",
                             info.getURI());
                 }
+
+                if (Format.UNKNOWN.equals(format)) {
+                    LOGGER.debug("Attempting to infer format from magic bytes for {}",
+                            info.getURI());
+                    try (InputStream bodyStream =
+                                 new BufferedInputStream(rangedGETResponseListener.getInputStream())) {
+                        List<MediaType> types =
+                                MediaType.detectMediaTypes(bodyStream);
+                        if (!types.isEmpty()) {
+                            format = types.get(0).toFormat();
+                            LOGGER.debug("Inferred {} format from magic bytes for {}",
+                                    format, info.getURI());
+                        } else {
+                            LOGGER.debug("Unable to infer a format from magic bytes for GET {}",
+                                    info.getURI());
+                        }
+                    }
+                }
             } else {
-                LOGGER.warn("HEAD returned status {} for {}",
-                        response.getStatus(), info.getURI());
+                LOGGER.warn("GET {} returned status {}",
+                        info.getURI(), response.getStatus());
             }
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
@@ -308,8 +353,12 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
         return null;
     }
 
-    private Response retrieveHEADResponse() throws IOException {
-        if (headResponse == null) {
+    /**
+     * Issues a {@literal GET} request specifying a small range of data and
+     * caches the result in {@link #rangedGETResponse}.
+     */
+    private Response retrieveRangedGETResponse() throws IOException {
+        if (rangedGETResponse == null) {
             ResourceInfo info;
             try {
                 info = getResourceInfo();
@@ -317,29 +366,30 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
                 LOGGER.error(e.getMessage(), e);
                 throw new IOException(e.getMessage(), e);
             } catch (Exception e) {
-                LOGGER.error("retrieveHEADResponse(): {}", e.getMessage());
+                LOGGER.error("retrieveRangedGETResponse(): {}", e.getMessage());
                 throw new IOException(e.getMessage(), e);
             }
 
             try {
                 final HttpClient client = getHTTPClient(info);
 
-                InputStreamResponseListener listener =
-                        new InputStreamResponseListener();
-                client.newRequest(info.getURI()).
-                        timeout(getRequestTimeout(), TimeUnit.SECONDS).
-                        method(HttpMethod.HEAD).send(listener);
+                rangedGETResponseListener = new InputStreamResponseListener();
 
-                // Wait for the response headers to arrive.
-                headResponse = listener.get(getRequestTimeout(),
-                        TimeUnit.SECONDS);
+                client.newRequest(info.getURI())
+                        .timeout(getRequestTimeout(), TimeUnit.SECONDS)
+                        .header("Range", "bytes=0-" + (FORMAT_INFERENCE_RANGE_LENGTH - 1))
+                        .method(HttpMethod.GET)
+                        .send(rangedGETResponseListener);
+
+                rangedGETResponse = rangedGETResponseListener.get(
+                        getRequestTimeout(), TimeUnit.SECONDS);
             } catch (ExecutionException e ) {
                 throw new AccessDeniedException(info.getURI().toString());
             } catch (InterruptedException | TimeoutException e) {
                 throw new IOException(e.getMessage(), e);
             }
         }
-        return headResponse;
+        return rangedGETResponse;
     }
 
     /**
@@ -410,7 +460,8 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
 
     @Override
     public void setIdentifier(Identifier identifier) {
-        headResponse = null;
+        rangedGETResponse = null;
+        rangedGETResponseListener = null;
         resourceInfo = null;
         sourceFormat = null;
         this.identifier = identifier;

src/test/java/edu/illinois/library/cantaloupe/resolver/HttpResolverTest.java
@@ -18,16 +18,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.*;
@@ -147,20 +144,21 @@ abstract class HttpResolverTest extends AbstractResolverTest {
     private void doTestCheckAccessWithPresentUnreadableImage(Identifier identifier)
             throws Exception {
         try {
+            server.setHandler(new DefaultHandler() {
+                @Override
+                public void handle(String target,
+                                   Request baseRequest,
+                                   HttpServletRequest request,
+                                   HttpServletResponse response) {
+                    response.setStatus(403);
+                    baseRequest.setHandled(true);
+                }
+            });
             server.start();
 
-            Path image = TestUtil.getImage("gif");
-            Set<PosixFilePermission> permissions =
-                    Files.getPosixFilePermissions(image);
-            try {
-                Files.setPosixFilePermissions(image, Collections.emptySet());
-
-                instance.setIdentifier(identifier);
-                instance.checkAccess();
-                fail("Expected exception");
-            } finally {
-                Files.setPosixFilePermissions(image, permissions);
-            }
+            instance.setIdentifier(identifier);
+            instance.checkAccess();
+            fail("Expected exception");
         } catch (AccessDeniedException e) {
             // pass
         }
@@ -309,7 +307,7 @@ abstract class HttpResolverTest extends AbstractResolverTest {
     }
 
     @Test
-    public void testGetSourceFormatWithNoIdentifierExtensionAndContentTypeHeader()
+    public void testGetSourceFormatWithNoIdentifierExtensionAndContentType()
             throws Exception {
         server.setHandler(new DefaultHandler() {
             @Override
@@ -328,30 +326,41 @@ abstract class HttpResolverTest extends AbstractResolverTest {
     }
 
     @Test
-    public void testGetSourceFormatWithNoIdentifierExtensionAndNoContentTypeHeader()
+    public void testGetSourceFormatWithNoIdentifierExtensionAndNoContentType()
             throws Exception {
         server.start();
 
         instance.setIdentifier(new Identifier("jpg"));
-        assertEquals(Format.UNKNOWN, instance.getSourceFormat());
+        assertEquals(Format.JPG, instance.getSourceFormat());
     }
 
     @Test
-    public void testGetSourceFormatWithNoIdentifierExtensionAndUnrecognizedContentTypeHeaderValue()
+    public void testGetSourceFormatWithNoIdentifierExtensionAndUnrecognizedContentType()
             throws Exception {
         server.setHandler(new DefaultHandler() {
             @Override
             public void handle(String target,
                                Request baseRequest,
                                HttpServletRequest request,
-                               HttpServletResponse response) {
-                response.setHeader("Content-Type", "image/bogus");
-                baseRequest.setHandled(true);
+                               HttpServletResponse response) throws IOException {
+                response.setHeader("Content-Type", "application/octet-stream");
+                try (OutputStream os = response.getOutputStream()) {
+                    Files.copy(TestUtil.getImage("jpg"), os);
+                }
             }
         });
         server.start();
 
         instance.setIdentifier(new Identifier("jpg"));
+        assertEquals(Format.JPG, instance.getSourceFormat());
+    }
+
+    @Test
+    public void testGetSourceFormatWithNoIdentifierExtensionAndNoContentTypeAndUnrecognizedMagicBytes()
+            throws Exception {
+        server.start();
+
+        instance.setIdentifier(new Identifier("txt"));
         assertEquals(Format.UNKNOWN, instance.getSourceFormat());
     }
 
@@ -526,7 +535,6 @@ abstract class HttpResolverTest extends AbstractResolverTest {
 
     @Test
     public void testNoUnnecessaryRequests() throws Exception {
-        final AtomicInteger numHEADRequests = new AtomicInteger(0);
         final AtomicInteger numGETRequests = new AtomicInteger(0);
 
         server.setHandler(new DefaultHandler() {
@@ -538,9 +546,6 @@ abstract class HttpResolverTest extends AbstractResolverTest {
                 switch (request.getMethod().toUpperCase()) {
                     case "GET":
                         numGETRequests.incrementAndGet();
-                        break;
-                    case "HEAD":
-                        numHEADRequests.incrementAndGet();
                         break;
                     default:
                         throw new IllegalArgumentException("WTF");
@@ -558,8 +563,7 @@ abstract class HttpResolverTest extends AbstractResolverTest {
             is.read();
         }
 
-        assertEquals(1, numHEADRequests.get());
-        assertEquals(1, numGETRequests.get());
+        assertEquals(2, numGETRequests.get());
     }
 
     @Test

website/manual/4.0/resolvers.html
@@ -95,9 +95,11 @@ end
 
 <p>For images with extensions that are missing or unrecognized, this resolver will check the "magic number" to determine type, which will add some overhead. It is therefore slightly more efficient to serve images with extensions.</p>
 
+<h4>Lookup Strategies</h4>
+
 <p>FilesystemResolver supports two distinct lookup strategies, defined by the <code>FilesystemResolver.lookup_strategy</code> configuration option.</p>
 
-<h4 id="FilesystemResolverBasicLookupStrategy">BasicLookupStrategy</h4>
+<h5 id="FilesystemResolverBasicLookupStrategy">BasicLookupStrategy</h5>
 
 <p>BasicLookupStrategy locates images by concatenating a pre-defined path prefix and/or suffix. For example, with the following configuration options set:</p>
 
@@ -118,7 +120,7 @@ FilesystemResolver.BasicLookupStrategy.path_suffix =
 
 <div class="alert alert-warning">It may be dangerous to <strong>not</strong> use <code>path_prefix</code>. The shallower the path, the more of the filesystem that will be exposed.</div>
 
-<h4 id="FilesystemResolverScriptLookupStrategy">ScriptLookupStrategy</h4>
+<h5 id="FilesystemResolverScriptLookupStrategy">ScriptLookupStrategy</h5>
 
 <p>Sometimes, BasicLookupStrategy will not offer enough control. Perhaps you want to serve images from multiple filesystems, or perhaps your identifiers are opaque and you need to perform a database or web service request to locate the corresponding images. With this lookup strategy, you can tell FilesystemResolver to invoke a method in your <a href="delegate-script.html">delegate script</a> and capture the pathname it returns.</p>
 
@@ -194,22 +196,35 @@ end
 
 <h3 id="HttpResolver">HttpResolver</h3>
 
-<p>HttpResolver maps a URL identifier to an HTTP or HTTPS resource, for retrieving images from a web server.</p>
+<p>HttpResolver maps a URL identifier to an HTTP or HTTPS resource, for retrieving images from a web server. HttpResolver uses a <a href="http://www.eclipse.org/jetty/documentation/current/http-client.html">Jetty HTTP client</a> internally and supports HTTP/1.1 and optional Basic authentication.</p>
 
-<p>Prior to version 3.4, HttpResolver used the HTTP client bundled with <a href="https://restlet.com/open-source/">Restlet Framework</a>. In version 3.4, the implementation changed to use the <a href="http://www.eclipse.org/jetty/documentation/current/http-client.html">Jetty HTTP client</a>, which is more robust.</p>
+<h4>Format Inference</h4>
 
-<p>It is preferable to use this resolver with source images with recognizable file extensions. For images with an extension that is missing or unrecognizable, it will issue an HTTP HEAD request to the server to check the <code>Content-Type</code> header. If the type cannot be inferred from that, an error response will be returned.</p>
+<p>Like all resolvers, HttpResolver needs to figure out the format of a source image before it can be served. It uses the following strategy to do this:</p>
+
+<ol>
+  <li>If the source image's identifier has a recognized filename extension, the format will be inferred from that.</li>
+  <li>Otherwise, a <code>GET</code> request will be sent with a <code>Range</code> header specifying a small range of data from the beginning of the resource.
+    <ol>
+      <li>If a <code>Content-Type</code> header is present in the response, and is specific enough (i.e. not <code>application/octet-stream</code>), a format will be inferred from it.</li>
+      <li>Otherwise, a format will be inferred from the magic bytes in the respone body.</li>
+    </ol>
+  </li>
+  <li>If none of the above are successful, an error response will be returned.</li>
+</ol>
+
+<h4>Lookup Strategies</h4>
 
 <p>HttpResolver supports two distinct lookup strategies, defined by the <code>HttpResolver.lookup_strategy</code> configuration option.</p>
 
-<h4 id="HttpResolverBasicLookupStrategy">BasicLookupStrategy</h4>
+<h5 id="HttpResolverBasicLookupStrategy">BasicLookupStrategy</h5>
 
 <p>BasicLookupStrategy locates images by concatenating a pre-defined URL prefix and/or suffix. For example, with the following configuration options set:</p>
 
 {% highlight properties %}
 # Note trailing slash!
-HttpResolver.url_prefix = http://example.org/images/
-HttpResolver.url_suffix =
+HttpResolver.BasicLookupStrategy.url_prefix = http://example.org/images/
+HttpResolver.BasicLookupStrategy.url_suffix =
 {% endhighlight %}
 
 <p>An identifier of <code>image.jpg</code> in the URL will resolve to
@@ -219,11 +234,20 @@ HttpResolver.url_suffix =
 
 <div class="alert alert-info">If you are operating behind a reverse proxy that is not capable of passing encoded URL characters through without decoding them, see the <code>slash_substitude</code> configuration key.</div>
 
-<h4 id="HttpResolverScriptLookupStrategy">ScriptLookupStrategy</h4>
+<h5 id="HttpResolverScriptLookupStrategy">ScriptLookupStrategy</h5>
 
 <p>Sometimes, BasicLookupStrategy will not offer enough control. Perhaps you want to serve images from multiple URLs, or perhaps your identifiers are opaque and you need to run a database or web service request to locate them.  With this lookup strategy, you can tell HttpResolver to invoke a method in your <a href="delegate-script.html">delegate script</a> and capture the URL it returns.</p>
 
 <p>The delegate script method, <code>get_url(identifier)</code>, will take in an identifier string and should return a URL, if available, or <code>nil</code>, if not. See the <a href="delegate-script.html">Delegate Script</a> section for general information about the delegate script, and the <a href="#FilesystemResolverScriptLookupStrategy">FilesystemResolver ScriptLookupStrategy</a> section for examples of similar scripts.</p>
+
+<h4>Authentication</h1>
+
+<p>HTTP Basic authentication is supported.</p>
+
+<ul>
+  <li>When using <a href="#HttpResolverBasicLookupStrategy">BasicLookupStrategy</a>, auth info is set globally in the <code>HttpResolver.auth.basic.username</code> and <code>HttpResolver.auth.basic.secret</code> configuration keys.</li>
+  <li>When using <a href="#HttpResolverScriptLookupStrategy">ScriptLookupStrategy</a>, auth info can be returned from the delegate method.</li>
+</ul>
 
 <hr>
 

commit 82b4c8175a30c01e806b70279616c944d02b39b8

Fix a bug introduced in 7e42505215b5fa65fa5fffd00fbf5e42e2af03e2

src/main/java/edu/illinois/library/cantaloupe/resolver/HttpResolver.java
@@ -298,8 +298,8 @@ class HttpResolver extends AbstractResolver implements StreamResolver {
                     format = new MediaType(field.getValue()).toFormat();
 
                     if (Format.UNKNOWN.equals(format)) {
-                        LOGGER.debug("Unrecognized Content-Type header value for GET {}: {}",
-                                info.getURI(), field.getValue().substring(0, 128));
+                        LOGGER.debug("Unrecognized Content-Type header value for GET {}",
+                                info.getURI());
                     }
                 } else {
                     LOGGER.debug("No Content-Type header for GET {}",