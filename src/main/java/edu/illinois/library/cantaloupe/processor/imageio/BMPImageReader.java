f6401cb54520ce4565cebe2d35e769be36733018
Alex Dolski <alexd@n-ary.net>
2018-02-28 14:30:28 -0600

    Override preferredIIOImplementations()

--- src/main/java/edu/illinois/library/cantaloupe/processor/imageio/BMPImageReader.java
+++ src/main/java/edu/illinois/library/cantaloupe/processor/imageio/BMPImageReader.java
@@ -35,4 +35,9 @@ final class BMPImageReader extends AbstractImageReader {
         return new NullMetadata(metadata, metadataFormat);
     }
 
+    @Override
+    String[] preferredIIOImplementations() {
+        return new String[] { "com.sun.imageio.plugins.bmp.BMPImageReader" };
+    }
+
 }
