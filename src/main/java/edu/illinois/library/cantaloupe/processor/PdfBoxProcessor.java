cantaloupe-project/cantaloupe

commit d6c3b3d6f56ed6d923070dfc8f2a46975144df50

PdfBoxProcessor.readImageInfo() populates the Info with one subimage per page. These subimages are used for validating the request.

src/main/java/edu/illinois/library/cantaloupe/processor/PdfBoxProcessor.java
@@ -41,14 +41,13 @@ import java.util.Set;
 class PdfBoxProcessor extends AbstractJava2DProcessor
         implements FileProcessor, StreamProcessor {
 
-    private static final Logger LOGGER = LoggerFactory.
-            getLogger(PdfBoxProcessor.class);
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(PdfBoxProcessor.class);
 
     private static final int FALLBACK_DPI = 150;
 
     private PDDocument doc;
     private InputStream docInputStream;
-    private Dimension imageSize;
     private Path sourceFile;
     private StreamSource streamSource;
 
@@ -62,13 +61,9 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
 
     @Override
     public Set<Format> getAvailableOutputFormats() {
-        final Set<Format> outputFormats;
-        if (Format.PDF.equals(format)) {
-            outputFormats = ImageWriterFactory.supportedFormats();
-        } else {
-            outputFormats = Collections.unmodifiableSet(Collections.emptySet());
-        }
-        return outputFormats;
+        return (Format.PDF.equals(format)) ?
+                ImageWriterFactory.supportedFormats() :
+                Collections.unmodifiableSet(Collections.emptySet());
     }
 
     @Override
@@ -79,30 +74,6 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
     @Override
     public StreamSource getStreamSource() {
         return streamSource;
-    }
-
-    private void loadDocument() throws IOException {
-        if (doc == null) {
-            final Stopwatch watch = new Stopwatch();
-
-            if (sourceFile != null) {
-                doc = PDDocument.load(sourceFile.toFile());
-            } else {
-                docInputStream = streamSource.newInputStream();
-                doc = PDDocument.load(docInputStream);
-            }
-
-            // Disable the document's cache of PDImageXObjects
-            // See: https://pdfbox.apache.org/2.0/faq.html#outofmemoryerror
-            doc.setResourceCache(new DefaultResourceCache() {
-                @Override
-                public void put(COSObject indirect, PDXObject xobject) {
-                    // no-op
-                }
-            });
-
-            LOGGER.debug("Loaded document in {}", watch);
-        }
     }
 
     @Override
@@ -118,8 +89,8 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
             scale = new Scale();
         }
         // If the op list contains a scale operation that is not
-        // NON_ASPECT_FILL, we can use a scale-appropriate rasterization
-        // DPI and omit the scale step.
+        // NON_ASPECT_FILL, we can use a scale-appropriate rasterization DPI
+        // and omit the scale step.
         if (!Scale.Mode.NON_ASPECT_FILL.equals(scale.getMode())) {
             hints.add(ReaderHint.IGNORE_SCALE);
         }
@@ -165,6 +136,31 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
         return Math.max(page, 1);
     }
 
+    private void readDocument() throws IOException {
+        if (doc == null) {
+            final Stopwatch watch = new Stopwatch();
+
+            if (sourceFile != null) {
+                doc = PDDocument.load(sourceFile.toFile());
+            } else {
+                docInputStream = streamSource.newInputStream();
+                doc = PDDocument.load(docInputStream);
+            }
+
+            // Disable the document's cache of PDImageXObjects
+            // See: https://pdfbox.apache.org/2.0/faq.html#outofmemoryerror
+            // This cache has never proven to be a problem, but it's not needed.
+            doc.setResourceCache(new DefaultResourceCache() {
+                @Override
+                public void put(COSObject indirect, PDXObject xobject) {
+                    // no-op
+                }
+            });
+
+            LOGGER.debug("Loaded document in {}", watch);
+        }
+    }
+
     /**
      * @return Rasterized page of the PDF.
      */
@@ -177,37 +173,35 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
 
     /**
      * @return Rasterized page of the PDF.
+     * @throws IndexOutOfBoundsException if the given page index is out of
+     *                                   bounds.
      */
     private BufferedImage readImage(int pageIndex,
                                     float dpi) throws IOException {
         LOGGER.debug("DPI: {}", dpi);
 
-        loadDocument();
-        // If the given page index is out of bounds, the renderer will
-        // throw an IndexOutOfBoundsException.
+        readDocument();
         PDFRenderer renderer = new PDFRenderer(doc);
         return renderer.renderImageWithDPI(pageIndex, dpi);
     }
 
     @Override
     public Info readImageInfo() throws IOException {
-        if (imageSize == null) {
-            loadDocument();
+        readDocument();
 
+        final Configuration config = Configuration.getInstance();
+        final int dpi = config.getInt(Key.PROCESSOR_DPI, FALLBACK_DPI);
+        final float scale = dpi / 72f;
+        final Info info = Info.builder()
+                .withFormat(getSourceFormat())
+                .withNumResolutions(1)
+                .build();
+        info.getImages().clear();
+
+        for (int i = 0; i < doc.getNumberOfPages(); i++) {
             // PDF doesn't have native dimensions, so figure out the dimensions
             // at the current DPI setting.
-            //
-            // Changing this setting will affect the returned info, which
-            // could have implications for info caching. Also, we hope that
-            // every page will have the same dimensions...
-            //
-            // N.B.: Accessing the application configuration from a processor
-            // is VERY BAD PRACTICE but we have to in this case.
-            final Configuration config = Configuration.getInstance();
-            final int dpi = config.getInt(Key.PROCESSOR_DPI, FALLBACK_DPI);
-            final float scale = dpi / 72f;
-
-            final PDPage page = doc.getPage(0);
+            final PDPage page = doc.getPage(i);
             final PDRectangle cropBox = page.getCropBox();
             final float widthPt = cropBox.getWidth();
             final float heightPt = cropBox.getHeight();
@@ -220,14 +214,14 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
                 widthPx = heightPx;
                 heightPx = tmp;
             }
-            imageSize = new Dimension(widthPx, heightPx);
+
+            Dimension size = new Dimension(widthPx, heightPx);
+            Info.Image image = new Info.Image();
+            image.setSize(size);
+            image.setTileSize(size);
+            info.getImages().add(image);
         }
-        return Info.builder()
-                .withSize(imageSize)
-                .withTileSize(imageSize)
-                .withFormat(getSourceFormat())
-                .withNumResolutions(1)
-                .build();
+        return info;
     }
 
     @Override
@@ -247,7 +241,7 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
                          Dimension fullSize) throws ProcessorException {
         StreamProcessor.super.validate(opList, fullSize);
 
-        // Check the format of the "page" option, if present.
+        // Check the format of the "page" argument, if present.
         final String pageStr = (String) opList.getOptions().get("page");
         if (pageStr != null) {
             try {
@@ -255,9 +249,10 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
                 if (page > 0) {
                     // Check that the page is actually contained in the PDF.
                     try {
-                        loadDocument();
+                        readDocument();
                         if (page > doc.getNumberOfPages()) {
-                            throw new IllegalArgumentException(
+                            close();
+                            throw new IndexOutOfBoundsException(
                                     "Page number is out-of-bounds.");
                         }
                     } catch (IOException e) {
@@ -265,7 +260,7 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
                         throw new ProcessorException(e.getMessage(), e);
                     }
                 } else {
-                    throw new IllegalArgumentException(
+                    throw new IndexOutOfBoundsException(
                             "Page number is out-of-bounds.");
                 }
             } catch (NumberFormatException e) {

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageResource.java
@@ -138,7 +138,7 @@ public class ImageResource extends IIIF1Resource {
                     ops.getIdentifier(), ops.getOutputFormat());
 
             final Info info = getOrReadInfo(identifier, processor);
-            final Dimension fullSize = info.getSize();
+            final Dimension fullSize = info.getSize(getPageIndex());
 
             getRequestContext().setOperationList(ops, fullSize);
 

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageResource.java
@@ -155,7 +155,7 @@ public class ImageResource extends IIIF2Resource {
                     resolver, processor, identifier, sourceFormat);
 
             final Info info = getOrReadInfo(ops.getIdentifier(), processor);
-            final Dimension fullSize = info.getSize();
+            final Dimension fullSize = info.getSize(getPageIndex());
 
             getRequestContext().setOperationList(ops, fullSize);
 

src/main/java/edu/illinois/library/cantaloupe/resource/PublicResource.java
@@ -28,6 +28,21 @@ public abstract class PublicResource extends AbstractResource {
     }
 
     /**
+     * @return Page index (a.k.a. page number - 1) from the {@literal page}
+     *         query argument, or {@literal 0} if not supplied.
+     */
+    protected int getPageIndex() {
+        String arg = getReference().getQueryAsForm().getFirstValue("page", "1");
+        try {
+            int index = Integer.parseInt(arg) - 1;
+            if (index >= 0) {
+                return index;
+            }
+        } catch (NumberFormatException ignore) {}
+        return 0;
+    }
+
+    /**
      * <p>Checks that the requested area is greater than zero and less than or
      * equal to {@link Key#MAX_PIXELS}.</p>
      *

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageInfoFactory.java
@@ -5,23 +5,18 @@ import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.image.Info;
 import edu.illinois.library.cantaloupe.processor.Processor;
-import edu.illinois.library.cantaloupe.processor.ProcessorException;
 import edu.illinois.library.cantaloupe.resource.iiif.ImageInfoUtil;
 
 import java.awt.Dimension;
 
-class ImageInfoFactory {
+final class ImageInfoFactory {
 
-    /** Will be used to calculate a maximum scale factor. */
     private static final int MIN_SIZE = 64;
 
     ImageInfo newImageInfo(final String imageUri,
                            final Processor processor,
-                           final Info cacheInfo) throws ProcessorException {
-        // We want to use the orientation-aware full size, which takes the
-        // embedded orientation into account.
-        final Dimension virtualSize = cacheInfo.getOrientationSize();
-
+                           final Info.Image infoImage,
+                           final int numResolutions) {
         final ComplianceLevel complianceLevel = ComplianceLevel.getLevel(
                 processor.getSupportedFeatures(),
                 processor.getSupportedIIIF1Qualities(),
@@ -34,15 +29,13 @@ class ImageInfoFactory {
         // calculate a tile size close to MIN_TILE_SIZE_CONFIG_KEY pixels.
         // Otherwise, use the smallest multiple of the tile size above
         // MIN_TILE_SIZE_CONFIG_KEY of image resolution 0.
-        final Info.Image firstImage =
-                cacheInfo.getImages().get(0);
-        Dimension virtualTileSize = firstImage.getOrientationTileSize();
+        final Dimension virtualSize = infoImage.getOrientationSize();
+        Dimension virtualTileSize = infoImage.getOrientationTileSize();
 
-        if (cacheInfo.getImages().size() > 0) {
+        if (numResolutions > 0) {
             if (!virtualTileSize.equals(virtualSize)) {
-                virtualTileSize = ImageInfoUtil.smallestTileSize(virtualSize,
-                        firstImage.getOrientationTileSize(),
-                        minTileSize);
+                virtualTileSize = ImageInfoUtil.smallestTileSize(
+                        virtualSize, virtualTileSize, minTileSize);
             }
         }
 

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/InformationResource.java
@@ -76,9 +76,12 @@ public class InformationResource extends IIIF1Resource {
                     if (format != null && !Format.UNKNOWN.equals(format)) {
                         final Processor processor = new ProcessorFactory().
                                 newProcessor(format);
+                        final Info.Image infoImage =
+                                info.getImages().get(getPageIndex());
                         final ImageInfo imageInfo =
                                 new ImageInfoFactory().newImageInfo(
-                                        getImageURI(), processor, info);
+                                        getImageURI(), processor, infoImage,
+                                        info.getNumResolutions());
                         addLinkHeader(imageInfo);
                         commitCustomResponseHeaders();
                         return newRepresentation(imageInfo);
@@ -132,8 +135,10 @@ public class InformationResource extends IIIF1Resource {
                     resolver, processor, identifier, format);
 
             final Info info = getOrReadInfo(identifier, processor);
+            final Info.Image infoImage = info.getImages().get(getPageIndex());
             final ImageInfo imageInfo = new ImageInfoFactory().newImageInfo(
-                    getImageURI(), processor, info);
+                    getImageURI(), processor, infoImage,
+                    info.getNumResolutions());
 
             addLinkHeader(imageInfo);
             commitCustomResponseHeaders();

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageInfoFactory.java
@@ -38,20 +38,23 @@ final class ImageInfoFactory {
                             ServiceFeature.PROFILE_LINK_HEADER));
 
     /**
-     * @param imageURI  May be {@literal null}.
+     * @param imageURI       May be {@literal null}.
      * @param processor
      * @param info
-     * @param proxy     May be {@literal null}.
+     * @param infoImageIndex Index of the image in the {@link Info} argument's
+     *                       {@link Info#getImages()} list.
+     * @param proxy          May be {@literal null}.
      */
     ImageInfo<String,Object> newImageInfo(final String imageURI,
                                           final Processor processor,
                                           final Info info,
+                                          final int infoImageIndex,
                                           final DelegateProxy proxy) {
         final Configuration config = Configuration.getInstance();
 
         // We want to use the orientation-aware full size, which takes the
         // embedded orientation into account.
-        final Dimension virtualSize = info.getOrientationSize();
+        final Dimension virtualSize = info.getOrientationSize(infoImageIndex);
 
         // Create a Map instance, which will eventually be serialized to JSON
         // and returned in the response body.
@@ -89,13 +92,12 @@ final class ImageInfoFactory {
         final List<ImageInfo.Tile> tiles = new ArrayList<>();
         responseInfo.put("tiles", tiles);
 
-        final Info.Image firstImage =
-                info.getImages().get(0);
+        final Info.Image firstImage = info.getImages().get(0);
 
         // Find the virtual tile size based on the virtual full image size.
         final Dimension virtualTileSize = firstImage.getOrientationTileSize();
 
-        if (info.getImages().size() == 1 &&
+        if (info.getNumResolutions() == 1 &&
                 virtualTileSize.equals(virtualSize)) {
             uniqueTileSizes.add(
                     ImageInfoUtil.smallestTileSize(virtualSize, minTileSize));

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/InformationResource.java
@@ -164,8 +164,10 @@ public class InformationResource extends IIIF2Resource {
                                              Processor processor) {
         final ImageInfo<String, Object> imageInfo =
                 new ImageInfoFactory().newImageInfo(
-                        getImageURI(), processor, info, getDelegateProxy());
+                        getImageURI(), processor, info, getPageIndex(),
+                        getDelegateProxy());
         final MediaType mediaType = getNegotiatedMediaType();
+
         return new JSONRepresentation(imageInfo, mediaType, () -> {
             if (tempFileFuture != null) {
                 Path tempFile = tempFileFuture.get();

commit 6bdc22eafb1186548ea160735d97df8cd3bb2d82

Fix a regression from d6c3b3d6f56ed6d923070dfc8f2a46975144df50

src/main/java/edu/illinois/library/cantaloupe/processor/PdfBoxProcessor.java
@@ -173,7 +173,7 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
 
     /**
      * @return Rasterized page of the PDF.
-     * @throws IndexOutOfBoundsException if the given page index is out of
+     * @throws IllegalArgumentException if the given page index is out of
      *                                   bounds.
      */
     private BufferedImage readImage(int pageIndex,
@@ -252,7 +252,7 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
                         readDocument();
                         if (page > doc.getNumberOfPages()) {
                             close();
-                            throw new IndexOutOfBoundsException(
+                            throw new IllegalArgumentException(
                                     "Page number is out-of-bounds.");
                         }
                     } catch (IOException e) {
@@ -260,7 +260,7 @@ class PdfBoxProcessor extends AbstractJava2DProcessor
                         throw new ProcessorException(e.getMessage(), e);
                     }
                 } else {
-                    throw new IndexOutOfBoundsException(
+                    throw new IllegalArgumentException(
                             "Page number is out-of-bounds.");
                 }
             } catch (NumberFormatException e) {

src/main/java/edu/illinois/library/cantaloupe/resource/IllegalClientArgumentException.java
@@ -10,6 +10,10 @@ public class IllegalClientArgumentException extends IllegalArgumentException {
         super(message);
     }
 
+    public IllegalClientArgumentException(Throwable t) {
+        super(t);
+    }
+
     public IllegalClientArgumentException(String message, Throwable t) {
         super(message, t);
     }

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageResource.java
@@ -138,7 +138,12 @@ public class ImageResource extends IIIF1Resource {
                     ops.getIdentifier(), ops.getOutputFormat());
 
             final Info info = getOrReadInfo(identifier, processor);
-            final Dimension fullSize = info.getSize(getPageIndex());
+            Dimension fullSize;
+            try {
+                fullSize = info.getSize(getPageIndex());
+            } catch (IndexOutOfBoundsException e) {
+                throw new IllegalClientArgumentException(e);
+            }
 
             getRequestContext().setOperationList(ops, fullSize);
 

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageResource.java
@@ -155,7 +155,12 @@ public class ImageResource extends IIIF2Resource {
                     resolver, processor, identifier, sourceFormat);
 
             final Info info = getOrReadInfo(ops.getIdentifier(), processor);
-            final Dimension fullSize = info.getSize(getPageIndex());
+            Dimension fullSize;
+            try {
+                fullSize = info.getSize(getPageIndex());
+            } catch (IndexOutOfBoundsException e) {
+                throw new IllegalClientArgumentException(e);
+            }
 
             getRequestContext().setOperationList(ops, fullSize);
 