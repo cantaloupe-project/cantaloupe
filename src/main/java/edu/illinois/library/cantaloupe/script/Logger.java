d148e8bbc12f3bfe1f54577c5cef4b0f15320ec7
Alex Dolski <alexd@n-ary.net>
2018-05-01 09:16:58 -0500

    Add (String, Throwable) signatures for every method

--- src/main/java/edu/illinois/library/cantaloupe/script/Logger.java
+++ src/main/java/edu/illinois/library/cantaloupe/script/Logger.java
@@ -15,20 +15,40 @@ public final class Logger {
         LOGGER.trace(message);
     }
 
+    public static void trace(String message, Throwable throwable) {
+        LOGGER.trace(message, throwable);
+    }
+
     public static void debug(String message) {
         LOGGER.debug(message);
+    }
+
+    public static void debug(String message, Throwable throwable) {
+        LOGGER.debug(message, throwable);
     }
 
     public static void info(String message) {
         LOGGER.info(message);
     }
 
+    public static void info(String message, Throwable throwable) {
+        LOGGER.info(message, throwable);
+    }
+
     public static void warn(String message) {
         LOGGER.warn(message);
     }
 
+    public static void warn(String message, Throwable throwable) {
+        LOGGER.warn(message, throwable);
+    }
+
     public static void error(String message) {
         LOGGER.error(message);
+    }
+
+    public static void error(String message, Throwable throwable) {
+        LOGGER.error(message, throwable);
     }
 
     private Logger() {}
