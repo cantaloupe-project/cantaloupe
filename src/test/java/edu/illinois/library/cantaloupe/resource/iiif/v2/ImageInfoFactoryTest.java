cantaloupe-project/cantaloupe

commit d6c3b3d6f56ed6d923070dfc8f2a46975144df50

PdfBoxProcessor.readImageInfo() populates the Info with one subimage per page. These subimages are used for validating the request.

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageInfoFactory.java
@@ -38,20 +38,23 @@ final class ImageInfoFactory {
                             ServiceFeature.PROFILE_LINK_HEADER));
 
     /**
-     * @param imageURI  May be {@literal null}.
+     * @param imageURI       May be {@literal null}.
      * @param processor
      * @param info
-     * @param proxy     May be {@literal null}.
+     * @param infoImageIndex Index of the image in the {@link Info} argument's
+     *                       {@link Info#getImages()} list.
+     * @param proxy          May be {@literal null}.
      */
     ImageInfo<String,Object> newImageInfo(final String imageURI,
                                           final Processor processor,
                                           final Info info,
+                                          final int infoImageIndex,
                                           final DelegateProxy proxy) {
         final Configuration config = Configuration.getInstance();
 
         // We want to use the orientation-aware full size, which takes the
         // embedded orientation into account.
-        final Dimension virtualSize = info.getOrientationSize();
+        final Dimension virtualSize = info.getOrientationSize(infoImageIndex);
 
         // Create a Map instance, which will eventually be serialized to JSON
         // and returned in the response body.
@@ -89,13 +92,12 @@ final class ImageInfoFactory {
         final List<ImageInfo.Tile> tiles = new ArrayList<>();
         responseInfo.put("tiles", tiles);
 
-        final Info.Image firstImage =
-                info.getImages().get(0);
+        final Info.Image firstImage = info.getImages().get(0);
 
         // Find the virtual tile size based on the virtual full image size.
         final Dimension virtualTileSize = firstImage.getOrientationTileSize();
 
-        if (info.getImages().size() == 1 &&
+        if (info.getNumResolutions() == 1 &&
                 virtualTileSize.equals(virtualSize)) {
             uniqueTileSizes.add(
                     ImageInfoUtil.smallestTileSize(virtualSize, minTileSize));

src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageInfoFactoryTest.java
@@ -39,7 +39,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         ((FileProcessor) processor).setSourceFile(
                 TestUtil.getImage("jpg-rgb-594x522x8-baseline.jpg"));
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
     }
 
     @Override
@@ -62,7 +62,7 @@ public class ImageInfoFactoryTest extends BaseTest {
                 TestUtil.getImage("jpg-rotated.jpg"));
 
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
     }
 
     @Test
@@ -125,7 +125,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         config.setProperty(Key.IIIF_MIN_SIZE, 200);
 
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
         @SuppressWarnings("unchecked")
         List<ImageInfo.Size> sizes =
                 (List<ImageInfo.Size>) imageInfo.get("sizes");
@@ -142,7 +142,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         config.setProperty(Key.MAX_PIXELS, 10000);
 
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
         @SuppressWarnings("unchecked")
         List<ImageInfo.Size> sizes =
                 (List<ImageInfo.Size>) imageInfo.get("sizes");
@@ -188,7 +188,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         ((FileProcessor) processor).setSourceFile(
                 TestUtil.getImage("tif-rgb-1res-64x56x8-tiled-uncompressed.tif"));
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
 
         @SuppressWarnings("unchecked")
         List<ImageInfo.Tile> tiles =
@@ -231,7 +231,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         config.setProperty(Key.MAX_PIXELS, 100);
 
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
         List<?> profile = (List<?>) imageInfo.get("profile");
         assertTrue(((Map<?, ?>) profile.get(1)).get("maxArea").
                 equals(config.getInt(Key.MAX_PIXELS)));
@@ -243,7 +243,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         config.setProperty(Key.MAX_PIXELS, 0);
 
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), null);
+                imageUri, processor, processor.readImageInfo(), 0, null);
         List<?> profile = (List<?>) imageInfo.get("profile");
         assertFalse(((Map<?, ?>) profile.get(1)).containsKey("maxArea"));
     }
@@ -274,7 +274,7 @@ public class ImageInfoFactoryTest extends BaseTest {
         DelegateProxy proxy = service.newDelegateProxy(context);
 
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), proxy);
+                imageUri, processor, processor.readImageInfo(), 0, proxy);
 
         assertEquals("Copyright My Great Organization. All rights reserved.",
                 imageInfo.get("attribution"));

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageInfoFactory.java
@@ -5,23 +5,18 @@ import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.image.Info;
 import edu.illinois.library.cantaloupe.processor.Processor;
-import edu.illinois.library.cantaloupe.processor.ProcessorException;
 import edu.illinois.library.cantaloupe.resource.iiif.ImageInfoUtil;
 
 import java.awt.Dimension;
 
-class ImageInfoFactory {
+final class ImageInfoFactory {
 
-    /** Will be used to calculate a maximum scale factor. */
     private static final int MIN_SIZE = 64;
 
     ImageInfo newImageInfo(final String imageUri,
                            final Processor processor,
-                           final Info cacheInfo) throws ProcessorException {
-        // We want to use the orientation-aware full size, which takes the
-        // embedded orientation into account.
-        final Dimension virtualSize = cacheInfo.getOrientationSize();
-
+                           final Info.Image infoImage,
+                           final int numResolutions) {
         final ComplianceLevel complianceLevel = ComplianceLevel.getLevel(
                 processor.getSupportedFeatures(),
                 processor.getSupportedIIIF1Qualities(),
@@ -34,15 +29,13 @@ class ImageInfoFactory {
         // calculate a tile size close to MIN_TILE_SIZE_CONFIG_KEY pixels.
         // Otherwise, use the smallest multiple of the tile size above
         // MIN_TILE_SIZE_CONFIG_KEY of image resolution 0.
-        final Info.Image firstImage =
-                cacheInfo.getImages().get(0);
-        Dimension virtualTileSize = firstImage.getOrientationTileSize();
+        final Dimension virtualSize = infoImage.getOrientationSize();
+        Dimension virtualTileSize = infoImage.getOrientationTileSize();
 
-        if (cacheInfo.getImages().size() > 0) {
+        if (numResolutions > 0) {
             if (!virtualTileSize.equals(virtualSize)) {
-                virtualTileSize = ImageInfoUtil.smallestTileSize(virtualSize,
-                        firstImage.getOrientationTileSize(),
-                        minTileSize);
+                virtualTileSize = ImageInfoUtil.smallestTileSize(
+                        virtualSize, virtualTileSize, minTileSize);
             }
         }
 

src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageInfoFactoryTest.java
@@ -3,6 +3,7 @@ package edu.illinois.library.cantaloupe.resource.iiif.v1;
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.image.Format;
+import edu.illinois.library.cantaloupe.image.Info;
 import edu.illinois.library.cantaloupe.processor.FileProcessor;
 import edu.illinois.library.cantaloupe.processor.Processor;
 import edu.illinois.library.cantaloupe.processor.ProcessorFactory;
@@ -16,7 +17,7 @@ import static org.junit.Assert.*;
 public class ImageInfoFactoryTest extends BaseTest {
 
     private String imageUri;
-    private ImageInfo info;
+    private ImageInfo imageInfo;
     private Processor processor;
 
     @Before
@@ -31,8 +32,10 @@ public class ImageInfoFactoryTest extends BaseTest {
         ((FileProcessor) processor).setSourceFile(
                 TestUtil.getImage("jpg-rgb-594x522x8-baseline.jpg"));
 
-        info = new ImageInfoFactory().newImageInfo(imageUri, processor,
-                processor.readImageInfo());
+        Info info = processor.readImageInfo();
+        Info.Image infoImage = info.getImages().get(0);
+        imageInfo = new ImageInfoFactory().newImageInfo(imageUri, processor,
+                infoImage, info.getNumResolutions());
     }
 
     @Override
@@ -53,61 +56,63 @@ public class ImageInfoFactoryTest extends BaseTest {
         ((FileProcessor) processor).setSourceFile(
                 TestUtil.getImage("jpg-rotated.jpg"));
 
-        info = new ImageInfoFactory().newImageInfo(imageUri, processor,
-                processor.readImageInfo());
+        Info info = processor.readImageInfo();
+        Info.Image infoImage = info.getImages().get(0);
+        imageInfo = new ImageInfoFactory().newImageInfo(imageUri, processor,
+                infoImage, info.getNumResolutions());
     }
 
     @Test
     public void newImageInfoContext() {
         assertEquals("http://library.stanford.edu/iiif/image-api/1.1/context.json",
-                info.context);
+                imageInfo.context);
     }
 
     @Test
     public void newImageInfoId() {
-        assertEquals("http://example.org/bla", info.id);
+        assertEquals("http://example.org/bla", imageInfo.id);
     }
 
     @Test
     public void newImageInfoWidth() {
-        assertEquals(594, (long) info.width);
+        assertEquals(594, (long) imageInfo.width);
     }
 
     @Test
     public void newImageInfoWidthWithRotatedImage() throws Exception {
         setUpForRotatedImage();
-        assertEquals(64, (long) info.width);
+        assertEquals(64, (long) imageInfo.width);
     }
 
     @Test
     public void newImageInfoHeight() {
-        assertEquals(522, (long) info.height);
+        assertEquals(522, (long) imageInfo.height);
     }
 
     @Test
     public void newImageInfoHeightWithRotatedImage() throws Exception {
         setUpForRotatedImage();
-        assertEquals(56, (long) info.height);
+        assertEquals(56, (long) imageInfo.height);
     }
 
     @Test
     public void newImageInfoScaleFactors() {
-        assertEquals(4, info.scaleFactors.size());
-        assertEquals(1, (long) info.scaleFactors.get(0));
-        assertEquals(2, (long) info.scaleFactors.get(1));
-        assertEquals(4, (long) info.scaleFactors.get(2));
-        assertEquals(8, (long) info.scaleFactors.get(3));
+        assertEquals(4, imageInfo.scaleFactors.size());
+        assertEquals(1, (long) imageInfo.scaleFactors.get(0));
+        assertEquals(2, (long) imageInfo.scaleFactors.get(1));
+        assertEquals(4, (long) imageInfo.scaleFactors.get(2));
+        assertEquals(8, (long) imageInfo.scaleFactors.get(3));
     }
 
     @Test
     public void newImageInfoTileWidthWithUntiledImage() {
-        assertEquals(594, (long) info.tileWidth);
+        assertEquals(594, (long) imageInfo.tileWidth);
     }
 
     @Test
     public void newImageInfoTileWidthWithRotatedImage() throws Exception {
         setUpForRotatedImage();
-        assertEquals(64, (long) info.tileWidth);
+        assertEquals(64, (long) imageInfo.tileWidth);
     }
 
     @Test
@@ -115,21 +120,23 @@ public class ImageInfoFactoryTest extends BaseTest {
         processor.setSourceFormat(Format.TIF);
         ((FileProcessor) processor).setSourceFile(
                 TestUtil.getImage("tif-rgb-1res-64x56x8-tiled-uncompressed.tif"));
-        info = new ImageInfoFactory().newImageInfo(imageUri, processor,
-                processor.readImageInfo());
+        Info info = processor.readImageInfo();
+        Info.Image infoImage = info.getImages().get(0);
+        imageInfo = new ImageInfoFactory().newImageInfo(imageUri, processor,
+                infoImage, info.getNumResolutions());
 
-        assertEquals(64, (long) info.tileWidth);
+        assertEquals(64, (long) imageInfo.tileWidth);
     }
 
     @Test
     public void newImageInfoTileHeightWithUntiledImage() {
-        assertEquals(522, (long) info.tileHeight);
+        assertEquals(522, (long) imageInfo.tileHeight);
     }
 
     @Test
     public void newImageInfoTileHeightWithRotatedImage() throws Exception {
         setUpForRotatedImage();
-        assertEquals(56, (long) info.tileHeight);
+        assertEquals(56, (long) imageInfo.tileHeight);
     }
 
     @Test
@@ -137,27 +144,29 @@ public class ImageInfoFactoryTest extends BaseTest {
         processor.setSourceFormat(Format.TIF);
         ((FileProcessor) processor).setSourceFile(
                 TestUtil.getImage("tif-rgb-1res-64x56x8-tiled-uncompressed.tif"));
-        info = new ImageInfoFactory().newImageInfo(imageUri, processor,
-                processor.readImageInfo());
+        Info info = processor.readImageInfo();
+        Info.Image infoImage = info.getImages().get(0);
+        imageInfo = new ImageInfoFactory().newImageInfo(imageUri, processor,
+                infoImage, info.getNumResolutions());
 
-        assertEquals(64, (long) info.tileWidth);
-        assertEquals(56, (long) info.tileHeight);
+        assertEquals(64, (long) imageInfo.tileWidth);
+        assertEquals(56, (long) imageInfo.tileHeight);
     }
 
     @Test
     public void newImageInfoFormats() {
-        assertTrue(info.formats.contains("jpg"));
+        assertTrue(imageInfo.formats.contains("jpg"));
     }
 
     @Test
     public void newImageInfoQualities() {
-        assertTrue(info.qualities.contains("color"));
+        assertTrue(imageInfo.qualities.contains("color"));
     }
 
     @Test
     public void newImageInfoProfile() {
         assertEquals("http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level2",
-                info.profile);
+                imageInfo.profile);
     }
 
 }

commit 02059dc2973cafac721eb8360dc04bf4a0c39bba

Fix tiles calculation for single-subimage images with multiple resolution levels (regression introduced in d6c3b3d6f56ed6d923070dfc8f2a46975144df50)

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageInfoFactory.java
@@ -27,6 +27,11 @@ final class ImageInfoFactory {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(ImageInfoFactory.class);
 
+    /**
+     * Will be used if {@link Key#IIIF_MIN_TILE_SIZE} is not set.
+     */
+    private static final int DEFAULT_MIN_TILE_SIZE = 512;
+
     private static final Set<ServiceFeature> SUPPORTED_SERVICE_FEATURES =
             Collections.unmodifiableSet(
                     EnumSet.of(ServiceFeature.SIZE_BY_CONFINED_WIDTH_HEIGHT,
@@ -80,10 +85,11 @@ final class ImageInfoFactory {
 
         // tiles -- this is not a canonical listing of tiles that are
         // actually encoded in the image, but rather a hint to the client as
-        // to what can be delivered efficiently.
+        // to what is efficient to deliver.
         final Set<Dimension> uniqueTileSizes = new HashSet<>();
 
-        final int minTileSize = config.getInt(Key.IIIF_MIN_TILE_SIZE, 1024);
+        final int minTileSize = config.getInt(Key.IIIF_MIN_TILE_SIZE,
+                DEFAULT_MIN_TILE_SIZE);
 
         // Find a tile width and height. If the image is not tiled,
         // calculate a tile size close to IIIF_MIN_TILE_SIZE pixels.
@@ -94,19 +100,17 @@ final class ImageInfoFactory {
 
         final Info.Image firstImage = info.getImages().get(0);
 
-        // Find the virtual tile size based on the virtual full image size.
-        final Dimension virtualTileSize = firstImage.getOrientationTileSize();
-
-        if (info.getNumResolutions() == 1 &&
-                virtualTileSize.equals(virtualSize)) {
-            uniqueTileSizes.add(
-                    ImageInfoUtil.smallestTileSize(virtualSize, minTileSize));
-        } else {
+        if (info.getImages().size() > 1) {
+            // This branch is used for images that have multiple physical
+            // embedded subimages, such as pyramidal tiled TIFF.
             for (Info.Image image : info.getImages()) {
                 uniqueTileSizes.add(
                         ImageInfoUtil.smallestTileSize(virtualSize,
                                 image.getOrientationTileSize(), minTileSize));
             }
+        } else {
+            uniqueTileSizes.add(ImageInfoUtil.smallestTileSize(
+                    virtualSize, minTileSize));
         }
         for (Dimension uniqueTileSize : uniqueTileSizes) {
             final ImageInfo.Tile tile = new ImageInfo.Tile();
@@ -144,14 +148,14 @@ final class ImageInfoFactory {
         }
 
         // qualities
-        Set<String> qualityStrings = new HashSet<>();
+        final Set<String> qualityStrings = new HashSet<>();
         for (Quality quality : processor.getSupportedIIIF2Qualities()) {
             qualityStrings.add(quality.toString().toLowerCase());
         }
         profileMap.put("qualities", qualityStrings);
 
         // supports
-        Set<String> featureStrings = new HashSet<>();
+        final Set<String> featureStrings = new HashSet<>();
         for (Feature feature : processor.getSupportedFeatures()) {
             featureStrings.add(feature.getName());
         }

src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageInfoFactoryTest.java
@@ -3,6 +3,7 @@ package edu.illinois.library.cantaloupe.resource.iiif.v2;
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.image.Format;
+import edu.illinois.library.cantaloupe.image.Info;
 import edu.illinois.library.cantaloupe.processor.FileProcessor;
 import edu.illinois.library.cantaloupe.processor.Processor;
 import edu.illinois.library.cantaloupe.processor.ProcessorFactory;
@@ -157,7 +158,7 @@ public class ImageInfoFactoryTest extends BaseTest {
     }
 
     @Test
-    public void testNewImageInfoTilesWithUntiledImage() {
+    public void testNewImageInfoTilesWithUntiledMonoResolutionImage() {
         @SuppressWarnings("unchecked")
         List<ImageInfo.Tile> tiles =
                 (List<ImageInfo.Tile>) imageInfo.get("tiles");
@@ -173,6 +174,30 @@ public class ImageInfoFactoryTest extends BaseTest {
     }
 
     @Test
+    public void testNewImageInfoTilesWithUntiledMultiResolutionImage() {
+        Info info = Info.builder()
+                .withSize(2000, 2000)
+                .withNumResolutions(3)
+                .build();
+        imageInfo = new ImageInfoFactory().newImageInfo(
+                imageUri, processor, info, 0, null);
+
+        @SuppressWarnings("unchecked")
+        List<ImageInfo.Tile> tiles =
+                (List<ImageInfo.Tile>) imageInfo.get("tiles");
+        assertEquals(1, tiles.size());
+        assertEquals(1000, (long) tiles.get(0).width);
+        assertEquals(1000, (long) tiles.get(0).height);
+
+        assertEquals(5, (long) tiles.get(0).scaleFactors.size());
+        assertEquals(1, (long) tiles.get(0).scaleFactors.get(0));
+        assertEquals(2, (long) tiles.get(0).scaleFactors.get(1));
+        assertEquals(4, (long) tiles.get(0).scaleFactors.get(2));
+        assertEquals(8, (long) tiles.get(0).scaleFactors.get(3));
+        assertEquals(16, (long) tiles.get(0).scaleFactors.get(4));
+    }
+
+    @Test
     public void testNewImageInfoTilesWithRotatedImage() throws Exception {
         setUpForRotatedImage();
         @SuppressWarnings("unchecked")
@@ -185,10 +210,12 @@ public class ImageInfoFactoryTest extends BaseTest {
     @Test
     public void testNewImageInfoTilesWithTiledImage() throws Exception {
         processor.setSourceFormat(Format.TIF);
-        ((FileProcessor) processor).setSourceFile(
-                TestUtil.getImage("tif-rgb-1res-64x56x8-tiled-uncompressed.tif"));
+        Info info = Info.builder()
+                .withSize(64, 56)
+                .withTileSize(64, 56)
+                .build();
         imageInfo = new ImageInfoFactory().newImageInfo(
-                imageUri, processor, processor.readImageInfo(), 0, null);
+                imageUri, processor, info, 0, null);
 
         @SuppressWarnings("unchecked")
         List<ImageInfo.Tile> tiles =