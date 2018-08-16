cantaloupe-project/cantaloupe

commit 396c9eb195587a2ff19327a8520831bba7f2cef0

Support a port in the X-Forwarded-Host header

src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -132,11 +132,18 @@ public abstract class AbstractResource extends ServerResource {
             // Try to use X-Forwarded-* headers.
             // N.B. Header values here may be comma-separated lists when
             // operating behind a chain of reverse proxies.
+
+            // This may or may not include a port number. If it does, it should
+            // be used if X-Forwarded-Port was not sent.
             final String hostHeader = requestHeaders.getFirstValue(
                     "X-Forwarded-Host", true, null);
             if (hostHeader != null) {
                 final String hostStr = hostHeader.split(",")[0].trim();
-                newRef.setHostDomain(hostStr);
+                String[] parts = hostStr.split(":");
+                newRef.setHostDomain(parts[0]);
+                if (parts.length > 1) {
+                    newRef.setHostPort(Integer.parseInt(parts[1]));
+                }
             }
             
             final String protocolHeader = requestHeaders.getFirstValue(

src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v1/InformationResourceTest.java
@@ -365,6 +365,41 @@ public class InformationResourceTest extends ResourceTest {
     }
 
     @Test
+    public void testGETURIsInJSONWithProxyHeadersWithHostPortAndPort() throws Exception {
+        client = newClient("/" + IMAGE + "/info.json");
+        client.getHeaders().set("X-Forwarded-Proto", "HTTP");
+        client.getHeaders().set("X-Forwarded-Host", "example.org:7500");
+        client.getHeaders().set("X-Forwarded-Port", "8080");
+        client.getHeaders().set("X-Forwarded-Path", "/cats");
+        client.getHeaders().set(
+                AbstractResource.PUBLIC_IDENTIFIER_HEADER, "originalID");
+        Response response = client.send();
+
+        String json = response.getBodyAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        ImageInfo info = mapper.readValue(json, ImageInfo.class);
+        assertEquals("http://example.org:8080/cats" +
+                RestletApplication.IIIF_1_PATH + "/originalID", info.id);
+    }
+
+    @Test
+    public void testGETURIsInJSONWithProxyHeadersWithHostPortAndNoPort() throws Exception {
+        client = newClient("/" + IMAGE + "/info.json");
+        client.getHeaders().set("X-Forwarded-Proto", "HTTP");
+        client.getHeaders().set("X-Forwarded-Host", "example.org:7500");
+        client.getHeaders().set("X-Forwarded-Path", "/cats");
+        client.getHeaders().set(
+                AbstractResource.PUBLIC_IDENTIFIER_HEADER, "originalID");
+        Response response = client.send();
+
+        String json = response.getBodyAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        ImageInfo info = mapper.readValue(json, ImageInfo.class);
+        assertEquals("http://example.org:7500/cats" +
+                RestletApplication.IIIF_1_PATH + "/originalID", info.id);
+    }
+
+    @Test
     public void testGETBaseURIOverridesProxyHeaders() throws Exception {
         Configuration config = Configuration.getInstance();
         config.setProperty(Key.BASE_URI, "https://example.net/");

src/test/java/edu/illinois/library/cantaloupe/resource/iiif/v2/InformationResourceTest.java
@@ -365,6 +365,41 @@ public class InformationResourceTest extends ResourceTest {
     }
 
     @Test
+    public void testGETURIsInJSONWithProxyHeadersWithHostPortAndPort() throws Exception {
+        client = newClient("/" + IMAGE + "/info.json");
+        client.getHeaders().set("X-Forwarded-Proto", "HTTP");
+        client.getHeaders().set("X-Forwarded-Host", "example.org:7500");
+        client.getHeaders().set("X-Forwarded-Port", "8080");
+        client.getHeaders().set("X-Forwarded-Path", "/cats");
+        client.getHeaders().set(
+                AbstractResource.PUBLIC_IDENTIFIER_HEADER, "originalID");
+        Response response = client.send();
+
+        String json = response.getBodyAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        ImageInfo<?, ?> info = mapper.readValue(json, ImageInfo.class);
+        assertEquals("http://example.org:8080/cats" +
+                RestletApplication.IIIF_2_PATH + "/originalID", info.get("@id"));
+    }
+
+    @Test
+    public void testGETURIsInJSONWithProxyHeadersWithHostPortAndNoPort() throws Exception {
+        client = newClient("/" + IMAGE + "/info.json");
+        client.getHeaders().set("X-Forwarded-Proto", "HTTP");
+        client.getHeaders().set("X-Forwarded-Host", "example.org:7500");
+        client.getHeaders().set("X-Forwarded-Path", "/cats");
+        client.getHeaders().set(
+                AbstractResource.PUBLIC_IDENTIFIER_HEADER, "originalID");
+        Response response = client.send();
+
+        String json = response.getBodyAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        ImageInfo<?, ?> info = mapper.readValue(json, ImageInfo.class);
+        assertEquals("http://example.org:7500/cats" +
+                RestletApplication.IIIF_2_PATH + "/originalID", info.get("@id"));
+    }
+
+    @Test
     public void testGETBaseURIOverridesProxyHeaders() throws Exception {
         Configuration config = Configuration.getInstance();
         config.setProperty(Key.BASE_URI, "https://example.net/");

commit 9031018270e5d9772bf615f079d3e35a18d87946

Fix a regression from 396c9eb1 when the X-Forwarded-Host header contains a scheme

src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -138,7 +138,13 @@ public abstract class AbstractResource extends ServerResource {
             final String hostHeader = requestHeaders.getFirstValue(
                     "X-Forwarded-Host", true, null);
             if (hostHeader != null) {
-                final String hostStr = hostHeader.split(",")[0].trim();
+                String hostStr = hostHeader;
+                if (hostStr.startsWith("http://")) {
+                    hostStr = hostStr.substring(7, hostHeader.length());
+                } else if (hostHeader.startsWith("https://")) {
+                    hostStr = hostStr.substring(8, hostHeader.length());
+                }
+                hostStr = hostStr.split(",")[0].trim();
                 String[] parts = hostStr.split(":");
                 newRef.setHostDomain(parts[0]);
                 if (parts.length > 1) {