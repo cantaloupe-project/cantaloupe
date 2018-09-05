ab0116fdb4076633bb1c7313adb2c4567c0b2e2d
Alex Dolski <alexd@n-ary.net>
2018-09-05 13:50:01 -0500

    Use CopyOnWriteArrayList instead of synchronization

--- src/main/java/edu/illinois/library/cantaloupe/resource/api/TaskMonitor.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/api/TaskMonitor.java
@@ -1,29 +1,29 @@
 package edu.illinois.library.cantaloupe.resource.api;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * Retains tasks submitted via
- * {@link edu.illinois.library.cantaloupe.resource.api.TasksResource} in a list
- * for observation by
- * {@link edu.illinois.library.cantaloupe.resource.api.TaskResource}.
+ * Retains tasks submitted via {@link
+ * edu.illinois.library.cantaloupe.resource.api.TasksResource} for observation
+ * by {@link edu.illinois.library.cantaloupe.resource.api.TaskResource}.
  */
 final class TaskMonitor {
 
-    private final List<APITask<?>> tasks = new ArrayList<>();
+    private final List<APITask<?>> tasks = new CopyOnWriteArrayList<>();
 
-    synchronized void add(APITask<?> task) {
+    void add(APITask<?> task) {
         tasks.add(task);
     }
 
-    synchronized APITask<?> get(final UUID uuid) {
-        Optional<APITask<?>> task = tasks.stream().
-                filter(t -> t.getUUID().equals(uuid)).
-                findFirst();
-        return task.orElse(null);
+    APITask<?> get(final UUID uuid) {
+        return tasks.stream()
+                .filter(t -> t.getUUID().equals(uuid))
+                .findFirst()
+                .orElse(null);
     }
+
+
 
 }
