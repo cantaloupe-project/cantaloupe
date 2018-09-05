ab7bfc35f6b7d0421255dfc7282948a002fdd10d
Alex Dolski <alexd@n-ary.net>
2018-09-05 15:59:35 -0500

    Refactoring

--- src/main/java/edu/illinois/library/cantaloupe/logging/ApplicationLogFilter.java
+++ src/main/java/edu/illinois/library/cantaloupe/logging/ApplicationLogFilter.java
@@ -11,35 +11,34 @@ import ch.qos.logback.core.spi.FilterReply;
 public class ApplicationLogFilter extends Filter<ILoggingEvent> {
 
     public FilterReply decide(ILoggingEvent event) {
-        // Filter out useless PDFBox log messages. Would be better to reduce
-        // their level, but we can't here...
+        // PDFBox log messages are unfortunately not calibrated very well.
+        // Would be better to reduce their level, but we can't here...
         if (event.getLoggerName().startsWith("org.apache.pdfbox") &&
                 event.getLevel().equals(Level.WARN)) {
             return FilterReply.DENY;
         }
-        // Filter out debug messages from the embedded Jetty server as they
-        // totally overwhelm the debug log.
-        if (event.getLoggerName().startsWith("org.eclipse.jetty") &&
+        // Reject Jetty debug messages as they totally overwhelm the debug log.
+        else if (event.getLoggerName().startsWith("org.eclipse.jetty") &&
                 Level.DEBUG.isGreaterOrEqual(event.getLevel())) {
             return FilterReply.DENY;
         }
-        // Jetty wants to log static content accesses at info level. We don't.
-        if (event.getLoggerName().equals(org.eclipse.jetty.server.ResourceService.class.getName()) &&
+        // Reject Jetty static content access messages.
+        else if ("org.eclipse.jetty.server.ResourceService".equals(event.getLoggerName()) &&
                 Level.INFO.isGreaterOrEqual(event.getLevel())) {
             return FilterReply.DENY;
         }
         // Reject Jetty access log messages.
-        if (event.getLoggerName().equals("LogService")) {
+        else if (event.getLoggerName().equals("LogService")) {
             return FilterReply.DENY;
         }
         // Reject Velocity debug messages.
-        if (event.getLoggerName().startsWith("org.apache.velocity") &&
+        else if (event.getLoggerName().startsWith("org.apache.velocity") &&
                 Level.DEBUG.isGreaterOrEqual(event.getLevel())) {
             return FilterReply.DENY;
         }
         // The Amazon S3 client wraps an Apache HTTP client which is extremely
         // verbose.
-        if (event.getLoggerName().equals("org.apache.http.wire") &&
+        else if ("org.apache.http.wire".equals(event.getLoggerName()) &&
                 Level.DEBUG.isGreaterOrEqual(event.getLevel())) {
             return FilterReply.DENY;
         }
