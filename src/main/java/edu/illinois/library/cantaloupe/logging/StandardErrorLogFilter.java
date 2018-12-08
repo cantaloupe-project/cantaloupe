f401d173e239b790b095fc958e732fc81f232dd1
Alex Dolski <alexd@n-ary.net>
2018-12-08 12:37:52 -0600

    Improve efficiency

--- src/main/java/edu/illinois/library/cantaloupe/logging/StandardErrorLogFilter.java
+++ src/main/java/edu/illinois/library/cantaloupe/logging/StandardErrorLogFilter.java
@@ -5,28 +5,20 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Filters out log messages with a level lower than WARN.
  */
 public class StandardErrorLogFilter extends Filter<ILoggingEvent> {
 
-    private static final Set<Level> EVENTS_TO_KEEP =
-            new HashSet<>(Arrays.asList(Level.WARN, Level.ERROR));
-
     public FilterReply decide(ILoggingEvent event) {
         if (!isStarted()) {
             return FilterReply.NEUTRAL;
         }
-
-        if (EVENTS_TO_KEEP.contains(event.getLevel())) {
+        if (Level.ERROR.equals(event.getLevel()) ||
+                Level.WARN.equals(event.getLevel())) {
             return FilterReply.NEUTRAL;
-        } else {
-            return FilterReply.DENY;
         }
+        return FilterReply.DENY;
     }
 
 }
