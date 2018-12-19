cantaloupe-project/cantaloupe

commit cfc11d1eca2281562fba840d54a8cc7b1af36a09

Remove StringUtils.filesystemSafe() and use md5() instead

src/main/java/edu/illinois/library/cantaloupe/operation/OperationList.java
@@ -549,7 +549,7 @@ public final class OperationList implements Comparable<OperationList>,
             extension = "." + encode.getFormat().getPreferredExtension();
         }
 
-        return StringUtils.filesystemSafe(idStr) + "_" + opsString + extension;
+        return StringUtils.md5(idStr) + "_" + opsString + extension;
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/cache/FilesystemCache.java
@@ -431,7 +431,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
     static Path infoFile(final Identifier identifier) {
         return rootInfoPath()
                 .resolve(hashedPathFragment(identifier.toString()))
-                .resolve(StringUtils.filesystemSafe(identifier.toString())
+                .resolve(StringUtils.md5(identifier.toString())
                         + INFO_EXTENSION);
     }
 
@@ -443,7 +443,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
     static Path infoTempFile(final Identifier identifier) {
         return rootInfoPath()
                 .resolve(hashedPathFragment(identifier.toString()))
-                .resolve(StringUtils.filesystemSafe(identifier.toString())
+                .resolve(StringUtils.md5(identifier.toString())
                         + INFO_EXTENSION + tempFileSuffix());
     }
 
@@ -454,7 +454,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
     static Path sourceImageFile(Identifier identifier) {
         return rootSourceImagePath()
                 .resolve(hashedPathFragment(identifier.toString()))
-                .resolve(StringUtils.filesystemSafe(identifier.toString()));
+                .resolve(StringUtils.md5(identifier.toString()));
     }
 
     /**
@@ -465,7 +465,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
     static Path sourceImageTempFile(Identifier identifier) {
         return rootSourceImagePath()
                 .resolve(hashedPathFragment(identifier.toString()))
-                .resolve(StringUtils.filesystemSafe(identifier.toString())
+                .resolve(StringUtils.md5(identifier.toString())
                         + tempFileSuffix());
     }
 
@@ -514,7 +514,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
         final Path cachePath = rootDerivativeImagePath().resolve(
                 hashedPathFragment(identifier.toString()));
         final String expectedNamePrefix =
-                StringUtils.filesystemSafe(identifier.toString());
+                StringUtils.md5(identifier.toString());
         try {
             return Files.list(cachePath)
                     .filter(p -> p.getFileName().toString().startsWith(expectedNamePrefix))

src/main/java/edu/illinois/library/cantaloupe/util/StringUtils.java
@@ -55,23 +55,6 @@ public final class StringUtils {
     }
 
     /**
-     * Returns a filename-safe string guaranteed to uniquely represent the
-     * given string.
-     *
-     * @return Filename-safe representation of the given string.
-     */
-    public static String filesystemSafe(String str) {
-        try {
-            final MessageDigest digest = MessageDigest.getInstance("MD5");
-            digest.update(str.getBytes(Charset.forName("UTF8")));
-            return Hex.encodeHexString(digest.digest());
-        } catch (NoSuchAlgorithmException e) {
-            LOGGER.error("filenameSafe(): {}", e.getMessage());
-        }
-        return str; // This should never hit.
-    }
-
-    /**
      * @param str String to hash.
      * @return    MD5 checksum.
      */

src/test/java/edu/illinois/library/cantaloupe/cache/FilesystemCacheTest.java
@@ -148,7 +148,7 @@ public class FilesystemCacheTest extends AbstractCacheTest {
                 pathname,
                 "info",
                 FilesystemCache.hashedPathFragment(identifier.toString()),
-                StringUtils.filesystemSafe(identifier.toString()) + ".json");
+                StringUtils.md5(identifier.toString()) + ".json");
         assertEquals(expected, infoFile(identifier));
     }
 
@@ -161,7 +161,7 @@ public class FilesystemCacheTest extends AbstractCacheTest {
                 pathname,
                 "info",
                 FilesystemCache.hashedPathFragment(identifier.toString()),
-                StringUtils.filesystemSafe(identifier.toString()) + ".json"
+                StringUtils.md5(identifier.toString()) + ".json"
                         + FilesystemCache.tempFileSuffix());
         assertEquals(expected, infoTempFile(identifier));
     }
@@ -175,7 +175,7 @@ public class FilesystemCacheTest extends AbstractCacheTest {
                 pathname,
                 "source",
                 FilesystemCache.hashedPathFragment(identifier.toString()),
-                StringUtils.filesystemSafe(identifier.toString()));
+                StringUtils.md5(identifier.toString()));
         assertEquals(expected, sourceImageFile(identifier));
     }
 
@@ -188,7 +188,7 @@ public class FilesystemCacheTest extends AbstractCacheTest {
                 pathname,
                 "source",
                 FilesystemCache.hashedPathFragment(identifier.toString()),
-                StringUtils.filesystemSafe(identifier.toString())
+                StringUtils.md5(identifier.toString())
                         + FilesystemCache.tempFileSuffix());
         assertEquals(expected, sourceImageTempFile(identifier));
     }

src/test/java/edu/illinois/library/cantaloupe/util/StringUtilsTest.java
@@ -26,12 +26,6 @@ public class StringUtilsTest extends BaseTest {
     }
 
     @Test
-    public void testFilenameSafe() {
-        assertEquals("0832c1202da8d382318e329a7c133ea0",
-                StringUtils.filesystemSafe("cats"));
-    }
-
-    @Test
     public void testMd5() {
         assertEquals("0832C1202DA8D382318E329A7C133EA0",
                 StringUtils.md5("cats"));

commit 4989a39fb2e2bbe0aec4fcb7bb97ae9f3766c1d2

Fix a regression from cfc11d1eca2281562fba840d54a8cc7b1af36a09

src/main/java/edu/illinois/library/cantaloupe/operation/OperationList.java
@@ -549,7 +549,7 @@ public final class OperationList implements Comparable<OperationList>,
             extension = "." + encode.getFormat().getPreferredExtension();
         }
 
-        return StringUtils.md5(idStr) + "_" + opsString + extension;
+        return StringUtils.md5(idStr).toLowerCase() + "_" + opsString + extension;
     }
 
     /**