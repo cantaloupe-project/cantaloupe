cantaloupe-project/cantaloupe

commit dd160752a907b909b7a15212d1fa09f09ac270e0

Address some test failures in Windows

src/main/java/edu/illinois/library/cantaloupe/cache/FilesystemCache.java
@@ -336,7 +336,7 @@ class FilesystemCache implements SourceCache, DerivativeCache {
         } catch (NoSuchAlgorithmException e) {
             LOGGER.error(e.getMessage(), e);
         }
-        return String.join("/", components);
+        return String.join(File.separator, components);
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/processor/ProcessorFactory.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,20 +20,35 @@ public final class ProcessorFactory {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(ProcessorFactory.class);
 
+    private static final Set<Class<? extends Processor>> ALL_PROCESSOR_IMPLS =
+            Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+                    FfmpegProcessor.class,
+                    GraphicsMagickProcessor.class,
+                    ImageMagickProcessor.class,
+                    JaiProcessor.class,
+                    Java2dProcessor.class,
+                    KakaduDemoProcessor.class,
+                    KakaduNativeProcessor.class,
+                    OpenJpegProcessor.class,
+                    PdfBoxProcessor.class)));
+
+    private static final Set<Processor> ALL_PROCESSORS = new HashSet<>();
+
     private SelectionStrategy selectionStrategy =
             SelectionStrategy.fromConfiguration();
 
-    public static Set<Processor> getAllProcessors() {
-        return new HashSet<>(Arrays.asList(
-                new FfmpegProcessor(),
-                new GraphicsMagickProcessor(),
-                new ImageMagickProcessor(),
-                new JaiProcessor(),
-                new Java2dProcessor(),
-                new KakaduDemoProcessor(),
-                new KakaduNativeProcessor(),
-                new OpenJpegProcessor(),
-                new PdfBoxProcessor()));
+    public static synchronized Set<Processor> getAllProcessors() {
+        if (ALL_PROCESSORS.isEmpty()) {
+            for (Class<? extends Processor> class_ : ALL_PROCESSOR_IMPLS) {
+                try {
+                    ALL_PROCESSORS.add(class_.getDeclaredConstructor().newInstance());
+                } catch (Exception e) {
+                    // This exception is safe to swallow as it will be thrown
+                    // and handled elsewhere.
+                }
+            }
+        }
+        return Collections.unmodifiableSet(ALL_PROCESSORS);
     }
 
     public SelectionStrategy getSelectionStrategy() {

src/main/java/edu/illinois/library/cantaloupe/source/FilesystemSource.java
@@ -7,6 +7,7 @@ import edu.illinois.library.cantaloupe.image.Identifier;
 import edu.illinois.library.cantaloupe.image.MediaType;
 import edu.illinois.library.cantaloupe.script.DelegateMethod;
 import edu.illinois.library.cantaloupe.util.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -178,8 +179,14 @@ class FilesystemSource extends AbstractSource
      * @return Sanitized identifier.
      */
     private Identifier sanitizedIdentifier() {
+        String idStr = identifier.toString();
+        if (SystemUtils.IS_OS_WINDOWS) {
+            idStr = idStr.replace("/", "\\");
+        } else {
+            idStr = idStr.replace("\\", "/");
+        }
         final String sanitized = StringUtils.sanitize(
-                identifier.toString(),
+                idStr,
                 UNIX_PATH_SEPARATOR + "..",
                 ".." + UNIX_PATH_SEPARATOR,
                 WINDOWS_PATH_SEPARATOR + "..",

src/test/java/edu/illinois/library/cantaloupe/async/AuditableFutureTaskTest.java
@@ -90,12 +90,12 @@ public class AuditableFutureTaskTest extends BaseTest {
             return null;
         });
         ThreadPool.getInstance().submit(task);
-        Thread.sleep(10);
+        Thread.sleep(20);
         assertEquals(TaskStatus.RUNNING, task.getStatus());
     }
 
     @Test
-    public void testRunSetsStatusUponSuccess() throws Exception {
+    public void testRunSetsStatusUponSuccess() {
         AuditableFutureTask<?> task = new AuditableFutureTask<>(() -> "");
         task.run();
 

src/test/java/edu/illinois/library/cantaloupe/async/TaskQueueTest.java
@@ -32,7 +32,7 @@ public class TaskQueueTest extends BaseTest {
         instance.submit(future2);
         instance.submit(future3);
 
-        Thread.sleep(20);
+        Thread.sleep(40);
 
         // The first has been taken out and is running.
         assertEquals(2, instance.queuedTasks().size());

src/test/java/edu/illinois/library/cantaloupe/async/ThreadPoolTest.java
@@ -45,7 +45,7 @@ public class ThreadPoolTest extends BaseTest {
             atomicInt.incrementAndGet();
             return null;
         });
-        Thread.sleep(10); // wait for it to happen
+        Thread.sleep(20); // wait for it to happen
         assertEquals(1, atomicInt.get());
     }
 
@@ -56,7 +56,7 @@ public class ThreadPoolTest extends BaseTest {
             atomicInt.incrementAndGet();
             return null;
         }, ThreadPool.Priority.HIGH);
-        Thread.sleep(10); // wait for it to happen
+        Thread.sleep(20); // wait for it to happen
         assertEquals(1, atomicInt.get());
     }
 

src/test/java/edu/illinois/library/cantaloupe/cache/FilesystemCacheTest.java
@@ -21,6 +21,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -51,7 +52,13 @@ public class FilesystemCacheTest extends AbstractCacheTest {
 
     @After
     public void tearDown() throws IOException {
-        Files.walkFileTree(fixturePath, new DeletingFileVisitor());
+        try {
+            Files.walkFileTree(fixturePath, new DeletingFileVisitor());
+        } catch (DirectoryNotEmptyException e) {
+            // This happens in Windows 7 (maybe other versions?) sometimes; not
+            // sure why, but it shouldn't result in a test failure.
+            System.err.println(e.getMessage());
+        }
     }
 
     @Override

src/test/java/edu/illinois/library/cantaloupe/config/PropertiesDocumentTest.java
@@ -173,16 +173,17 @@ public class PropertiesDocumentTest extends BaseTest {
         try {
             instance.save(tempFile);
 
-            final String expected = "# This is a test document\n" +
-                    "#with another comment\n" +
-                    "! and a different kind of comment\n" +
-                    "\n" +
-                    "key1=newValue\n" +
-                    "key2 = value=value\n" +
-                    "  key3 = value\\\\value\n" +
-                    "key4:newValue\n" +
-                    "key5 : value\n" +
-                    "newKey=value\n";
+            final String sep = System.lineSeparator();
+            final String expected = "# This is a test document" + sep +
+                    "#with another comment" + sep +
+                    "! and a different kind of comment" + sep +
+                    sep +
+                    "key1=newValue" + sep +
+                    "key2 = value=value" + sep +
+                    "  key3 = value\\\\value" + sep +
+                    "key4:newValue" + sep +
+                    "key5 : value" + sep +
+                    "newKey=value" + sep;
 
             byte[] bytes = Files.readAllBytes(tempFile);
             String actual = new String(bytes);

commit e96c45bc523edc3edde0c03957d61c1ccf64c8fd

Add missing java.io.File import (#270)
Fixes breakage introduced in dd160752a907b909b7a15212d1fa09f09ac270e0

src/main/java/edu/illinois/library/cantaloupe/cache/FilesystemCache.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;