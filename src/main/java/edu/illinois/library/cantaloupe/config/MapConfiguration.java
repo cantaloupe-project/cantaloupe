cantaloupe-project/cantaloupe

commit dada7e2e48a239eb0f94088b1f668b6939dfca8c

Move Configuration.toMap() to FileConfiguration

src/main/java/edu/illinois/library/cantaloupe/config/MapConfiguration.java
@@ -2,9 +2,7 @@ package edu.illinois.library.cantaloupe.config;
 
 import edu.illinois.library.cantaloupe.util.StringUtils;
 
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -161,11 +159,6 @@ class MapConfiguration implements Configuration {
     @Override
     public void setProperty(String key, Object value) {
         configuration.put(key, value);
-    }
-
-    @Override
-    public Map<String, Object> toMap() {
-        return Collections.unmodifiableMap(configuration);
     }
 
 }

src/main/java/edu/illinois/library/cantaloupe/resource/admin/ConfigurationResource.java
@@ -2,6 +2,8 @@ package edu.illinois.library.cantaloupe.resource.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.illinois.library.cantaloupe.config.Configuration;
+import edu.illinois.library.cantaloupe.config.ConfigurationProvider;
+import edu.illinois.library.cantaloupe.config.FileConfiguration;
 import edu.illinois.library.cantaloupe.http.Method;
 import edu.illinois.library.cantaloupe.http.Status;
 import edu.illinois.library.cantaloupe.resource.JacksonRepresentation;
@@ -9,7 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -43,9 +47,16 @@ public class ConfigurationResource extends AbstractAdminResource {
         getResponse().setHeader("Content-Type",
                 "application/json;charset=UTF-8");
 
-        Configuration config = Configuration.getInstance();
-        new JacksonRepresentation(config.toMap())
-                .write(getResponse().getOutputStream());
+        Map<String,Object> map                   = Collections.emptyMap();
+        final ConfigurationProvider provider     = (ConfigurationProvider) Configuration.getInstance();
+        final List<Configuration> wrappedConfigs = provider.getWrappedConfigurations();
+        for (Configuration config : wrappedConfigs) {
+            if (config instanceof FileConfiguration) {
+                map = ((FileConfiguration) config).toMap();
+            }
+        }
+
+        new JacksonRepresentation(map).write(getResponse().getOutputStream());
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/resource/api/ConfigurationResource.java
@@ -2,6 +2,8 @@ package edu.illinois.library.cantaloupe.resource.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.illinois.library.cantaloupe.config.Configuration;
+import edu.illinois.library.cantaloupe.config.ConfigurationProvider;
+import edu.illinois.library.cantaloupe.config.FileConfiguration;
 import edu.illinois.library.cantaloupe.http.Method;
 import edu.illinois.library.cantaloupe.http.Status;
 import edu.illinois.library.cantaloupe.resource.JacksonRepresentation;
@@ -9,7 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ConfigurationResource extends AbstractAPIResource {
@@ -39,9 +43,16 @@ public class ConfigurationResource extends AbstractAPIResource {
         getResponse().setHeader("Content-Type",
                 "application/json;charset=UTF-8");
 
-        Configuration config = Configuration.getInstance();
-        new JacksonRepresentation(config.toMap())
-                .write(getResponse().getOutputStream());
+        Map<String,Object> map                   = Collections.emptyMap();
+        final ConfigurationProvider provider     = (ConfigurationProvider) Configuration.getInstance();
+        final List<Configuration> wrappedConfigs = provider.getWrappedConfigurations();
+        for (Configuration config : wrappedConfigs) {
+            if (config instanceof FileConfiguration) {
+                map = ((FileConfiguration) config).toMap();
+            }
+        }
+
+        new JacksonRepresentation(map).write(getResponse().getOutputStream());
     }
 
     /**

src/main/java/edu/illinois/library/cantaloupe/config/Configuration.java
@@ -3,10 +3,7 @@ package edu.illinois.library.cantaloupe.config;
 import edu.illinois.library.cantaloupe.util.StringUtils;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
@@ -295,22 +292,5 @@ public interface Configuration {
     }
 
     void setProperty(String key, Object value);
-
-    /**
-     * This default implementation uses the {@link Iterator} returned by {@link
-     * #getKeys} in conjunction with {@link #getProperty(String)} to build a
-     * map. Implementations should override if they can do it more efficiently.
-     *
-     * @return Configuration keys and values in a read-only map.
-     */
-    default Map<String,Object> toMap() {
-        final Map<String,Object> map = new LinkedHashMap<>();
-        final Iterator<String> keys = getKeys();
-        while (keys.hasNext()) {
-            final String key = keys.next();
-            map.put(key, getProperty(key));
-        }
-        return Collections.unmodifiableMap(map);
-    }
 
 }

src/main/java/edu/illinois/library/cantaloupe/config/ConfigurationProvider.java
@@ -116,7 +116,7 @@ public final class ConfigurationProvider implements Configuration {
     @Override
     public Iterator<String> getKeys() {
         final List<Iterator<String>> iterators = wrappedConfigs.stream()
-                .map(c -> c.toMap().keySet().iterator())
+                .map(Configuration::getKeys)
                 .collect(Collectors.toList());
 
         return new Iterator<String>() {

src/main/java/edu/illinois/library/cantaloupe/config/FileConfiguration.java
@@ -2,6 +2,10 @@ package edu.illinois.library.cantaloupe.config;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public interface FileConfiguration extends Configuration {
 
@@ -19,6 +23,23 @@ public interface FileConfiguration extends Configuration {
             return Paths.get(configFilePath).toAbsolutePath();
         }
         return null;
+    }
+
+    /**
+     * This default implementation uses the {@link Iterator} returned by {@link
+     * #getKeys} in conjunction with {@link #getProperty(String)} to build a
+     * map. Implementations should override if they can do it more efficiently.
+     *
+     * @return Configuration keys and values in a read-only map.
+     */
+    default Map<String,Object> toMap() {
+        final Map<String,Object> map = new LinkedHashMap<>();
+        final Iterator<String> keys = getKeys();
+        while (keys.hasNext()) {
+            final String key = keys.next();
+            map.put(key, getProperty(key));
+        }
+        return Collections.unmodifiableMap(map);
     }
 
 }

src/test/java/edu/illinois/library/cantaloupe/config/AbstractConfigurationTest.java
@@ -5,8 +5,6 @@ import edu.illinois.library.cantaloupe.test.ConcurrentReaderWriter;
 import org.junit.Test;
 
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -907,21 +905,5 @@ public abstract class AbstractConfigurationTest extends BaseTest {
         final Configuration instance = getInstance();
         assertEquals("cats", instance.getString("test1", "cats"));
     }
-
-    /* toMap() */
-
-    @Test
-    public void testToMap() {
-        final Configuration instance = getInstance();
-        Map<String,Object> expected = new LinkedHashMap<>();
-        Iterator<String> keys = instance.getKeys();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            expected.put(key, instance.getProperty(key));
-        }
-
-        assertEquals(expected, instance.toMap());
-    }
-
 
 }

src/test/java/edu/illinois/library/cantaloupe/config/AbstractFileConfigurationTest.java
@@ -6,6 +6,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -27,6 +30,21 @@ public abstract class AbstractFileConfigurationTest extends AbstractConfiguratio
         } catch (IOException e) {
             fail("Failed to set " + ConfigurationFactory.CONFIG_VM_ARGUMENT);
         }
+    }
+
+    /* toMap() */
+
+    @Test
+    public void testToMap() {
+        final FileConfiguration instance = (FileConfiguration) getInstance();
+        Map<String,Object> expected      = new LinkedHashMap<>();
+        Iterator<String> keys            = instance.getKeys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            expected.put(key, instance.getProperty(key));
+        }
+
+        assertEquals(expected, instance.toMap());
     }
 
 }

commit 7205200f8e54568d42725c16b85a82ef3aa1f6fe

Fix a regression from dada7e2e48a239eb0f94088b1f668b6939dfca8c

src/main/java/edu/illinois/library/cantaloupe/config/MapConfiguration.java
@@ -3,6 +3,7 @@ package edu.illinois.library.cantaloupe.config;
 import edu.illinois.library.cantaloupe.util.StringUtils;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -10,7 +11,7 @@ import java.util.concurrent.ConcurrentMap;
 /**
  * In-memory configuration that cannot be persisted.
  */
-class MapConfiguration implements Configuration {
+public class MapConfiguration implements Configuration {
 
     private final ConcurrentMap<String,Object> configuration =
             new ConcurrentHashMap<>();
@@ -23,6 +24,10 @@ class MapConfiguration implements Configuration {
     @Override
     public void clearProperty(String key) {
         configuration.remove(key);
+    }
+
+    public Map<String,Object> getBackingMap() {
+        return configuration;
     }
 
     @Override

src/main/java/edu/illinois/library/cantaloupe/resource/admin/ConfigurationResource.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.ConfigurationProvider;
 import edu.illinois.library.cantaloupe.config.FileConfiguration;
+import edu.illinois.library.cantaloupe.config.MapConfiguration;
 import edu.illinois.library.cantaloupe.http.Method;
 import edu.illinois.library.cantaloupe.http.Status;
 import edu.illinois.library.cantaloupe.resource.JacksonRepresentation;
@@ -53,6 +54,8 @@ public class ConfigurationResource extends AbstractAdminResource {
         for (Configuration config : wrappedConfigs) {
             if (config instanceof FileConfiguration) {
                 map = ((FileConfiguration) config).toMap();
+            } else if (config instanceof MapConfiguration) {
+                map = ((MapConfiguration) config).getBackingMap();
             }
         }
 