4bbf4a062112f2e99f59047cc768ca55a9516b65
Alex Dolski <alexd@n-ary.net>
2019-03-08 15:05:24 -0600

    Add TurboJPEG JNI binding

---
+++ src/main/java/org/libjpegturbo/turbojpeg/package-info.java
@@ -0,0 +1,31 @@
+/**
+ * <p>Contains the {@link org.libjpegturbo.turbojpeg libjpeg-turbo JNI binding}
+ * from the {@literal java} subdirectory in the libjpeg-turbo 2.0.2 source
+ * distribution.</p>
+ *
+ * <p>Note that libjpeg-turbo must be compiled with Java support, which it
+ * isn't by default.</p>
+ *
+ * <h2>Building libjpeg-turbo with Java support</h2>
+ *
+ * <h3>Linux</h3>
+ *
+ * TODO: write this
+ *
+ * <h3>macOS</h3>
+ *
+ * {@code
+ * $ cd libjpeg-turbo-2.0.x
+ * $ export JAVAFLAGS="-source 1.8 -target 1.8"
+ * $ export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+ * $ make clean
+ * $ cmake -G"Unix Makefiles" -DWITH_JAVA=1
+ * $ make
+ * $ sudo make install
+ * }
+ *
+ * <h3>Windows</h3>
+ *
+ * TODO: write this
+ */
+package org.libjpegturbo.turbojpeg;
