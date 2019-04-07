3ad20f844a83b41a40c15bc925b9de3d63c2c19c
Alex Dolski <alexd@n-ary.net>
2019-04-06 20:59:52 -0500

    Replace awkward List population with List.of()

--- src/main/java/edu/illinois/library/cantaloupe/processor/AutomaticSelectionStrategy.java
+++ src/main/java/edu/illinois/library/cantaloupe/processor/AutomaticSelectionStrategy.java
@@ -2,8 +2,6 @@ package edu.illinois.library.cantaloupe.processor;
 
 import edu.illinois.library.cantaloupe.image.Format;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,52 +10,32 @@ import java.util.List;
  */
 class AutomaticSelectionStrategy implements SelectionStrategy {
 
-    private static final List<Class<? extends Processor>> JP2_CANDIDATES,
-            JPG_CANDIDATES, PDF_CANDIDATES, VIDEO_CANDIDATES,
-            FALLBACK_CANDIDATES;
-
-    static {
-        // JP2
-        List<Class<? extends Processor>> list = new ArrayList<>();
-        list.add(KakaduNativeProcessor.class);
-        list.add(OpenJpegProcessor.class);
-        list.add(ImageMagickProcessor.class);
-        list.add(GraphicsMagickProcessor.class);
-        JP2_CANDIDATES = Collections.unmodifiableList(list);
-
-        // JPEG
-        list = new ArrayList<>();
-        list.add(TurboJpegProcessor.class);
-        list.add(Java2dProcessor.class);
-        list.add(GraphicsMagickProcessor.class);
-        list.add(ImageMagickProcessor.class);
-        JPG_CANDIDATES = Collections.unmodifiableList(list);
-
-        // PDF
-        list = new ArrayList<>();
-        list.add(PdfBoxProcessor.class);
-        list.add(GraphicsMagickProcessor.class);
-        list.add(ImageMagickProcessor.class);
-        PDF_CANDIDATES = Collections.unmodifiableList(list);
-
-        // Video
-        list = new ArrayList<>();
-        list.add(FfmpegProcessor.class);
-        VIDEO_CANDIDATES = Collections.unmodifiableList(list);
-
-        // Fallback
-        list = new ArrayList<>();
-        list.add(Java2dProcessor.class);
-        list.add(GraphicsMagickProcessor.class);
-        list.add(ImageMagickProcessor.class);
-        FALLBACK_CANDIDATES = Collections.unmodifiableList(list);
-    }
+    private static final List<Class<? extends Processor>> JP2_CANDIDATES = List.of(
+            KakaduNativeProcessor.class,
+            OpenJpegProcessor.class,
+            ImageMagickProcessor.class,
+            GraphicsMagickProcessor.class);
+    private static final List<Class<? extends Processor>> JPG_CANDIDATES = List.of(
+            TurboJpegProcessor.class,
+            Java2dProcessor.class,
+            GraphicsMagickProcessor.class,
+            ImageMagickProcessor.class);
+    private static final List<Class<? extends Processor>> PDF_CANDIDATES = List.of(
+            PdfBoxProcessor.class,
+            GraphicsMagickProcessor.class,
+            ImageMagickProcessor.class);
+    private static final List<Class<? extends Processor>> VIDEO_CANDIDATES = List.of(
+            FfmpegProcessor.class);
+    private static final List<Class<? extends Processor>> FALLBACK_CANDIDATES = List.of(
+            Java2dProcessor.class,
+            GraphicsMagickProcessor.class,
+            ImageMagickProcessor.class);
 
     @Override
     public List<Class<? extends Processor>> getPreferredProcessors(Format sourceFormat) {
         if (Format.JP2.equals(sourceFormat)) {
             return JP2_CANDIDATES;
-        } if (Format.JPG.equals(sourceFormat)) {
+        } else if (Format.JPG.equals(sourceFormat)) {
             return JPG_CANDIDATES;
         } else if (Format.PDF.equals(sourceFormat)) {
             return PDF_CANDIDATES;
