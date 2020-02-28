cantaloupe-project/cantaloupe

commit fc7c30f37d36cba8abcec54826058d0a5bf3f880

Move ImageResource & InformationResource logic into ImageRequestHandler & InformationRequestHandler

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageResource.java
@@ -1,37 +1,20 @@
 package edu.illinois.library.cantaloupe.resource.iiif.v1;
 
-import edu.illinois.library.cantaloupe.cache.CacheFacade;
-import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.http.Method;
 import edu.illinois.library.cantaloupe.image.Dimension;
 import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.image.Identifier;
 import edu.illinois.library.cantaloupe.image.Info;
-import edu.illinois.library.cantaloupe.image.MediaType;
 import edu.illinois.library.cantaloupe.operation.OperationList;
 import edu.illinois.library.cantaloupe.operation.Scale;
 import edu.illinois.library.cantaloupe.processor.Processor;
-import edu.illinois.library.cantaloupe.processor.ProcessorFactory;
-import edu.illinois.library.cantaloupe.processor.SourceFormatException;
-import edu.illinois.library.cantaloupe.status.HealthChecker;
-import edu.illinois.library.cantaloupe.source.Source;
-import edu.illinois.library.cantaloupe.source.SourceFactory;
-import edu.illinois.library.cantaloupe.processor.ProcessorConnector;
-import edu.illinois.library.cantaloupe.resource.IllegalClientArgumentException;
-import edu.illinois.library.cantaloupe.resource.ImageRepresentation;
+import edu.illinois.library.cantaloupe.resource.generic.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Handles IIIF Image API 1.1 image requests.
@@ -39,7 +22,6 @@ import java.util.stream.Collectors;
  * @see <a href="http://iiif.io/api/image/1.1/#url-syntax-image-request">Image
  * Request Operations</a>
  */
-@SuppressWarnings("Duplicates")
 public class ImageResource extends IIIF1Resource {
 
     private static final Logger LOGGER =
@@ -68,7 +50,7 @@ public class ImageResource extends IIIF1Resource {
      *
      * <p>N.B.: This method only respects {@link
      * Key#CACHE_SERVER_RESOLVE_FIRST} for infos, as doing so with images is
-     * not really possible with current API.</p>
+     * not really possible using current API.</p>
      */
     @Override
     public void doGET() throws Exception {
@@ -76,131 +58,47 @@ public class ImageResource extends IIIF1Resource {
             return;
         }
 
-        final Configuration config         = Configuration.getInstance();
-        final Identifier identifier        = getIdentifier();
-        final CacheFacade cacheFacade      = new CacheFacade();
-        final boolean isBypassingCacheRead = isBypassingCacheRead();
-        final boolean isBypassingCache     = isBypassingCache();
+        final OperationList opList = getOperationList();
 
-        Iterator<Format> formatIterator = Collections.emptyIterator();
-        boolean isFormatKnownYet = false;
-
-        // If we are using a cache, and don't need to resolve first, see if we
-        // can pluck an info from it. This will be more efficient than getting
-        // it from a source.
-        if (!isBypassingCache && !isBypassingCacheRead && !isResolvingFirst()) {
-            try {
-                Optional<Info> optInfo = cacheFacade.getInfo(identifier);
-                if (optInfo.isPresent()) {
-                    Info info = optInfo.get();
-                    Format infoFormat = info.getSourceFormat();
-                    if (infoFormat != null) {
-                        formatIterator = Collections.singletonList(infoFormat).iterator();
-                        isFormatKnownYet = true;
-                    }
-                }
-            } catch (IOException e) {
-                // Don't rethrow -- it's still possible to service the request.
-                LOGGER.error(e.getMessage());
+        class CustomCallback implements ImageRequestHandler.Callback {
+            @Override
+            public void willStreamImageFromDerivativeCache() {
+                throw new RuntimeException("This method is not supposed to get called");
             }
-        }
 
-        final Source source = new SourceFactory().newSource(
-                identifier, getDelegateProxy());
-
-        // If we are resolving first, or if the source image is not present in
-        // the source cache (if enabled), check access to it in preparation for
-        // retrieval.
-        final Optional<Path> sourceImage = cacheFacade.getSourceCacheFile(identifier);
-        if (sourceImage.isEmpty() || isResolvingFirst()) {
-            try {
-                source.checkAccess();
-            } catch (NoSuchFileException e) { // this needs to be rethrown!
-                if (config.getBoolean(Key.CACHE_SERVER_PURGE_MISSING, false)) {
-                    // If the image was not found, purge it from the cache.
-                    cacheFacade.purgeAsync(identifier);
-                }
-                throw e;
+            @Override
+            public boolean authorize() throws Exception {
+                return ImageResource.this.authorize();
             }
-        }
 
-        if (!isFormatKnownYet) {
-            // If we are not resolving first, and there is a hit in the source
-            // cache, read the format from the source-cached-file, as we will
-            // expect source cache access to be more efficient.
-            // Otherwise, read it from the source.
-            if (!isResolvingFirst() && sourceImage.isPresent()) {
-                List<MediaType> mediaTypes = MediaType.detectMediaTypes(sourceImage.get());
-                if (!mediaTypes.isEmpty()) {
-                    formatIterator = mediaTypes
-                            .stream()
-                            .map(MediaType::toFormat)
-                            .iterator();
-                }
-            } else {
-                formatIterator = source.getFormatIterator();
-            }
-        }
-
-        while (formatIterator.hasNext()) {
-            final Format format = formatIterator.next();
-            // Obtain an instance of the processor assigned to this format.
-            String processorName = "unknown processor";
-            try (Processor processor = new ProcessorFactory().newProcessor(format)) {
-                processorName = processor.getClass().getSimpleName();
-                // Connect it to the source.
-                tempFileFuture = new ProcessorConnector().connect(
-                        source, processor, identifier, format);
-
-                final Set<Format> availableOutputFormats =
-                        processor.getAvailableOutputFormats();
-
-                final OperationList ops = getOperationList(availableOutputFormats);
+            @Override
+            public void willProcessImage(Processor processor,
+                                         Info info) throws Exception {
+                final Dimension fullSize = info.getSize(getPageIndex());
+                validateScale(info.getMetadata().getOrientation().adjustedSize(fullSize),
+                        (Scale) opList.getFirst(Scale.class));
 
                 final String disposition = getRepresentationDisposition(
-                        ops.getIdentifier(), ops.getOutputFormat());
-
-                final Info info = getOrReadInfo(identifier, processor);
-                Dimension fullSize;
-                try {
-                    fullSize = info.getSize(getPageIndex());
-                    getRequestContext().setMetadata(info.getMetadata());
-
-                    ops.setScaleConstraint(getScaleConstraint());
-                    ops.applyNonEndpointMutations(info, getDelegateProxy());
-                    ops.freeze();
-                    getRequestContext().setOperationList(ops, fullSize);
-                } catch (IllegalArgumentException | IndexOutOfBoundsException e) {
-                    throw new IllegalClientArgumentException(e);
-                }
-
-                if (!authorize()) {
-                    return;
-                }
-
-                processor.validate(ops, fullSize);
-                validateScale(info.getMetadata().getOrientation().adjustedSize(fullSize),
-                        (Scale) ops.getFirst(Scale.class));
-
-                addHeaders(processor, ops.getOutputFormat(), disposition);
-
-                new ImageRepresentation(info, processor, ops,
-                        isBypassingCacheRead, isBypassingCache)
-                        .write(getResponse().getOutputStream());
-
-                // Notify the health checker of a successful response.
-                HealthChecker.addSourceProcessorPair(source, processor, ops);
-                return;
-            } catch (SourceFormatException e) {
-                LOGGER.debug("Format inferred by {} disagrees with the one " +
-                                "supplied by {} ({}) for {}; trying again",
-                        processorName, source, format, identifier);
+                        getIdentifier(), opList.getOutputFormat());
+                addHeaders(processor.getAvailableOutputFormats(),
+                        opList.getOutputFormat(), disposition);
             }
         }
-        throw new SourceFormatException();
+
+        try (ImageRequestHandler handler = ImageRequestHandler.builder()
+                .withOperationList(opList)
+                .withBypassingCache(isBypassingCache())
+                .withBypassingCacheRead(isBypassingCacheRead())
+                .withDelegateProxy(getDelegateProxy())
+                .withRequestContext(getRequestContext())
+                .withPageIndex(getPageIndex())
+                .withCallback(new CustomCallback())
+                .build()) {
+            handler.handle(getResponse().getOutputStream());
+        }
     }
 
-    private void addHeaders(Processor processor,
+    private void addHeaders(Set<Format> availableOutputFormats,
                             Format outputFormat,
                             String disposition) {
         if (disposition != null) {
@@ -210,12 +108,12 @@ public class ImageResource extends IIIF1Resource {
                 outputFormat.getPreferredMediaType().toString());
 
         final ComplianceLevel complianceLevel = ComplianceLevel.getLevel(
-                processor.getAvailableOutputFormats());
+                availableOutputFormats);
         getResponse().setHeader("Link",
                 String.format("<%s>;rel=\"profile\";", complianceLevel.getUri()));
     }
 
-    private OperationList getOperationList(Set<Format> availableOutputFormats) {
+    private OperationList getOperationList() {
         final List<String> args = getPathArguments();
 
         // If the URI path contains a format extension, try to use that.
@@ -225,8 +123,7 @@ public class ImageResource extends IIIF1Resource {
         try {
             outputFormat = args.get(5);
         } catch (IndexOutOfBoundsException e) {
-            outputFormat = getEffectiveOutputFormat(availableOutputFormats).
-                    getPreferredExtension();
+            outputFormat = getEffectiveOutputFormat().getPreferredExtension();
         }
 
         final Identifier identifier = getIdentifier();
@@ -240,12 +137,12 @@ public class ImageResource extends IIIF1Resource {
     }
 
     /**
-     * @param limitToFormats Set of OutputFormats to limit the result to.
-     * @return The best output format based on the URI extension, {@literal
-     *         Accept} header, or default, as outlined by the Image API 1.1
-     *         spec.
+     * Negotiates an output format.
+     *
+     * @return The best output format based on the URI extension, {@code
+     *         Accept} header, or default.
      */
-    private Format getEffectiveOutputFormat(Set<Format> limitToFormats) {
+    private Format getEffectiveOutputFormat() {
         // Check for a format extension in the URI.
         final String extension = getRequest().getReference().getPathExtension();
 
@@ -257,23 +154,10 @@ public class ImageResource extends IIIF1Resource {
                     .findFirst()
                     .orElse(null);
         }
-        if (format == null) { // if none, check the Accept header.
-            String contentType = negotiateContentType(limitToFormats.stream()
-                    .map(Format::getPreferredMediaType)
-                    .map(MediaType::toString)
-                    .collect(Collectors.toList()));
-            if (contentType != null) {
-                format = new MediaType(contentType).toFormat();
-            } else {
-                format = DEFAULT_FORMAT;
-            }
+        if (format == null) {
+            format = DEFAULT_FORMAT;
         }
         return format;
-    }
-
-    private boolean isResolvingFirst() {
-        return Configuration.getInstance().
-                getBoolean(Key.CACHE_SERVER_RESOLVE_FIRST, true);
     }
 
 }

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageResource.java
@@ -1,41 +1,23 @@
 package edu.illinois.library.cantaloupe.resource.iiif.v2;
 
-import edu.illinois.library.cantaloupe.cache.CacheFacade;
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.http.Method;
 import edu.illinois.library.cantaloupe.image.Dimension;
-import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.image.Identifier;
 import edu.illinois.library.cantaloupe.image.Info;
-import edu.illinois.library.cantaloupe.image.MediaType;
 import edu.illinois.library.cantaloupe.image.Metadata;
 import edu.illinois.library.cantaloupe.image.Orientation;
 import edu.illinois.library.cantaloupe.operation.OperationList;
 import edu.illinois.library.cantaloupe.operation.Scale;
 import edu.illinois.library.cantaloupe.processor.Processor;
-import edu.illinois.library.cantaloupe.processor.ProcessorConnector;
-import edu.illinois.library.cantaloupe.processor.ProcessorFactory;
-import edu.illinois.library.cantaloupe.processor.SourceFormatException;
-import edu.illinois.library.cantaloupe.resource.InputStreamRepresentation;
-import edu.illinois.library.cantaloupe.resource.IllegalClientArgumentException;
-import edu.illinois.library.cantaloupe.resource.ImageRepresentation;
 import edu.illinois.library.cantaloupe.resource.Route;
-import edu.illinois.library.cantaloupe.status.HealthChecker;
+import edu.illinois.library.cantaloupe.resource.generic.ImageRequestHandler;
 import edu.illinois.library.cantaloupe.resource.iiif.SizeRestrictedException;
-import edu.illinois.library.cantaloupe.source.Source;
-import edu.illinois.library.cantaloupe.source.SourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Handles IIIF Image API 2.x image requests.
@@ -43,7 +25,6 @@ import java.util.Optional;
  * @see <a href="http://iiif.io/api/image/2.1/#image-request-parameters">Image
  * Request Operations</a>
  */
-@SuppressWarnings("Duplicates")
 public class ImageResource extends IIIF2Resource {
 
     private static final Logger LOGGER =
@@ -71,155 +52,60 @@ public class ImageResource extends IIIF2Resource {
             return;
         }
 
-        final Configuration config         = Configuration.getInstance();
-        final List<String> args            = getPathArguments();
-        final Identifier identifier        = getIdentifier();
-        final CacheFacade cacheFacade      = new CacheFacade();
-        final boolean isBypassingCacheRead = isBypassingCacheRead();
-        final boolean isBypassingCache     = isBypassingCache();
-
-        // Assemble the URI parameters into a Parameters object.
+        // Assemble the URI path segments into a Parameters object.
+        final List<String> args = getPathArguments();
         final Parameters params = new Parameters(
-                identifier, args.get(1), args.get(2),
+                getIdentifier(), args.get(1), args.get(2),
                 args.get(3), args.get(4), args.get(5));
+        // Convert it into an OperationList.
         final OperationList ops = params.toOperationList();
         ops.getOptions().putAll(getRequest().getReference().getQuery().toMap());
-
+        final int pageIndex = getPageIndex();
         final String disposition = getRepresentationDisposition(
                 ops.getIdentifier(), ops.getOutputFormat());
 
-        Iterator<Format> formatIterator = Collections.emptyIterator();
-        boolean isFormatKnownYet = false;
-
-        // If we are using a cache, and don't need to resolve first:
-        // 1. If the cache contains an image matching the request, skip all the
-        //    setup and just return the cached image.
-        // 2. Otherwise, if the cache contains a relevant info, get it to avoid
-        //    having to get it from a source later.
-        if (!isBypassingCache && !isBypassingCacheRead && !isResolvingFirst()) {
-            final Optional<Info> optInfo = cacheFacade.getInfo(identifier);
-            if (optInfo.isPresent()) {
-                Info info = optInfo.get();
-                ops.setScaleConstraint(getScaleConstraint());
-                ops.applyNonEndpointMutations(info, getDelegateProxy());
-
-                InputStream cacheStream = null;
-                try {
-                    cacheStream = cacheFacade.newDerivativeImageInputStream(ops);
-                } catch (IOException e) {
-                    // Don't rethrow -- it's still possible to service the
-                    // request.
-                    LOGGER.error(e.getMessage());
-                }
-
-                if (cacheStream != null) {
-                    addHeaders(disposition,
-                            params.getOutputFormat().toFormat().getPreferredMediaType().toString());
-
-                    new InputStreamRepresentation(cacheStream)
-                            .write(getResponse().getOutputStream());
-                    return;
-                } else {
-                    Format infoFormat = info.getSourceFormat();
-                    if (infoFormat != null) {
-                        formatIterator = Collections.singletonList(infoFormat).iterator();
-                        isFormatKnownYet = true;
-                    }
-                }
+        class CustomCallback implements ImageRequestHandler.Callback {
+            @Override
+            public void willStreamImageFromDerivativeCache() {
+                addHeaders(disposition,
+                        params.getOutputFormat().toFormat()
+                                .getPreferredMediaType().toString());
             }
-        }
 
-        final Source source = new SourceFactory().newSource(
-                identifier, getDelegateProxy());
-
-        // If we are resolving first, or if the source image is not present in
-        // the source cache (if enabled), check access to it in preparation for
-        // retrieval.
-        final Optional<Path> sourceImage = cacheFacade.getSourceCacheFile(identifier);
-        if (sourceImage.isEmpty() || isResolvingFirst()) {
-            try {
-                source.checkAccess();
-            } catch (NoSuchFileException e) { // this needs to be rethrown!
-                if (config.getBoolean(Key.CACHE_SERVER_PURGE_MISSING, false)) {
-                    // If the image was not found, purge it from the cache.
-                    cacheFacade.purgeAsync(ops.getIdentifier());
-                }
-                throw e;
+            @Override
+            public boolean authorize() throws Exception {
+                return ImageResource.this.authorize();
             }
-        }
 
-        if (!isFormatKnownYet) {
-            // If we are not resolving first, and there is a hit in the source
-            // cache, read the format from the source-cached-file, as we will
-            // expect source cache access to be more efficient.
-            // Otherwise, read it from the source.
-            if (!isResolvingFirst() && sourceImage.isPresent()) {
-                List<MediaType> mediaTypes = MediaType.detectMediaTypes(sourceImage.get());
-                if (!mediaTypes.isEmpty()) {
-                    formatIterator = mediaTypes
-                            .stream()
-                            .map(MediaType::toFormat)
-                            .iterator();
-                }
-            } else {
-                formatIterator = source.getFormatIterator();
-            }
-        }
-
-        while (formatIterator.hasNext()) {
-            final Format format = formatIterator.next();
-            // Obtain an instance of the processor assigned to this format.
-            String processorName = "unknown processor";
-            try (Processor processor = new ProcessorFactory().newProcessor(format)) {
-                // Connect it to the source.
-                tempFileFuture = new ProcessorConnector().connect(
-                        source, processor, identifier, format);
-
-                final Info info = getOrReadInfo(ops.getIdentifier(), processor);
-                Dimension fullSize;
-                try {
-                    fullSize = info.getSize(getPageIndex());
-                    getRequestContext().setMetadata(info.getMetadata());
-
-                    ops.setScaleConstraint(getScaleConstraint());
-                    ops.applyNonEndpointMutations(info, getDelegateProxy());
-                    ops.freeze();
-                    getRequestContext().setOperationList(ops, fullSize);
-                } catch (IllegalArgumentException | IndexOutOfBoundsException e) {
-                    throw new IllegalClientArgumentException(e);
-                }
-
-                if (!authorize()) {
-                    return;
-                }
-
-                processor.validate(ops, fullSize);
-
+            @Override
+            public void willProcessImage(Processor processor,
+                                         Info info) throws Exception {
                 final Metadata metadata = info.getMetadata();
                 final Orientation orientation = (metadata != null) ?
                         metadata.getOrientation() : Orientation.ROTATE_0;
-                final Dimension virtualSize = orientation.adjustedSize(fullSize);
+                final Dimension virtualSize = orientation.adjustedSize(info.getSize(pageIndex));
                 final Dimension resultingSize = ops.getResultingSize(info.getSize());
                 validateScale(virtualSize, (Scale) ops.getFirst(Scale.class));
                 validateSize(resultingSize, virtualSize);
 
-                addHeaders(params, fullSize, disposition,
+                addHeaders(params,
+                        info.getSize(pageIndex),
+                        disposition,
                         params.getOutputFormat().toFormat().getPreferredMediaType().toString());
-
-                new ImageRepresentation(info, processor, ops,
-                        isBypassingCacheRead, isBypassingCache)
-                        .write(getResponse().getOutputStream());
-
-                // Notify the health checker of a successful response.
-                HealthChecker.addSourceProcessorPair(source, processor, ops);
-                return;
-            } catch (SourceFormatException e) {
-                LOGGER.debug("Format inferred by {} disagrees with the one " +
-                                "supplied by {} ({}) for {}; trying again",
-                        processorName, source, format, identifier);
             }
         }
-        throw new SourceFormatException();
+
+        try (ImageRequestHandler handler = ImageRequestHandler.builder()
+                .withOperationList(ops)
+                .withBypassingCache(isBypassingCache())
+                .withBypassingCacheRead(isBypassingCacheRead())
+                .withDelegateProxy(getDelegateProxy())
+                .withRequestContext(getRequestContext())
+                .withPageIndex(pageIndex)
+                .withCallback(new CustomCallback())
+                .build()) {
+            handler.handle(getResponse().getOutputStream());
+        }
     }
 
     /**
@@ -268,11 +154,6 @@ public class ImageResource extends IIIF2Resource {
                     .orElseThrow(() -> new SizeRestrictedException(
                             "Available sizes are limited to those in the information response."));
         }
-    }
-
-    private boolean isResolvingFirst() {
-        return Configuration.getInstance().
-                getBoolean(Key.CACHE_SERVER_RESOLVE_FIRST, true);
     }
 
 }

commit 1557473a3188c5d3d21c533d7282aae253667e5c

Fix a regression related to scale constraints introduced in fc7c30f

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/ImageResource.java
@@ -132,6 +132,7 @@ public class ImageResource extends IIIF1Resource {
                 args.get(3), args.get(4), outputFormat);
 
         final OperationList ops = params.toOperationList();
+        ops.setScaleConstraint(getScaleConstraint());
         ops.getOptions().putAll(getRequest().getReference().getQuery().toMap());
         return ops;
     }

src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v2/ImageResource.java
@@ -59,6 +59,7 @@ public class ImageResource extends IIIF2Resource {
                 args.get(3), args.get(4), args.get(5));
         // Convert it into an OperationList.
         final OperationList ops = params.toOperationList();
+        ops.setScaleConstraint(getScaleConstraint());
         ops.getOptions().putAll(getRequest().getReference().getQuery().toMap());
         final int pageIndex = getPageIndex();
         final String disposition = getRepresentationDisposition(