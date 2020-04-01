cantaloupe-project/cantaloupe

commit 2b25b761a8a9038fff33902959db619f556484a8

Fix Reference's copy constructor failing to clone the Query (closes #359)

src/main/java/edu/illinois/library/cantaloupe/http/Query.java
@@ -19,6 +19,13 @@ public final class Query implements Iterable<KeyValuePair> {
 
     public Query() {}
 
+    /**
+     * Copy constructor.
+     */
+    public Query(Query query) {
+        this(query.toString());
+    }
+
     public Query(String query) {
         String[] pairs = query.split("&");
         for (String pair : pairs) {

src/test/java/edu/illinois/library/cantaloupe/http/QueryTest.java
@@ -20,6 +20,13 @@ public class QueryTest {
     }
 
     @Test
+    public void testCopyConstructor() {
+        Query query2 = new Query(instance);
+        assertEquals("value1", query2.getFirstValue("key1"));
+        assertEquals("value2", query2.getFirstValue("key2"));
+    }
+
+    @Test
     public void testConstructorWithString() {
         assertEquals("value1", instance.getFirstValue("key1"));
         assertEquals("value2", instance.getFirstValue("key2"));

src/main/java/edu/illinois/library/cantaloupe/http/Reference.java
@@ -42,7 +42,7 @@ public final class Reference {
         setHost(reference.getHost());
         setPort(reference.getPort());
         setPath(reference.getPath());
-        setQuery(reference.getQuery());
+        setQuery(new Query(reference.getQuery()));
         setFragment(reference.getFragment());
     }
 

src/test/java/edu/illinois/library/cantaloupe/http/ReferenceTest.java
@@ -31,6 +31,12 @@ public class ReferenceTest {
     }
 
     @Test
+    public void testCopyConstructorClonesQuery() {
+        Reference copy = new Reference(instance);
+        assertNotSame(instance.getQuery(), copy.getQuery());
+    }
+
+    @Test
     public void testStringConstructor() {
         String uri = "http://example.org/cats/dogs?cats=dogs";
         Reference ref = new Reference(uri);

commit 0c62917fe19c409214484791d779d47148323207

Fix a regression in 2b25b761a8a9038fff33902959db619f556484a8

src/main/java/edu/illinois/library/cantaloupe/http/Query.java
@@ -27,13 +27,15 @@ public final class Query implements Iterable<KeyValuePair> {
     }
 
     public Query(String query) {
-        String[] pairs = query.split("&");
-        for (String pair : pairs) {
-            String[] kv = pair.split("=");
-            if (kv.length > 1) {
-                add(kv[0], kv[1]);
-            } else if (kv.length > 0) {
-                add(kv[0]);
+        if (!query.isEmpty()) {
+            String[] pairs = query.split("&");
+            for (String pair : pairs) {
+                String[] kv = pair.split("=");
+                if (kv.length > 1) {
+                    add(kv[0], kv[1]);
+                } else if (kv.length > 0) {
+                    add(kv[0]);
+                }
             }
         }
     }

src/test/java/edu/illinois/library/cantaloupe/http/QueryTest.java
@@ -27,7 +27,13 @@ public class QueryTest {
     }
 
     @Test
-    public void testConstructorWithString() {
+    public void testConstructorWithEmptyString() {
+        instance = new Query("");
+        assertTrue(instance.isEmpty());
+    }
+
+    @Test
+    public void testConstructorWithNonEmptyString() {
         assertEquals("value1", instance.getFirstValue("key1"));
         assertEquals("value2", instance.getFirstValue("key2"));
     }