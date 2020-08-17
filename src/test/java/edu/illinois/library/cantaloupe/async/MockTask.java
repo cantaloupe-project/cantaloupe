1e94c5e2c9e3ac012c07c3c1a73da22e403514b7
Alex Dolski <alexd@n-ary.net>
2020-08-17 12:42:05 -0500

    Increase wait time

--- src/test/java/edu/illinois/library/cantaloupe/async/MockTask.java
+++ src/test/java/edu/illinois/library/cantaloupe/async/MockTask.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 abstract class MockTask {
 
-    final static int WAIT = 200;
+    final static int WAIT = 400;
 
     final AtomicBoolean ran = new AtomicBoolean(false);
 
