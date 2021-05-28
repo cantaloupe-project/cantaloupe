4991fb868aaf222b673f109906fe88c073483850
Alex Dolski <alexd@n-ary.net>
2021-05-28 15:17:59 -0500

    Refactor getOpenPorts()

--- src/main/java/edu/illinois/library/cantaloupe/util/SocketUtils.java
+++ src/main/java/edu/illinois/library/cantaloupe/util/SocketUtils.java
@@ -2,8 +2,8 @@ package edu.illinois.library.cantaloupe.util;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class SocketUtils {
 
@@ -12,20 +12,21 @@ public final class SocketUtils {
     }
 
     public static int[] getOpenPorts(int howMany) {
-        final Set<ServerSocket> triedSockets = new HashSet<>();
-        final int[] ports = new int[howMany];
-
-        for (int i = 0; i < howMany; i++) {
-            try {
-                ServerSocket socket = new ServerSocket(0);
-                ports[i] = socket.getLocalPort();
-                triedSockets.add(socket);
-                // Leave it open for now so it isn't returned again on the next
-                // iteration.
-            } catch (IOException e) {
-                System.err.println("TestUtil.getOpenPorts(): " + e.getMessage());
+        final List<Integer> ports = new ArrayList<>(65535);
+        int numFound = 0;
+        for (int port = 1; port < 65535; port++) {
+            if (numFound >= howMany) {
+                break;
+            }
+            try (ServerSocket socket = new ServerSocket(port)) {
+                ports.add(port);
+                numFound++;
+            } catch (IOException ignore) {
+                // probably not open, fine
             }
         }
+        return ports.stream().mapToInt(i -> i).toArray();
+    }
 
         for (ServerSocket socket : triedSockets) {
             try {
