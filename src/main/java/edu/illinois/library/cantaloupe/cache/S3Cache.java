691de834c9b688c8ee88ba22231caf1ca7d83a1f
Alex Dolski <alexd@n-ary.net>
2021-08-30 11:39:12 -0500

    Fix purge() and purgeInvalid() not respecting the key prefix (#532)

--- src/main/java/edu/illinois/library/cantaloupe/cache/S3Cache.java
+++ src/main/java/edu/illinois/library/cantaloupe/cache/S3Cache.java
@@ -393,7 +393,7 @@ class S3Cache implements DerivativeCache {
         final String bucketName     = getBucketName();
         final AtomicInteger counter = new AtomicInteger();
 
-        S3Utils.walkObjects(client, bucketName, (object) -> {
+        S3Utils.walkObjects(client, bucketName, getObjectKeyPrefix(), (object) -> {
             try {
                 client.deleteObject(DeleteObjectRequest.builder()
                         .bucket(bucketName)
@@ -463,7 +463,7 @@ class S3Cache implements DerivativeCache {
         final AtomicInteger counter        = new AtomicInteger();
         final AtomicInteger deletedCounter = new AtomicInteger();
 
-        S3Utils.walkObjects(client, bucketName, (object) -> {
+        S3Utils.walkObjects(client, bucketName, getObjectKeyPrefix(), (object) -> {
             counter.incrementAndGet();
             if (!isValid(object)) {
                 try {
