a46a3d719e46b4420040044581bc583050f70207
Justin Coyne <jcoyne@justincoyne.com>
2025-10-10 09:05:29 -0500

    Move methods for getting the Reference out of the controller
    The Reference is mostly drawn from attributes we have in the Request model, so it makes sense to keep it there.

--- src/main/java/edu/illinois/library/cantaloupe/resource/Request.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/Request.java
@@ -4,6 +4,7 @@ import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.delegate.DelegateProxy;
 import edu.illinois.library.cantaloupe.http.Headers;
+import edu.illinois.library.cantaloupe.http.Method;
 import edu.illinois.library.cantaloupe.http.Query;
 import edu.illinois.library.cantaloupe.http.Reference;
 import edu.illinois.library.cantaloupe.image.MetaIdentifier;
@@ -83,6 +84,10 @@ public class Request {
      */
     public InputStream getInputStream() throws IOException {
         return wrappedRequest.getInputStream();
+    }
+
+    public Method getMethod() {
+        return Method.valueOf(wrappedRequest.getMethod());
     }
 
     /**
