1ff14acb593741ecb2ec59e6378ecda99f4b24e5
Justin Coyne <jcoyne@justincoyne.com>
2025-10-22 09:47:00 -0500

    Hook up InformationFactory to the InformationController

--- src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v3/InformationFactory.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v3/InformationFactory.java
@@ -1,7 +1,20 @@
 package edu.illinois.library.cantaloupe.resource.iiif.v3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.script.ScriptException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
+import edu.illinois.library.cantaloupe.delegate.DelegateProxy;
 import edu.illinois.library.cantaloupe.image.Dimension;
 import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.image.Info;
@@ -10,17 +23,6 @@ import edu.illinois.library.cantaloupe.image.Orientation;
 import edu.illinois.library.cantaloupe.image.ScaleConstraint;
 import edu.illinois.library.cantaloupe.processor.Processor;
 import edu.illinois.library.cantaloupe.resource.iiif.ImageInfoUtil;
-import edu.illinois.library.cantaloupe.delegate.DelegateProxy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.script.ScriptException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Builds new {@link Information} instances.
@@ -53,7 +55,7 @@ public final class InformationFactory {
     private long maxPixels;
     private int minSize, minTileSize;
 
-    InformationFactory() {
+    public InformationFactory() {
         var config  = Configuration.getInstance();
         maxPixels   = config.getInt(Key.MAX_PIXELS, 0);
         maxScale    = config.getDouble(Key.MAX_SCALE, Double.MAX_VALUE);
@@ -70,7 +72,7 @@ public final class InformationFactory {
      *                               list.
      * @param scaleConstraint        May be {@code null}.
      */
-    Information<String,Object> newImageInfo(final Set<Format> processorOutputFormats,
+    public Information<String,Object> newImageInfo(final Set<Format> processorOutputFormats,
                                             final String imageURI,
                                             final Info info,
                                             final int infoImageIndex,
@@ -247,7 +249,7 @@ public final class InformationFactory {
         return (long) Math.min(area * maxScale, maxPixels);
     }
 
-    void setDelegateProxy(DelegateProxy proxy) {
+    public void setDelegateProxy(DelegateProxy proxy) {
         this.delegateProxy = proxy;
     }
 
