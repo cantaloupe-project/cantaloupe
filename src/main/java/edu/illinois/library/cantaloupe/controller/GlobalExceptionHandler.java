5958edd880735e5fe96b1d7ef218f6fc43430b64
Justin Coyne <jcoyne@justincoyne.com>
2025-10-22 09:21:36 -0500

    Add a 404 handler

--- src/main/java/edu/illinois/library/cantaloupe/controller/GlobalExceptionHandler.java
+++ src/main/java/edu/illinois/library/cantaloupe/controller/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import edu.illinois.library.cantaloupe.resource.EndpointDisabledException;
 import edu.illinois.library.cantaloupe.resource.ResourceException;
@@ -58,11 +59,24 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public  ResponseEntity<Map<String, Object>> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", 404);
+        errorResponse.put("error", "Not Found");
+        errorResponse.put("message", "The requested resource was not found");
+        errorResponse.put("path", request.getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGenericException(
             Exception ex, WebRequest request) {
 
         Map<String, Object> errorResponse = new HashMap<>();
+        ex.printStackTrace();
         errorResponse.put("status", 500);
         errorResponse.put("error", "Internal Server Error");
         errorResponse.put("message", "An unexpected error occurred");
