5331178bf1178c6ab454875ac258f7c8a1871cc4
Justin Coyne <jcoyne@justincoyne.com>
2025-10-23 08:56:15 -0500

    Disable LandingController when so configured

--- src/test/java/edu/illinois/library/cantaloupe/controller/iiif/v3/LandingControllerTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/controller/iiif/v3/LandingControllerTest.java
@@ -1,10 +1,12 @@
 package edu.illinois.library.cantaloupe.controller.iiif.v3;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,6 +15,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import edu.illinois.library.cantaloupe.config.Configuration;
+import edu.illinois.library.cantaloupe.config.Key;
 
 @WebMvcTest(LandingController.class)
 @TestPropertySource(properties = {
@@ -25,6 +28,13 @@ public class LandingControllerTest {
     @MockitoBean
     private Configuration configuration;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        // Default: endpoint is enabled
+        when(configuration.getBoolean(Key.IIIF_3_ENDPOINT_ENABLED, true)).thenReturn(true);
+    }
+
+
     @Test
     void testIiif3Landing_WithEndpointEnabled() throws Exception {
         mockMvc.perform(get("/iiif/3"))
@@ -33,12 +43,13 @@ public class LandingControllerTest {
                 .andExpect(content().string(containsString("<h1>IIIF Image API 3.x Endpoint</h1>")));
     }
 
-    // @Test
-    // void testIiif3Landing_WithEndpointDisabled() {
-    //     Configuration config = Configuration.getInstance();
-    //     config.setProperty(Key.IIIF_3_ENDPOINT_ENABLED, false);
-    //     assertStatus(403, getHTTPURI(""));
-    // }
+    @Test
+    void testIiif3Landing_WithEndpointDisabled() throws Exception {
+        when(configuration.getBoolean(Key.IIIF_3_ENDPOINT_ENABLED, true)).thenReturn(false);
+        mockMvc.perform(get("/iiif/3"))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string(containsString("This endpoint is disabled")));
+    }
 
     // @Test
     // void testGETWithTrailingSlashRedirectsToWithout() throws Exception {
