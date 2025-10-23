8f0fd3ca96617dd62ed6cb15d5e21d69d7af9cb8
Justin Coyne <jcoyne@justincoyne.com>
2025-10-23 08:31:44 -0500

    Update test

--- src/main/java/edu/illinois/library/cantaloupe/resource/PoweredByHeader.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/PoweredByHeader.java
@@ -9,14 +9,22 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class PoweredByHeader implements Filter {
+    @Autowired
+    private Configuration config;
+    PoweredByHeader(Configuration config) {
+        this.config = config;
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        final Configuration config = Configuration.getInstance();
         // Only show the x-powered-by header if configured to do so.
         if (config.getBoolean(Key.HEADERS_POWERED_BY_DISPLAY, true)) {
           ((HttpServletResponse) response).setHeader("X-Powered-By",
--- src/test/java/edu/illinois/library/cantaloupe/controller/iiif/v3/LandingControllerTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/controller/iiif/v3/LandingControllerTest.java
@@ -3,6 +3,7 @@ package edu.illinois.library.cantaloupe.controller.iiif.v3;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import edu.illinois.library.cantaloupe.Application;
 import edu.illinois.library.cantaloupe.config.Configuration;
 
 @WebMvcTest(LandingController.class)
@@ -26,11 +28,56 @@ public class LandingControllerTest {
     private Configuration configuration;
 
     @Test
-    void testIiif3Landing() throws Exception {
+    void testIiif3Landing_WithEndpointEnabled() throws Exception {
         mockMvc.perform(get("/iiif/3"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(header().string("X-Powered-By", Application.getName() + "/" + Application.getVersion()))
                 .andExpect(content().string(containsString("<h1>IIIF Image API 3.x Endpoint</h1>")));
     }
+
+    // @Test
+    // void testIiif3Landing_WithEndpointDisabled() {
+    //     Configuration config = Configuration.getInstance();
+    //     config.setProperty(Key.IIIF_3_ENDPOINT_ENABLED, false);
+    //     assertStatus(403, getHTTPURI(""));
+    // }
+
+    // @Test
+    // void testGETWithTrailingSlashRedirectsToWithout() throws Exception {
+    //     final URI uri = getHTTPURI("");
+    //     assertRedirect(new URI(uri + "/"), uri, 301);
+    // }
+
+    // @Test
+    // void testOPTIONSWhenEnabled() throws Exception {
+    //     Configuration config = Configuration.getInstance();
+    //     config.setProperty(Key.IIIF_3_ENDPOINT_ENABLED, true);
+
+    //     client = newClient("");
+    //     client.setMethod(Method.OPTIONS);
+    //     Response response = client.send();
+    //     assertEquals(204, response.getStatus());
+
+    //     Headers headers = response.getHeaders();
+    //     List<String> methods =
+    //             List.of(StringUtils.split(headers.getFirstValue("Allow"), ", "));
+    //     assertEquals(2, methods.size());
+    //     assertTrue(methods.contains("GET"));
+    //     assertTrue(methods.contains("OPTIONS"));
+    // }
+
+    // @Test
+    // void testOPTIONSWhenDisabled() {
+    //     Configuration config = Configuration.getInstance();
+    //     config.setProperty(Key.IIIF_3_ENDPOINT_ENABLED, false);
+
+    //     ResourceException e = assertThrows(ResourceException.class, () -> {
+    //         client = newClient("");
+    //         client.setMethod(Method.OPTIONS);
+    //         client.send();
+    //     });
+    //     assertEquals(403, e.getStatusCode());
+    // }
 
 }
