22698145bb20b06f0b8d9b55544f6f94bcc898f4
Justin Coyne <jcoyne@justincoyne.com>
2025-10-27 10:26:41 -0500

    Handle page out of range error

--- src/main/java/edu/illinois/library/cantaloupe/controller/GlobalExceptionHandler.java
+++ src/main/java/edu/illinois/library/cantaloupe/controller/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import edu.illinois.library.cantaloupe.processor.SourceFormatException;
 import edu.illinois.library.cantaloupe.resource.EndpointDisabledException;
+import edu.illinois.library.cantaloupe.resource.IllegalClientArgumentException;
 import edu.illinois.library.cantaloupe.resource.ResourceException;
 import edu.illinois.library.cantaloupe.resource.iiif.FormatException;
 
@@ -48,6 +49,20 @@ public class GlobalExceptionHandler {
         errorResponse.put("path", request.getDescription(false));
 
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+    }
+
+    // Handles a page request that is greater than the max
+    @ExceptionHandler(IllegalClientArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleAccessDeniedException(
+            IllegalClientArgumentException ex, WebRequest request) {
+
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", 400);
+        errorResponse.put("error", "Bad Request");
+        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("path", request.getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
 
