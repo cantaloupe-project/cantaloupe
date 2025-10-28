4ed9427803f5bafaf6a7b7ef3e5301b8f6dfa1c6
Justin Coyne <jcoyne@justincoyne.com>
2025-10-28 15:22:55 -0500

    Convert v2.LandingResource to a controller

---
+++ src/main/java/edu/illinois/library/cantaloupe/controller/iiif/v2/LandingController.java
@@ -0,0 +1,63 @@
+package edu.illinois.library.cantaloupe.controller.iiif.v2;
+
+import java.net.URI;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.illinois.library.cantaloupe.config.Configuration;
+import edu.illinois.library.cantaloupe.resource.EndpointDisabledException;
+import edu.illinois.library.cantaloupe.resource.Request;
+import edu.illinois.library.cantaloupe.resource.TemplateVariables;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Spring Boot controller for IIIF Image API 2.x landing page.
+ * Replaces the previous iiif.v2.LandingResource class.
+ */
+@Controller("v3LandingController")
+@RequestMapping("/iiif/2")
+public class LandingController extends AbstractIIIFController {
+
+    @Autowired
+    public LandingController(Configuration configuration) {
+        super(configuration);
+    }
+
+    @GetMapping
+    public String iiif3Landing(Model model, HttpServletRequest request, HttpServletResponse response) throws EndpointDisabledException {
+        checkEndpointEnabled();
+
+        response.setHeader("Content-Type", "text/html;charset=UTF-8");
+
+        // Create a minimal request wrapper for template variables
+        Request requestWrapper = new Request(request, Collections.emptyList(), configuration);
+        model.addAllAttributes(TemplateVariables.getDefault(requestWrapper).getVars());
+
+        return "iiif_2_landing";
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Void> redirectToBase(HttpServletRequest request,
+                                               HttpServletResponse response) throws EndpointDisabledException {
+        checkEndpointEnabled();
+        return ResponseEntity.status(HttpStatus.SEE_OTHER)
+                .location(URI.create(request.getContextPath() + "/iiif/2"))
+                .build();
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.OPTIONS)
+    public void options(HttpServletResponse response) throws EndpointDisabledException {
+        checkEndpointEnabled();
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        response.setHeader("Allow", "GET,OPTIONS");
+    }
+}
