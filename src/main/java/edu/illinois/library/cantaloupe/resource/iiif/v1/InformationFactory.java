69298400673e72c3cff29b2357c8da2b7724a758
Justin Coyne <jcoyne@justincoyne.com>
2025-10-29 13:15:43 -0500

    Replace v1.InformationResource with a spring controller

--- src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/InformationFactory.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v1/InformationFactory.java
@@ -1,5 +1,7 @@
 package edu.illinois.library.cantaloupe.resource.iiif.v1;
 
+import java.util.Set;
+
 import edu.illinois.library.cantaloupe.config.Configuration;
 import edu.illinois.library.cantaloupe.config.Key;
 import edu.illinois.library.cantaloupe.image.Dimension;
@@ -9,8 +11,6 @@ import edu.illinois.library.cantaloupe.image.Metadata;
 import edu.illinois.library.cantaloupe.image.Orientation;
 import edu.illinois.library.cantaloupe.image.ScaleConstraint;
 import edu.illinois.library.cantaloupe.resource.iiif.ImageInfoUtil;
-
-import java.util.Set;
 
 /**
  * Builds new {@link Information} instances.
@@ -24,7 +24,7 @@ public final class InformationFactory {
      */
     private static final int DEFAULT_MIN_TILE_SIZE = 512;
 
-    Information newImageInfo(final String imageURI,
+    public Information newImageInfo(final String imageURI,
                              final Set<Format> availableOutputFormats,
                              final Info info,
                              final int imageIndex,
--- src/test/java/edu/illinois/library/cantaloupe/resource/RouteTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/resource/RouteTest.java
@@ -16,12 +16,7 @@ class RouteTest extends BaseTest {
 
     @Test
     void testForPathWithIIIFv1Routes() {
-        Route route = Route.forPath(Route.IIIF_1_PATH + "/0bef-234a/info.json");
-        assertEquals(edu.illinois.library.cantaloupe.resource.iiif.v1.InformationResource.class,
-                route.getResource());
-        assertEquals("0bef-234a", route.getPathArguments().get(0));
-
-        route = Route.forPath(Route.IIIF_1_PATH + "/0bef-234a/0,0,100,100/max/0/native.jpg");
+        Route route = Route.forPath(Route.IIIF_1_PATH + "/0bef-234a/0,0,100,100/max/0/native.jpg");
         assertEquals(edu.illinois.library.cantaloupe.resource.iiif.v1.ImageResource.class,
                 route.getResource());
         assertEquals("0bef-234a", route.getPathArguments().get(0));
