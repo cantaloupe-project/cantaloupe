862f636af02086a2ba44379094091d6fc0392588
Justin Coyne <jcoyne@justincoyne.com>
2025-10-29 14:21:54 -0500

    Convert v1.ImageResource to a Spring MVC Controller

--- src/test/java/edu/illinois/library/cantaloupe/resource/RouteTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/resource/RouteTest.java
@@ -15,28 +15,6 @@ class RouteTest extends BaseTest {
     }
 
     @Test
-    void testForPathWithIIIFv1Routes() {
-        Route route = Route.forPath(Route.IIIF_1_PATH + "/0bef-234a/0,0,100,100/max/0/native.jpg");
-        assertEquals(edu.illinois.library.cantaloupe.resource.iiif.v1.ImageResource.class,
-                route.getResource());
-        assertEquals("0bef-234a", route.getPathArguments().get(0));
-        assertEquals("0,0,100,100", route.getPathArguments().get(1));
-        assertEquals("max", route.getPathArguments().get(2));
-        assertEquals("0", route.getPathArguments().get(3));
-        assertEquals("native", route.getPathArguments().get(4));
-        assertEquals("jpg", route.getPathArguments().get(5));
-
-        route = Route.forPath(Route.IIIF_1_PATH + "/0bef-234a/0,0,100,100/max/0/native");
-        assertEquals(edu.illinois.library.cantaloupe.resource.iiif.v1.ImageResource.class,
-                route.getResource());
-        assertEquals("0bef-234a", route.getPathArguments().get(0));
-        assertEquals("0,0,100,100", route.getPathArguments().get(1));
-        assertEquals("max", route.getPathArguments().get(2));
-        assertEquals("0", route.getPathArguments().get(3));
-        assertEquals("native", route.getPathArguments().get(4));
-    }
-
-    @Test
     void testForPathWithInvalidRoute() {
         Route route = Route.forPath("/notfound");
         assertNull(route);
